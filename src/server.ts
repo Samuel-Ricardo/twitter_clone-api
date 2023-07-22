c2e8d9d2b35c0aee36c1caf0dcf530480496d722
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-07-22 10:53:19 -0300

    [:sparkles:] | create: server (app)

---
+++ src/server.ts
@@ -0,0 +1,9 @@
+import expresss from 'express'
+
+const app = expresss()
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(3000, () => console.log('[API] | listening on: http://localhost:3000'))
