3003c23e6efff0363354d728cfd7fc540f7d4116
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-07-27 18:48:12 -0300

    [:sparkles:] | create: fun - logs (SwaggerModule)

--- src/modules/documentation/swagger/swagger.module.ts
+++ src/modules/documentation/swagger/swagger.module.ts
@@ -6,6 +6,7 @@ import { ENV } from '@/modules/config/env';
 export class SwaggerModule {
   private static docs_on_prod: boolean = false;
   private static port: number | string;
+  private static doc_route: string;
 
   private static server() {
     return Swagger.serve;
@@ -16,7 +17,8 @@ export class SwaggerModule {
   }
 
   private static shouldRun() {
-    return ENV.ENVIRONMENT === 'prod' ? this.docs_on_prod : true;
+    const env_check = ENV.ENVIRONMENT === 'prod' ? this.docs_on_prod : true;
+    return env_check;
   }
 
   static setup(props: {
@@ -29,21 +31,25 @@ export class SwaggerModule {
     if (!this.shouldRun()) return;
 
     const { app, route } = props;
-    const doc_route = !route ? '/docs' : route;
+    this.doc_route = !route ? '/docs' : route;
     this.port = !props.port ? ENV.PORT : props.port;
 
-    app.use(doc_route, this.server(), this.initialize());
+    app.use(this.doc_route, this.server(), this.initialize());
 
-    app.get(`${doc_route}-json`, (req, res) => {
+    app.get(`${this.doc_route}-json`, (req, res) => {
       res.setHeader('Content-Type', 'application/json');
       res.send(SwaggerConfig);
     });
+  }
+
+  static logs() {
+    if (!this.shouldRun() && !this.doc_route) return;
 
     console.log(
-      `[DOCS] | Swagger UI is available at http://localhost:${port}${doc_route}`,
+      `[DOCS] | Swagger UI is available at http://localhost:${this.port}${this.doc_route}`,
     );
     console.log(
-      `[DOCS] | Documentation schema is available at http://localhost:${port}${doc_route}-json`,
+      `[DOCS] | Documentation schema is available at http://localhost:${this.port}${this.doc_route}-json`,
     );
   }
 }
