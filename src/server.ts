89dba0af8ec651aed8e07ee87f8bc1d4edec3c1a
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-07-27 17:57:40 -0300

    [:memo:] | setup: module - swagger (docs)

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,10 @@
 import { app } from './app';
+import { SwaggerModule } from './modules/documentation';
 
 const port = process.env.PORT || 3000;
 
-app.listen(port, () =>
-  console.log('[API] | listening on: http://localhost:3000'),
-);
+app.listen(port, () => {
+  SwaggerModule.setup({ app, port });
+
+  console.log('[API] | listening on: http://localhost:3000');
+});
