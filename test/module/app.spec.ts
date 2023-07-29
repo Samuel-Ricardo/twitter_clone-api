84871330ca4d96e8e2db0f7da81a7cc67c2e409b
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-07-29 14:10:36 -0300

    [:white_check_mark:]  | create: unit test  => prisma (module)

--- test/module/app.spec.ts
+++ test/module/app.spec.ts
@@ -1,4 +1,5 @@
 import { AppModule, MODULE } from '@modules';
+import { PrismaClient } from '@prisma/client';
 import { Express } from 'express';
 
 describe('[MODULE] | AppModule', () => {
@@ -7,5 +8,12 @@ describe('[MODULE] | AppModule', () => {
 
     expect(app).toBeDefined();
     expect(app).toHaveProperty('post');
+  });
+
+  it('Should be able to bind module [PRISMA]', () => {
+    const prisma = AppModule.get<PrismaClient>(MODULE.PRISMA);
+
+    expect(prisma).toBeDefined();
+    expect(prisma).toHaveProperty('user');
   });
 });
