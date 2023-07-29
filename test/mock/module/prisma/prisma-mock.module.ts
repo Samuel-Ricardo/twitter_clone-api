ed88233b86a861b45a01494111155c2cd51cc050
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-07-29 17:19:14 -0300

    [:clown_face:]  | create:  mock - database (prisma)

--- test/mock/module/prisma/prisma-mock.module.ts
+++ test/mock/module/prisma/prisma-mock.module.ts
@@ -1,19 +1,30 @@
+import { ENV } from '@test/env';
 import { PrismaClient } from '@prisma/client';
 import { Container } from 'inversify';
+import { randomUUID } from 'crypto';
 
 jest.mock('@prisma/client');
 
 const PrismaMock = PrismaClient as jest.Mock<PrismaClient>;
-
 const mockPrisma = () => new PrismaMock() as jest.Mocked<PrismaClient>;
+
+const url = ENV.DB.URL.replace('[hash]', randomUUID());
+
+console.log({ url });
 
 export const PrismaMockResgistry = {
   PRISMA: Symbol.for('Prisma'),
+  PRISMA_DEV: Symbol.for('PrismaDev'),
 };
 
 export const PrismaMockModule = new Container({ autoBindInjectable: true });
 
 PrismaMockModule.bind(PrismaMockResgistry.PRISMA).toDynamicValue(mockPrisma);
+PrismaMockModule.bind(PrismaMockResgistry.PRISMA_DEV).toConstantValue(
+  new PrismaClient({
+    datasources: { db: { url } },
+  }),
+);
 
 export const PrismaMockFactory = {
   PRISMA: () =>
