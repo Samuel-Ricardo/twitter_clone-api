dce39e16bcebd8c0cee65b1a0525c1ea20b05558
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-07-29 17:47:46 -0300

    [:sparkles:] | update: DTO - create (user)

--- src/modules/@core/user/DTO/create.dto.ts
+++ src/modules/@core/user/DTO/create.dto.ts
@@ -1,20 +1,6 @@
-import { injectable } from 'inversify';
-
-@injectable()
-export class CreateUserDTO {
-  constructor(
-    public name: string,
-    public username: string,
-    public email: string,
-    public password: string,
-  ) {}
-
-  toObject() {
-    return {
-      name: this.name,
-      username: this.username,
-      email: this.email,
-      password: this.password,
-    };
-  }
+export interface CreateUserDTO {
+  name: string;
+  username: string;
+  email: string;
+  password: string;
 }
