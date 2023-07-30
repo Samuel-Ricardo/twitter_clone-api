7783bccb3e86161d2e3c03f6f71463c26a164178
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-07-30 14:09:55 -0300

    [:clown_face:] | setup: module - app (mock)

--- test/mock/module/app.module.ts
+++ test/mock/module/app.module.ts
@@ -4,13 +4,25 @@ import {
   PrismaMockModule,
   PrismaMockFactory,
 } from './prisma';
+import {
+  PrismaRepositoryMockModule,
+  PrismaRepositoryMockRegistry,
+} from './repository/prisma/user.repository';
+import { RepositoryMockFactory } from './repository/repository.module';
+import { PrismaDevFactory } from './prisma/prisma-dev';
 
 export const MODULE_MOCK = {
   ...PrismaMockResgistry,
+  PRISMA: { ...PrismaRepositoryMockRegistry },
 };
 
-export const AppModuleMock = PrismaMockModule;
+export const AppModuleMock = Container.merge(
+  PrismaMockModule,
+  PrismaRepositoryMockModule,
+);
 
 export const MockFactory = {
   ...PrismaMockFactory,
+  ...PrismaDevFactory,
+  REPOSITORY: { ...RepositoryMockFactory },
 };
