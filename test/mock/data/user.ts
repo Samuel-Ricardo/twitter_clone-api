60b589d1bb0fcbc84002b4fbb0e3314122456941
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-07-30 14:01:29 -0300

    [:clown_face:] | create: data - user (mock)

---
+++ test/mock/data/user.ts
@@ -0,0 +1,9 @@
+import { CreateUserDTO, UpdateUserDTO, User } from '@User';
+import { randomUUID } from 'crypto';
+
+export const CREATE_USER_DATA: CreateUserDTO = {
+  name: 'pedro',
+  username: 'p3dr0',
+  email: 'pedro@email.com',
+  password: 'h3j2f6',
+};
