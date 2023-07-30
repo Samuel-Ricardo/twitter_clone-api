cc566e11967c3760490f1ff5bc2d2716d73946eb
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-07-30 16:57:09 -0300

    [:card_file_box:]  | update: module - prisma (app)

--- src/modules/prisma/prisma.module.ts
+++ src/modules/prisma/prisma.module.ts
@@ -2,11 +2,15 @@ import { PrismaClient } from '@prisma/client';
 import { Container } from 'inversify';
 
 export const PrismaRegistry = {
-  Prisma: Symbol.for('Prisma'),
+  PRISMA: Symbol.for('Prisma'),
 };
 
 export const PrismaModule = new Container({ autoBindInjectable: true });
 
-PrismaModule.bind<PrismaClient>(PrismaRegistry.Prisma).toConstantValue(
+PrismaModule.bind<PrismaClient>(PrismaRegistry.PRISMA).toConstantValue(
   new PrismaClient(),
 );
+
+export const PrismaFactory = {
+  PRISMA: () => PrismaModule.get<PrismaClient>(PrismaRegistry.PRISMA),
+};
