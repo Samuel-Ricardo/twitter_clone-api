bf2e4bd1bcca11465bf694f411f2da52674d6218
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-07-30 14:05:35 -0300

    [:clown_face:] | setup: prisma (mock)

--- test/mock/module/prisma/prisma-mock.module.ts
+++ test/mock/module/prisma/prisma-mock.module.ts
@@ -2,15 +2,13 @@ import { ENV } from '@test/env';
 import { PrismaClient } from '@prisma/client';
 import { Container } from 'inversify';
 import { randomUUID } from 'crypto';
+import { DeepMockProxy, mockDeep } from 'jest-mock-extended';
 
-jest.mock('@prisma/client');
-
-const PrismaMock = PrismaClient as jest.Mock<PrismaClient>;
-const mockPrisma = () => new PrismaMock() as jest.Mocked<PrismaClient>;
-
-const url = ENV.DB.URL.replace('[hash]', randomUUID());
-
-console.log({ url });
+let PrismaMock: DeepMockProxy<PrismaClient>;
+const mockPrisma = () => {
+  PrismaMock = mockDeep<PrismaClient>();
+  return PrismaMock;
+};
 
 export const PrismaMockResgistry = {
   PRISMA: Symbol.for('Prisma'),
@@ -20,15 +18,10 @@ export const PrismaMockResgistry = {
 export const PrismaMockModule = new Container({ autoBindInjectable: true });
 
 PrismaMockModule.bind(PrismaMockResgistry.PRISMA).toDynamicValue(mockPrisma);
-PrismaMockModule.bind(PrismaMockResgistry.PRISMA_DEV).toConstantValue(
-  new PrismaClient({
-    datasources: { db: { url } },
-  }),
-);
 
 export const PrismaMockFactory = {
   PRISMA: () =>
-    PrismaMockModule.get<jest.Mocked<PrismaClient>>(PrismaMockResgistry.PRISMA),
-  PRISMA_DEV: () =>
-    PrismaMockModule.get<PrismaClient>(PrismaMockResgistry.PRISMA_DEV),
+    PrismaMockModule.get<DeepMockProxy<PrismaClient>>(
+      PrismaMockResgistry.PRISMA,
+    ),
 };
