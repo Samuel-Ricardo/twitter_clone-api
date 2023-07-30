f0f2b3f830e613089dcf4fd8eb3feed89cc8e8e0
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-07-30 14:04:09 -0300

    [:clown_face:] | setup: prisma (test)

---
+++ test/mock/module/prisma/prisma-dev.ts
@@ -0,0 +1,14 @@
+import { randomUUID } from 'crypto';
+import { PrismaMockModule, PrismaMockResgistry } from './prisma-mock.module';
+import { PrismaClient } from '@prisma/client';
+import { ENV } from '@test/env';
+
+const url = ENV.DB.URL.replace('[hash]', 'test_' + randomUUID());
+
+console.log({ url });
+
+export const prisma_dev = new PrismaClient({
+  datasources: { db: { url } },
+});
+
+export const PrismaDevFactory = {};
