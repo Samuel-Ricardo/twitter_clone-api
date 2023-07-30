703c475c40b82cf8c7fec6573a976134c78db716
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-07-30 16:56:22 -0300

    [:sparkles:]  | update: index - module (app)

--- src/modules/index.ts
+++ src/modules/index.ts
@@ -1,7 +1,9 @@
+export * from './app.registry';
 export * from './app.module';
+export * from './app.factory';
 export * as DOC from './documentation';
 export * as CONFIG from './config/';
 export * from './logger';
 export * from './manager';
-export * from './repository';
 export * from './prisma';
+export * from './repository';
