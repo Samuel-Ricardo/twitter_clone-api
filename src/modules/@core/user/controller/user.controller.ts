ade64ebab37163503550c6680b3b34dedb1dee85
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-07-31 17:09:54 -0300

    [:sparkles:]  | update: factory - user (module)

--- src/modules/@core/user/controller/user.controller.ts
+++ src/modules/@core/user/controller/user.controller.ts
@@ -1,7 +1,11 @@
 import { UserService } from '@User/service/user.service';
-import { injectable } from 'inversify';
+import { inject, injectable } from 'inversify';
+import { UserRegistry as USER_MODULE } from '@User/user.registry';
 
 @injectable()
 export class UserController {
-  constructor(service: UserService) {}
+  constructor(
+    @inject(USER_MODULE.SERVICE.DEFAULT)
+    service: UserService,
+  ) {}
 }
