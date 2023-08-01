8bc669a25250c3ab69985790646d2176efa77e43
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-07-31 21:41:44 -0300

    [:sparkles:] | update: controller - user > create (module)

--- src/modules/@core/user/controller/user.controller.ts
+++ src/modules/@core/user/controller/user.controller.ts
@@ -1,11 +1,16 @@
 import { UserService } from '@User/service/user.service';
 import { inject, injectable } from 'inversify';
-import { UserRegistry as USER_MODULE } from '@User/user.registry';
+import { UserRegistry as USER_MODULE } from '../user.registry';
+import { CreateUserDTO } from '@User/DTO';
 
 @injectable()
 export class UserController {
   constructor(
     @inject(USER_MODULE.SERVICE.DEFAULT)
-    service: UserService,
+    private service: UserService,
   ) {}
+
+  async create(data: CreateUserDTO) {
+    return await this.service.create(data);
+  }
 }
