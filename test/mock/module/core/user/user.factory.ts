9c2869e5926b97290dc7a21708cb2d10a6d347c1
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-08-01 17:20:04 -0300

    [:clown_face:] | setup: factory - user (mock/module)

---
+++ test/mock/module/core/user/user.factory.ts
@@ -0,0 +1,13 @@
+import { DeepMockProxy } from 'jest-mock-extended';
+import { UserMockModule } from './user.module';
+import { UserMockRegistry } from './user.resgistry';
+import { CreateUserUseCase } from '@User';
+
+export const UserMockFactory = {
+  USE_CASE: {
+    CREATE: () =>
+      UserMockModule.get<DeepMockProxy<CreateUserUseCase>>(
+        UserMockRegistry.USE_CASE.CREATE,
+      ),
+  },
+};
