00bd2aba6a5579c89521f435aa5aec7918054d2a
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-08-01 17:38:33 -0300

    [:clown_face:] | update: registry - user (mock/module)

--- test/mock/module/core/user/user.resgistry.ts
+++ test/mock/module/core/user/user.resgistry.ts
@@ -2,5 +2,8 @@ export const UserMockRegistry = {
   USE_CASE: {
     CREATE: Symbol.for('CreateUserMockUseCase'),
     UPDATE: Symbol.for('UpdateUserMockUseCase'),
+    SELECT: {
+      BY_ID: Symbol.for('SelectUserByIdMockUseCase'),
+    },
   },
 };
