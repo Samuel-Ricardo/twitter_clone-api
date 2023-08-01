202e99188c0b306e176ed111e279ca44583ad406
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-08-01 16:18:27 -0300

    [:white_check_mark:] | update: E2E - healthcheck (test)

--- test/E2E/server.spec.ts
+++ test/E2E/server.spec.ts
@@ -6,6 +6,6 @@ describe('[API] | health check', () => {
     const response = await supertest(app).get('/');
 
     expect(response.status).toBe(200);
-    expect(response.body).toEqual({ result: 'Hello World! :D' });
+    expect(response.body).toEqual({ message: 'Hello World! :D' });
   });
 });
