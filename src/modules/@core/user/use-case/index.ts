0c0701617c1e68c8472e664873444682e90515ac
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-08-01 17:22:33 -0300

    [:truck:] | create: index - use-case > user (module)

--- src/modules/@core/user/use-case/index.ts
+++ src/modules/@core/user/use-case/index.ts
@@ -1,1 +1,5 @@
 export * from './create.use-case';
+export * from './delete.use-case';
+export * from './update.use-case';
+export * from './select_all.use-case';
+export * from './select_by_id.use-case';
