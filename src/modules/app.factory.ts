0eab94cb4967d379014d93755037eed81789953d
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-07-31 23:33:47 -0300

    [:sparkles:] | update: factory - app (module)

--- src/modules/app.factory.ts
+++ src/modules/app.factory.ts
@@ -1,13 +1,16 @@
+import 'reflect-metadata';
 import { MODULE } from './app.registry';
 
 import { AppModule } from './app.module';
-import { RepositoryFactory } from './repository/repository.factory';
+import { RepositoryFactory as REPOSITORY } from './repository/repository.factory';
 import { PrismaFactory } from './prisma';
+import { USER_MODULE as USER } from './@core/user/user.factory';
 
 import { Express } from 'express';
 
 export const MODULES = {
   ...PrismaFactory,
-  REPOSITORY: { ...RepositoryFactory },
+  USER,
+  REPOSITORY,
   APP: () => AppModule.get<Express>(MODULE.APP),
 };
