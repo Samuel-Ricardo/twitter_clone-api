7119a949b4a2aca3848866e1fa025d82c454ca91
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-07-31 21:26:04 -0300

    [:sparkles:]  | create: service - user (module)

--- src/modules/@core/user/service/user.service.ts
+++ src/modules/@core/user/service/user.service.ts
@@ -1,6 +1,16 @@
-import { injectable } from 'inversify';
+import { CreateUserDTO } from '@User/DTO';
+import { CreateUserUseCase } from '@User/use-case';
+import { USER_MODULE } from '../user.factory';
+import { inject, injectable } from 'inversify';
 
 @injectable()
 export class UserService {
-  constructor() {}
+  constructor(
+    @inject(USER_MODULE.USE_CASE.CREATE)
+    private createUser: CreateUserUseCase,
+  ) {}
+
+  async create(data: CreateUserDTO) {
+    return await this.createUser.execute(data);
+  }
 }
