37bf8050e19be96eda9c76269275109594254728
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-08-01 16:30:07 -0300

    [:sparkles:] | update: factory - app > router (module)

--- src/modules/app.factory.ts
+++ src/modules/app.factory.ts
@@ -7,9 +7,11 @@ import { PrismaFactory } from './prisma';
 import { USER_MODULE as USER } from './@core/user/user.factory';
 
 import { Express } from 'express';
+import { RoutesFactory } from './routes/routes.factory';
 
 export const MODULES = {
   ...PrismaFactory,
+  ...RoutesFactory,
   USER,
   REPOSITORY,
   APP: () => AppModule.get<Express>(MODULE.APP),
