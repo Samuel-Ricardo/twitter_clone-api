3925005394806b7c4092e6e3f02d7629ecb37a98
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-08-01 17:26:16 -0300

    [:clown_face:] | update: module - user (mock)

--- test/mock/module/core/user/user.module.ts
+++ test/mock/module/core/user/user.module.ts
@@ -1,9 +1,13 @@
 import { Container } from 'inversify';
-import { mockCreateUserUseCase } from './use-case/create';
+import { mockCreateUserUseCase, mockUpdateUserUseCase } from './use-case';
 import { UserMockRegistry } from './user.resgistry';
 
 export const UserMockModule = new Container({ autoBindInjectable: true });
 
 UserMockModule.bind(UserMockRegistry.USE_CASE.CREATE).toDynamicValue(
   mockCreateUserUseCase,
+);
+
+UserMockModule.bind(UserMockRegistry.USE_CASE.UPDATE).toDynamicValue(
+  mockUpdateUserUseCase,
 );
