d5a59173aea33ecdfb3072541534efea99113824
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-08-01 15:59:19 -0300

    [:sparkles:] | update: repository - prisma > user (module)

--- src/modules/repository/prisma/user.repository.ts
+++ src/modules/repository/prisma/user.repository.ts
@@ -6,15 +6,17 @@ import {
   User,
 } from '@User';
 import { IDeleteuserDTO } from '@User/DTO';
-import { MODULE, MODULES } from '@modules';
+import { MODULE } from '@modules';
 import { PrismaClient, User as PrismaUser } from '@prisma/client';
-import { inject, injectable } from 'inversify';
+import { inject, injectable, interfaces } from 'inversify';
 
 @injectable()
 export class PrismaUserRepository implements IUserRepository {
   constructor(
     @inject(MODULE.PRISMA)
     private readonly prisma: PrismaClient,
+    @inject(MODULE.USER.FOR_PRISMA)
+    private readonly userFactory: interfaces.Factory<User, [PrismaUser]>,
   ) {}
 
   async create(user: CreateUserDTO): Promise<User> {
@@ -26,13 +28,10 @@ export class PrismaUserRepository implements IUserRepository {
   }
 
   async selectAll(): Promise<User[]> {
-    // return await this.prisma.user.findMany();
-
     const _users = await this.prisma.user.findMany();
-    const factory = MODULES.USER.FOR_PRISMA();
 
     const users = _users.map((user) => {
-      return factory(user);
+      return this.userFactory(user);
     });
 
     return users as User[];
