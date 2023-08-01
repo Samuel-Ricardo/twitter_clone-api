58bdc775b6bee3652ecb543ef6ebc8e6380eac3b
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-08-01 17:46:05 -0300

    [:clown_face:] | update: module - app (mock)

--- test/mock/module/app.module.ts
+++ test/mock/module/app.module.ts
@@ -1,8 +1,10 @@
 import { Container } from 'inversify';
 import { PrismaMockModule } from './prisma';
 import { PrismaRepositoryMockModule } from './repository/prisma/user.repository';
+import { UserMockModule } from './core/user';
 
 export const AppModuleMock = Container.merge(
   PrismaMockModule,
   PrismaRepositoryMockModule,
+  UserMockModule,
 );
