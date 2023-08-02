07f4c906eb8cbcf978025655aef258eb08a08401
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-08-02 14:32:34 -0300

    [:sparkles:] | update:  app (module)

--- src/modules/app.module.ts
+++ src/modules/app.module.ts
@@ -5,13 +5,15 @@ import { Container } from 'inversify';
 import { PrismaModule } from './prisma/prisma.module';
 import { RepositoryModule } from './repository/repository.module';
 
-import { Express } from 'express';
+import { Express, json } from 'express';
 import { MODULE } from './app.registry';
 import { UserModule } from '@User';
 import { RoutesModule } from './router/router.module';
 
 const Module = new Container();
+
 Module.bind<Express>(MODULE.APP).toConstantValue(Application.Instance());
+Module.bind(MODULE.BODY_PARSER).toConstantValue(json());
 
 export const AppModule = Container.merge(
   Module,
