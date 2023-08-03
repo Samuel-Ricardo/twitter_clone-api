6b3690591aff2b046b1acb156af73b23742bae5f
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-08-03 16:05:39 -0300

    [:recycle:] | refact: move - middleware > logger (module)

--- src/modules/middleware/logger/logger.module.ts
+++ src/modules/middleware/logger/logger.module.ts
@@ -5,15 +5,20 @@ export const loggerMiddleware: RequestHandler = (req, res, next) => {
   const method = req.method;
   const body = req.body;
   const params = req.params;
-  const response = res.json();
 
   switch (method) {
     case 'POST':
-      logger.info({ context: 'POST', message: `url: ${req.url}` }, { body });
+      logger.info(
+        { context: 'POST', message: `url: ${req.url}` },
+        { params, body },
+      );
       break;
 
     case 'PUT':
-      logger.info({ context: 'PUT', message: `url: ${req.url}` }, { body });
+      logger.info(
+        { context: 'PUT', message: `url: ${req.url}` },
+        { params, body },
+      );
       break;
 
     case 'DELETE':
@@ -24,10 +29,7 @@ export const loggerMiddleware: RequestHandler = (req, res, next) => {
       break;
 
     case 'GET':
-      logger.info(
-        { context: 'GET', message: `url: ${req.url}` },
-        { params, response },
-      );
+      logger.info({ context: 'GET', message: `url: ${req.url}` }, { params });
       break;
   }
 
