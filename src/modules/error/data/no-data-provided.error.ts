3c345c9b264ef75aa591d0735dc1fc540daef85d
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-08-03 11:13:18 -0300

    [:goal_net:]  | create: error - NoDataProvidedError (module)

---
+++ src/modules/error/data/no-data-provided.error.ts
@@ -0,0 +1,12 @@
+import { AppError } from '../app.error';
+
+export class NoDataProvidedError extends AppError {
+  constructor(
+    public message: string,
+    public status: number = 404,
+    public data?: any,
+    public error: boolean = true,
+  ) {
+    super(message, status, data, error);
+  }
+}
