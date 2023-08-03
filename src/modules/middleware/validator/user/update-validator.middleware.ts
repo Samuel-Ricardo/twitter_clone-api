6f39da446257172a0b7ac656e7edaf0cde188987
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-08-03 16:08:15 -0300

    [:bug:] | fix: api timeout (missing next())

--- src/modules/middleware/validator/user/update-validator.middleware.ts
+++ src/modules/middleware/validator/user/update-validator.middleware.ts
@@ -4,6 +4,7 @@ import { RequestHandler } from 'express';
 export const validateUpdateUserDTO: RequestHandler = (req, res, next) => {
   try {
     USER_MODULE.VALIDATOR().validateUpdateDTO(req.body);
+    next();
   } catch (error) {
     next(error);
   }
