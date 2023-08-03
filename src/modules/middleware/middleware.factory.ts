f7c979909270d83760ba8625be5eef04bf127de7
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-08-03 14:33:05 -0300

    [:safety_vest:] | update: rfactory - middleware ] (module)

--- src/modules/middleware/middleware.factory.ts
+++ src/modules/middleware/middleware.factory.ts
@@ -1,7 +1,16 @@
-import { ErrorRequestHandler } from 'express';
+import { ErrorRequestHandler, RequestHandler } from 'express';
 import { MiddlewareModule } from './middleware.module';
 import { MiddlewareRegistry } from './middleware.registry';
 
 export const MiddlewareFactory = {
-  ERROR: MiddlewareModule.get<ErrorRequestHandler>(MiddlewareRegistry.ERROR),
+  ERROR: () =>
+    MiddlewareModule.get<ErrorRequestHandler>(MiddlewareRegistry.ERROR),
+  LOGGER: {
+    APP: () =>
+      MiddlewareModule.get<RequestHandler>(MiddlewareRegistry.LOGGER.APP),
+    ERROR: () =>
+      MiddlewareModule.get<ErrorRequestHandler>(
+        MiddlewareRegistry.LOGGER.ERROR,
+      ),
+  },
 };
