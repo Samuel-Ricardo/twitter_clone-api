4bf94478fda623c672d5b8b0949c7b2d5200cf7b
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-08-03 14:15:10 -0300

    [:safety_vest:] | create: validator - user (module)

---
+++ src/modules/@core/user/validator/user.validator.ts
@@ -0,0 +1,17 @@
+import { CreateUserDTO } from '@User/DTO';
+import { InvalidDataError, NoDataProvidedError } from '@modules/error/data';
+import { injectable } from 'inversify';
+
+@injectable()
+export class UserValidator {
+  constructor() {}
+
+  validateCreateDTO(data: CreateUserDTO) {
+    if (!data) throw new NoDataProvidedError();
+
+    if (!data.name) throw new InvalidDataError('name is missing');
+    if (!data.username) throw new InvalidDataError('username is missing');
+    if (!data.email) throw new InvalidDataError('email is missing');
+    if (!data.password) throw new InvalidDataError('password is missing');
+  }
+}
