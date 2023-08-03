813422950cb1be658ec511de28962441fa981c82
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-08-03 16:06:52 -0300

    [:bug:] | fix: api timeout (missing next())

--- src/modules/middleware/validator/user/create-validator.middleware.ts
+++ src/modules/middleware/validator/user/create-validator.middleware.ts
@@ -4,6 +4,7 @@ import { RequestHandler } from 'express';
 export const validateCreateUserData: RequestHandler = (req, res, next) => {
   try {
     USER_MODULE.VALIDATOR().validateCreateDTO(req.body);
+    next();
   } catch (error) {
     next(error);
   }
