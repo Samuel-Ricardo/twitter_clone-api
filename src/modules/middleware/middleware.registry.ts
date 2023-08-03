03c9b9d4fd78a18baaef6e32b98c053085318024
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-08-03 00:53:54 -0300

    [:sparkles:] | update: registry - middleware > logger (module)

--- src/modules/middleware/middleware.registry.ts
+++ src/modules/middleware/middleware.registry.ts
@@ -1,3 +1,7 @@
 export const MiddlewareRegistry = {
   ERROR: Symbol('Middleware.ERROR'),
+  LOGGER: {
+    APP: Symbol('Middleware.LOGGER.APP'),
+    ERROR: Symbol('Middleware.LOGGER.ERROR'),
+  },
 };
