a5f7eb42635747185bcb42dda74cca256cc684f5
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-08-03 16:07:21 -0300

    [:bug:] | fix: api timeout (missing next())

--- src/modules/middleware/validator/user/delete-validator.middleware.ts
+++ src/modules/middleware/validator/user/delete-validator.middleware.ts
@@ -4,6 +4,7 @@ import { RequestHandler } from 'express';
 export const validateDeleteUserDTO: RequestHandler = (req, res, next) => {
   try {
     USER_MODULE.VALIDATOR().validateDeleteDTO({ id: req.params.id });
+    next();
   } catch (e) {
     next(e);
   }
