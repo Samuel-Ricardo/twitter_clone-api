a6c75050811fb8914ca0c2fcd69c617cc26dd96e
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-08-03 14:36:35 -0300

    [:safety_vest:] | update: module - middleware > validator

--- src/modules/middleware/middleware.module.ts
+++ src/modules/middleware/middleware.module.ts
@@ -4,6 +4,7 @@ import { ERROR_MIDDLEWARE } from './error';
 import { loggerMiddleware } from './logger/logger.module';
 import { errorLoggerMiddleware } from './logger/error/logger.middleware';
 import { UserModule } from '@User';
+import { validateCreateUserData } from './validator';
 
 const Module = new Container();
 
@@ -12,6 +13,10 @@ Module.bind(MiddlewareRegistry.ERROR).toConstantValue(ERROR_MIDDLEWARE);
 Module.bind(MiddlewareRegistry.LOGGER.APP).toConstantValue(loggerMiddleware);
 Module.bind(MiddlewareRegistry.LOGGER.ERROR).toConstantValue(
   errorLoggerMiddleware,
+);
+
+Module.bind(MiddlewareRegistry.VALIDATOR.USER.CREATE).toConstantValue(
+  validateCreateUserData,
 );
 
 export const MiddlewareModule = Container.merge(Module, UserModule);
