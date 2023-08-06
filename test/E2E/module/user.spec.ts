5c794d2fc5e9308fd4a2e8f8550071a9ed0f1bcd
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-08-05 23:46:00 -0300

    [:white_check_mark:] | create: E2E test - user > select by id (module)

--- test/E2E/module/user.spec.ts
+++ test/E2E/module/user.spec.ts
@@ -8,7 +8,7 @@ import { CREATE_USER_DATA } from '@test/mock';
 import supertest from 'supertest';
 
 describe('[MODULE] | User', () => {
-  jest.setTimeout(30000);
+  let user: User;
 
   it('[E2E] | Should: Create => [USER]', async () => {
     const response = await supertest(app).post('/users').send(CREATE_USER_DATA);
@@ -19,6 +19,8 @@ describe('[MODULE] | User', () => {
     expect(body.user.username).toBe(CREATE_USER_DATA.username);
     expect(body.user.email).toBe(CREATE_USER_DATA.email);
     expect(body.user.bio).toBeNull();
+
+    user = body.user;
   });
 
   it('[E2E] | Should: Select - [all] => [USER]', async () => {
@@ -27,5 +29,16 @@ describe('[MODULE] | User', () => {
     expect(response.status).toBe(200);
     expect(response.body.users).toBeInstanceOf(Array);
     expect(response.body.users.length).toBeGreaterThanOrEqual(0);
+    expect(response.body.users).toContainEqual(user);
+  });
+
+  it('[E2E] | Should: Select - [by id] => [USER]', async () => {
+    const response = await supertest(app).get(`/users/${user.id}`);
+    const body: { user: User } = response.body;
+
+    expect(response.status).toBe(200);
+    expect(body.user).toHaveProperty('id');
+    expect(body.user.id).toEqual(user.id);
+    expect(body.user.username).toBe(user.username);
   });
 });
