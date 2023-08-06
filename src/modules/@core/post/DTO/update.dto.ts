dfee6b325666f19ec91d852d76d593fed49fe68e
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-08-06 16:55:04 -0300

    [:safety_vest:] | create: schema - update > post (module)

--- src/modules/@core/post/DTO/update.dto.ts
+++ src/modules/@core/post/DTO/update.dto.ts
@@ -1,4 +1,14 @@
+import { catchZod } from '@modules/util/validator';
+import { z } from 'zod';
+
 export interface IUpdatePostDTO {
   body: string;
   image?: string;
 }
+
+export const UpdatePostSchema = z
+  .object({
+    body: z.string().catch(catchZod),
+    image: z.string().optional().catch(catchZod),
+  })
+  .catch(catchZod);
