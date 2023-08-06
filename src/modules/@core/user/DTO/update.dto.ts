ac48ad9f22f572959f2f0efa3da4d6db88385e15
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-08-06 12:41:40 -0300

    [:safety_vest:] | create: schema - DTO > update (user)

--- src/modules/@core/user/DTO/update.dto.ts
+++ src/modules/@core/user/DTO/update.dto.ts
@@ -1,4 +1,7 @@
+import { InvalidDataError } from '@modules/error/data';
+import { catchZod } from '@modules/util/validator';
 import { injectable } from 'inversify';
+import { z } from 'zod';
 
 export interface UpdateUserDTO {
   id: string;
@@ -16,3 +19,20 @@ export interface UpdateUserDTO {
   coverImage?: string;
   profileImage?: string;
 }
+
+export const UpdateUserSchema = z.object({
+  id: z.string().nonempty().catch(catchZod),
+  name: z.string().optional().catch(catchZod),
+  username: z.string().optional().catch(catchZod),
+  bio: z.string().max(255).optional().catch(catchZod),
+  email: z.string().email().optional().catch(catchZod),
+  password: z.string().optional().catch(catchZod),
+  createdAt: z.date().optional().catch(catchZod),
+  updatedAt: z.date().optional().catch(catchZod),
+  sessionToken: z.string().optional().catch(catchZod),
+  hasNotifications: z.boolean().optional().catch(catchZod),
+  emailVerified: z.date().optional().catch(catchZod),
+  image: z.string().optional().catch(catchZod),
+  coverImage: z.string().optional().catch(catchZod),
+  profileImage: z.string().optional().catch(catchZod),
+});
