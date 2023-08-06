c928df64a0d33ec46aaaff5a9b41c6cce85150b8
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-08-06 16:51:58 -0300

    [:safety_vest:] | create: schema - find by  id > post (module)

--- src/modules/@core/post/DTO/find_by_id.dto.ts
+++ src/modules/@core/post/DTO/find_by_id.dto.ts
@@ -1,3 +1,10 @@
+import { catchZod } from '@modules/util/validator';
+import { z } from 'zod';
+
 export interface IFindPostByIdDTO {
   id: string;
 }
+
+export const FindPostByIdSchema = z.object({
+  id: z.string().nonempty().catch(catchZod),
+});
