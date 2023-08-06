8a793783d4e0bcde84cfb2299f0d674983423a27
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-08-06 16:59:33 -0300

    [:card_file_box:] | create: repositry - post (module)

---
+++ src/modules/@core/post/post.repository.ts
@@ -0,0 +1,17 @@
+import { Post } from '@prisma/client';
+import {
+  ICreatePostDTO,
+  IFindPostByIdDTO,
+  IFindPostByAuthorIdDTO,
+  IUpdatePostDTO,
+  IDeletePostDTO,
+} from './DTO';
+
+export interface IPostRepository {
+  create(data: ICreatePostDTO): Promise<Post>;
+  findAll(): Promise<Post[]>;
+  findById(data: IFindPostByIdDTO): Promise<Post>;
+  findByAuhorId(data: IFindPostByAuthorIdDTO): Promise<Post>;
+  update(data: IUpdatePostDTO): Promise<Post>;
+  delete(post: IDeletePostDTO): Promise<Post>;
+}
