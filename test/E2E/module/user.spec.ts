fdcab9995e5b39bc06ea0c5945181339cab74cf6
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-08-05 23:41:16 -0300

    [:white_check_mark:] | create: E2E test - user > create (module)

--- test/E2E/module/user.spec.ts
+++ test/E2E/module/user.spec.ts
@@ -3,9 +3,24 @@
  */
 
 import { app } from '@/app';
+import { User } from '@User';
+import { CREATE_USER_DATA } from '@test/mock';
 import supertest from 'supertest';
 
 describe('[MODULE] | User', () => {
+  jest.setTimeout(30000);
+
+  it('[E2E] | Should: Create => [USER]', async () => {
+    const response = await supertest(app).post('/users').send(CREATE_USER_DATA);
+    const body: { user: User } = response.body;
+
+    expect(response.status).toBe(201);
+    expect(body.user).toHaveProperty('id');
+    expect(body.user.username).toBe(CREATE_USER_DATA.username);
+    expect(body.user.email).toBe(CREATE_USER_DATA.email);
+    expect(body.user.bio).toBeNull();
+  });
+
   it('[E2E] | Should: Select - [all] => [USER]', async () => {
     const response = await supertest(app).get('/users');
 
