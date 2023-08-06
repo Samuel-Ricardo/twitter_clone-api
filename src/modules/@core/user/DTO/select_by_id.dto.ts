a218d55a52ddade12f905ceb50a508067fb77626
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-08-06 13:46:43 -0300

    [:safety_vest:] | create: scheme - DTO > delete (user)

--- src/modules/@core/user/DTO/select_by_id.dto.ts
+++ src/modules/@core/user/DTO/select_by_id.dto.ts
@@ -1,3 +1,10 @@
+import { catchZod } from '@modules/util/validator';
+import { z } from 'zod';
+
 export interface SelectUserByIdDTO {
   id: string;
 }
+
+export const SelectUserByIdSchema = z.object({
+  id: z.string().nonempty().catch(catchZod),
+});
