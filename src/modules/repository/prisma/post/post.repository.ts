06f5db63197b79b3320ea7646e7b6025342ada88
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-08-06 17:21:23 -0300

    [:card_file_box:] | create: repository - prisma > create (post)

--- src/modules/repository/prisma/post/post.repository.ts
+++ src/modules/repository/prisma/post/post.repository.ts
@@ -7,10 +7,20 @@ import {
   IUpdatePostDTO,
   Post,
 } from '@Post';
+import { MODULE } from '@modules/app.registry';
+import { PrismaClient } from '@prisma/client';
+import { inject, injectable } from 'inversify';
 
+@injectable()
 export class PrismaPostRepository implements IPostRepository {
-  create(data: ICreatePostDTO): Promise<Post> {
-    throw new Error('Method not implemented.');
+  constructor(
+    @inject(MODULE.PRISMA)
+    private readonly prisma: PrismaClient,
+  ) {}
+
+  async create(data: ICreatePostDTO) {
+    const result = await this.prisma.post.create({ data });
+    return Post.fromPrisma(result);
   }
   findAll(): Promise<Post[]> {
     throw new Error('Method not implemented.');
