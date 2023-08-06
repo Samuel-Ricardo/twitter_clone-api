520704a42d2e568737deb66ee6200103929ed0be
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-08-06 16:47:51 -0300

    [:safety_vest:] | create: schema - delete > post (module)

--- src/modules/@core/post/DTO/delete.dto.ts
+++ src/modules/@core/post/DTO/delete.dto.ts
@@ -1,3 +1,10 @@
+import { catchZod } from '@modules/util/validator';
+import { z } from 'zod';
+
 export interface IDeletePostDTO {
   id: string;
 }
+
+export const DeletePostSchema = z.object({
+  id: z.string().nonempty().catch(catchZod),
+});
