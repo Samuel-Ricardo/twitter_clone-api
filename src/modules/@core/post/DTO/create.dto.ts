66a432b2c55401cb33fed0a1269fc70a0344c88f
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-08-06 16:45:28 -0300

    [:sparkles:] | create: schema - create > post (module)

--- src/modules/@core/post/DTO/create.dto.ts
+++ src/modules/@core/post/DTO/create.dto.ts
@@ -1,5 +1,14 @@
+import { catchZod } from '@modules/util/validator';
+import { z } from 'zod';
+
 export interface ICreatePostDTO {
   authorId: string;
   body: string;
   image?: string;
 }
+
+export const CreatePostSchema = z.object({
+  authorId: z.string().nonempty().catch(catchZod),
+  body: z.string().nonempty().catch(catchZod),
+  image: z.string().optional().catch(catchZod),
+});
