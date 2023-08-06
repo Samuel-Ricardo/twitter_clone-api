859dac99b86d214d46e4d50b7827a8d9e11fed4a
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-08-06 16:36:08 -0300

    [:sparkles:] | update: entity - post (module)

--- src/modules/@core/post/post.entity.ts
+++ src/modules/@core/post/post.entity.ts
@@ -1,4 +1,5 @@
 import { injectable } from 'inversify';
+import { IPostDTO } from './DTO/post.dto';
 
 @injectable()
 export class Post {
@@ -10,4 +11,15 @@ export class Post {
     private updatedAt: Date,
     private image?: string,
   ) {}
+
+  public static create(data: IPostDTO): Post {
+    return new Post(
+      data.id,
+      data.body,
+      data.authorId,
+      data.createdAt,
+      data.updatedAt,
+      data.image,
+    );
+  }
 }
