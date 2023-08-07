ade9ecfd668f7b0bb41307a08873d61ece584f20
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-08-07 16:44:31 -0300

    [:recycle:] | update: index - reposiroty (mock/module)

--- test/mock/module/repository/prisma/index.ts
+++ test/mock/module/repository/prisma/index.ts
@@ -1,1 +1,5 @@
 export * from './user.repository';
+export * from './post.repository';
+export * from './prisma-repository.module';
+export * from './prisma-repository.registry';
+export * from './prisma-repository.factory';
