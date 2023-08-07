dbf5a96af965ae9b324cad5c70eadf0d98a19f32
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-08-07 15:04:41 -0300

    [:clown_face:] | simulate: repository - prisma > post (modules)

--- test/mock/module/repository/prisma/post.repository.ts
+++ test/mock/module/repository/prisma/post.repository.ts
@@ -1,4 +1,12 @@
 import { PrismaPostRepository } from '@modules/repository/prisma/post';
 import { mockDeep } from 'jest-mock-extended';
+import { MockFactory } from '../../app.factory';
 
 export const mockPrismaPostRepository = () => mockDeep<PrismaPostRepository>();
+
+export const simulatePrismaPostRepository = () => {
+  const prisma = MockFactory.PRISMA();
+  const repository = new PrismaPostRepository(prisma);
+
+  return { repository, prisma };
+};
