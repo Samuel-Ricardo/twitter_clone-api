6910b512eae0e105638dce5e3c4d6ce64b36f617
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-08-07 16:43:27 -0300

    [:recycle:] | refact: imports - reposiroty (mock/module)

--- test/mock/module/repository/repository.module.ts
+++ test/mock/module/repository/repository.module.ts
@@ -1,8 +1,6 @@
-import {
-  PrismaRepositoryFactoryMock,
-  PrismaRepositoryMockModule,
-  PrismaRepositoryMockRegistry,
-} from './prisma/user.repository';
+import { PrismaRepositoryMockRegistry } from './prisma/prisma-repository.registry';
+import { PrismaRepositoryMockModule } from './prisma/prisma-repository.module';
+import { PrismaRepositoryFactoryMock } from './prisma/prisma-repository.factory';
 
 export const RepositoryMockRegistry = {
   PRISMA: { ...PrismaRepositoryMockRegistry },
