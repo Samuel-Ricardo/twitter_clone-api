3517ed3f77180981255c6f28e63889ed758adab2
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-08-07 16:46:31 -0300

    [:clown_face:] | update: factory - repository > prisma (module)

---
+++ test/mock/module/repository/prisma/prisma-repository.factory.ts
@@ -0,0 +1,30 @@
+import { PrismaPostRepository } from '@modules/repository/prisma/post';
+import { PrismaClient } from '@prisma/client';
+import { DeepMockProxy } from 'jest-mock-extended';
+import { PrismaRepositoryMockModule } from './prisma-repository.module';
+import { PrismaUserRepository } from '@modules';
+import { PrismaRepositoryMockRegistry } from './prisma-repository.registry';
+
+type simulatePostType = {
+  repository: PrismaPostRepository;
+  prisma: DeepMockProxy<PrismaClient>;
+};
+
+export const PrismaRepositoryFactoryMock = {
+  USER: () =>
+    PrismaRepositoryMockModule.get<DeepMockProxy<PrismaUserRepository>>(
+      PrismaRepositoryMockRegistry.USER,
+    ),
+  USER_DEV: () =>
+    PrismaRepositoryMockModule.get<PrismaUserRepository>(
+      PrismaRepositoryMockRegistry.USER_DEV,
+    ),
+  POST: () =>
+    PrismaRepositoryMockModule.get<DeepMockProxy<PrismaPostRepository>>(
+      PrismaRepositoryMockRegistry.POST,
+    ),
+  POST_DEV: () =>
+    PrismaRepositoryMockModule.get<simulatePostType>(
+      PrismaRepositoryMockRegistry.POST_DEV,
+    ),
+};
