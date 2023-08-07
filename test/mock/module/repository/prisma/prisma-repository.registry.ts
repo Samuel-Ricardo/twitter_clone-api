33cc0f9b1a086ca797bd5d3493836d1f0530518f
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-08-07 16:39:25 -0300

    [:clown_face:] | update: registry - repository > prisma (module)

---
+++ test/mock/module/repository/prisma/prisma-repository.registry.ts
@@ -0,0 +1,6 @@
+export const PrismaRepositoryMockRegistry = {
+  USER: Symbol.for('UserPrismaRepositoryMock'),
+  USER_DEV: Symbol.for('UserPrismaDevRepositoryMock'),
+  POST: Symbol.for('PostPrismaRepositoryMock'),
+  POST_DEV: Symbol.for('PostPrismaDevRepositoryMock'),
+};
