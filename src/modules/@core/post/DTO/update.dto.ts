d3e55fa462e57b826d22ea09a5687a9c12aa2641
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-08-07 16:48:16 -0300

    [:recycle:] | refact: type - DTO > post (module)

--- src/modules/@core/post/DTO/update.dto.ts
+++ src/modules/@core/post/DTO/update.dto.ts
@@ -2,12 +2,14 @@ import { catchZod } from '@modules/util/validator';
 import { z } from 'zod';
 
 export interface IUpdatePostDTO {
+  id: string;
   body: string;
   image?: string;
 }
 
 export const UpdatePostSchema = z
   .object({
+    id: z.string().catch(catchZod),
     body: z.string().catch(catchZod),
     image: z.string().optional().catch(catchZod),
   })
