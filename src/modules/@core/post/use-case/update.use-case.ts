7904ab93ad42633ec63495a036a6bcd4900a177f
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-08-08 15:25:32 -0300

    [:sparkles:] | update: use-case > post (module)

--- src/modules/@core/post/use-case/update.use-case.ts
+++ src/modules/@core/post/use-case/update.use-case.ts
@@ -1,15 +1,9 @@
 import { IUpdatePostDTO } from '@Post/DTO';
-import { IPostRepository } from '@Post/post.repository';
-import { MODULE } from '@modules/app.registry';
-import { inject, injectable } from 'inversify';
+import { injectable } from 'inversify';
+import { PostRepositoryAcess } from '../repository_acess';
 
 @injectable()
-export class UpdatePostUseCase {
-  constructor(
-    @inject(MODULE.REPOSITORY.PRISMA.POST)
-    private readonly repository: IPostRepository,
-  ) {}
-
+export class UpdatePostUseCase extends PostRepositoryAcess {
   async execute(post: IUpdatePostDTO) {
     return await this.repository.update(post);
   }
