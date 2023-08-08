4079062f86445f12e1998fdb621b68575a8feff1
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-08-08 14:39:50 -0300

    [:white_check_mark:] | create: integration test - repository (post)

--- test/module/repository/prisma/integration/post.repository.spec.ts
+++ test/module/repository/prisma/integration/post.repository.spec.ts
@@ -4,23 +4,47 @@
 
 import { MODULES } from '@modules';
 import { PrismaPostRepository } from '../../../../../src/modules/repository/prisma/post/post.repository';
-import { CREATE_POST_DATA, VALID_POST, VALID_POST_DATA } from '@test/mock/data/post';
+import { CREATE_POST_DATA, VALID_POST } from '@test/mock/data/post';
 
 describe('[REPOSITORY] | Post', () => {
   let repository: PrismaPostRepository;
 
-  beforeEach(() => {
+  beforeEach(async () => {
     repository = MODULES.REPOSITORY.PRISMA.POST();
     expect(repository).toBeInstanceOf(PrismaPostRepository);
   });
 
   it('[INTEGRATION] | Should: Create => [POST]', async () => {
-    
     const result = await repository.create(CREATE_POST_DATA);
 
     expect(result).toHaveProperty('id');
     expect(result.authorId).toEqual(CREATE_POST_DATA.authorId);
     expect(result.body).toEqual(CREATE_POST_DATA.body);
     expect(result.image).toEqual(CREATE_POST_DATA.image);
+  });
+
+  it('[INTEGRATION] | Should: FindAll => [POST]', async () => {
+    const result = await repository.findAll();
+
+    expect(result).toHaveLength(1);
+    expect(result[0]).toHaveProperty('id');
+    expect(result[0].authorId).toEqual(CREATE_POST_DATA.authorId);
+  });
+
+  it('[INTEGRATION] | Should: FindByAuthorId & FindById => [POST]', async () => {
+    const byAuthor = await repository.findByAuhorId({
+      id: CREATE_POST_DATA.authorId,
+    });
+
+    expect(byAuthor).toHaveProperty('id');
+    expect(byAuthor.authorId).toEqual(CREATE_POST_DATA.authorId);
+    expect(byAuthor.body).toEqual(CREATE_POST_DATA.body);
+
+    const result = await repository.findById({ id: byAuthor.id });
+
+    expect(result).toHaveProperty('id');
+    expect(result.id).toEqual(byAuthor.id);
+    expect(result.authorId).toEqual(byAuthor.authorId);
+    expect(result.body).toEqual(byAuthor.body);
   });
 });
