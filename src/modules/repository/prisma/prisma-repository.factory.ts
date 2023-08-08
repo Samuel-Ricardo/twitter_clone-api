43cb0da5471200f450779471c90ffcbca54189b0
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-08-08 13:36:35 -0300

    [:sparkles:] | update: factory - repository > prisma (module)

--- src/modules/repository/prisma/prisma-repository.factory.ts
+++ src/modules/repository/prisma/prisma-repository.factory.ts
@@ -1,3 +1,4 @@
+import { PrismaPostRepository } from './post';
 import { PrismaRepositoryModule } from './prisma-repository.module';
 import { PrismaRepositoryRegistry } from './prisma-repository.registry';
 import { PrismaUserRepository } from './user/user.repository';
@@ -6,5 +7,9 @@ export const PrismaRepositoryFactory = {
   USER: () =>
     PrismaRepositoryModule.get<PrismaUserRepository>(
       PrismaRepositoryRegistry.USER,
+    ),
+  POST: () =>
+    PrismaRepositoryModule.get<PrismaPostRepository>(
+      PrismaRepositoryRegistry.POST,
     ),
 };
