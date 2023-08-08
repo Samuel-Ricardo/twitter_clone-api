8b9db9816ae0f1410fd7f352f03812b8b4ec6039
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-08-08 16:25:26 -0300

    [:sparkles:] | create: factory - post (module)

---
+++ src/modules/@core/post/post.factory.ts
@@ -0,0 +1,33 @@
+import { PostModule } from './post.module';
+import { PostRegistry } from './post.registry';
+import {
+  CreatePostUseCase,
+  UpdatePostUseCase,
+  DetailPostsUseCase,
+  ListUserPostsUseCase,
+  ListPostsUseCase,
+  DeletePostUseCase,
+} from './use-case';
+
+export const PostFactory = {
+  USE_CASE: {
+    CREATE: () =>
+      PostModule.get<CreatePostUseCase>(PostRegistry.USE_CASE.CREATE),
+    UPDATE: () =>
+      PostModule.get<UpdatePostUseCase>(PostRegistry.USE_CASE.UPDATE),
+    DELETE: () =>
+      PostModule.get<DeletePostUseCase>(PostRegistry.USE_CASE.DELETE),
+    FIND: {
+      ALL: () =>
+        PostModule.get<ListPostsUseCase>(PostRegistry.USE_CASE.FIND.ALL),
+      BY: {
+        ID: () =>
+          PostModule.get<DetailPostsUseCase>(PostRegistry.USE_CASE.FIND.BY.ID),
+        AUTHOR: () =>
+          PostModule.get<ListUserPostsUseCase>(
+            PostRegistry.USE_CASE.FIND.BY.AUTHOR,
+          ),
+      },
+    },
+  },
+};
