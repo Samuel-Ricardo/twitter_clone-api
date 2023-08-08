86526e52cd52ef5c6abccca931839188c1c29025
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-08-08 12:17:27 -0300

    [:recycle:] | refact

--- src/modules/repository/prisma/prisma-repository.module.ts
+++ src/modules/repository/prisma/prisma-repository.module.ts
@@ -2,11 +2,13 @@ import { Container } from 'inversify';
 import { PrismaUserRepository } from './user/user.repository';
 import { PrismaRepositoryRegistry } from './prisma-repository.registry';
 import { PrismaModule } from '../../prisma/prisma.module';
+import { PrismaPostRepository } from './post/post.repository';
 
 const Module = new Container({
   autoBindInjectable: true,
 });
 
 Module.bind(PrismaRepositoryRegistry.USER).to(PrismaUserRepository);
+Module.bind(PrismaRepositoryRegistry.POST).to(PrismaPostRepository);
 
 export const PrismaRepositoryModule = Container.merge(Module, PrismaModule);
