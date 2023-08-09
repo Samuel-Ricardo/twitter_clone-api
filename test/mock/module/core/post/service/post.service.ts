640347774d6e24e2f59128a76f77ac5cca0d477d
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-08-09 15:14:27 -0300

    [:clown_face:] | update: service - post (mock/module)

--- test/mock/module/core/post/service/post.service.ts
+++ test/mock/module/core/post/service/post.service.ts
@@ -1,5 +1,5 @@
 import { PostService } from '@Post/service/post.service';
-import { mockDeep } from 'jest-mock-extended';
+import { DeepMockProxy, mockDeep } from 'jest-mock-extended';
 import { interfaces } from 'inversify';
 import { PostMockRegistry } from '../post.registry';
 import {
@@ -10,26 +10,29 @@ import {
   ListUserPostsUseCase,
   UpdatePostUseCase,
 } from '@Post/use-case';
+import { ISimulatePostService } from '@test/@types/simulate/post.service';
 
 export const mockPostService = () => mockDeep<PostService>();
 
-export const simulatePostService = ({ container }: interfaces.Context) => {
-  const create = container.get<CreatePostUseCase>(
+export const simulatePostService = ({
+  container,
+}: interfaces.Context): ISimulatePostService => {
+  const create = container.get<DeepMockProxy<CreatePostUseCase>>(
     PostMockRegistry.USE_CASE.CREATE,
   );
-  const update = container.get<UpdatePostUseCase>(
+  const update = container.get<DeepMockProxy<UpdatePostUseCase>>(
     PostMockRegistry.USE_CASE.UPDATE,
   );
-  const deletePost = container.get<DeletePostUseCase>(
+  const deletePost = container.get<DeepMockProxy<DeletePostUseCase>>(
     PostMockRegistry.USE_CASE.DELETE,
   );
-  const list = container.get<ListPostsUseCase>(
+  const list = container.get<DeepMockProxy<ListPostsUseCase>>(
     PostMockRegistry.USE_CASE.FIND.ALL,
   );
-  const detail = container.get<DetailPostsUseCase>(
+  const detail = container.get<DeepMockProxy<DetailPostsUseCase>>(
     PostMockRegistry.USE_CASE.FIND.BY.ID,
   );
-  const listUserPosts = container.get<ListUserPostsUseCase>(
+  const listUserPosts = container.get<DeepMockProxy<ListUserPostsUseCase>>(
     PostMockRegistry.USE_CASE.FIND.BY.AUTHOR,
   );
 
