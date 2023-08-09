710e2d1708bbcfaf06087a44d70e96a64c754506
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-08-09 09:57:31 -0300

    [:truck:] | create: index - use-case > post (module)

---
+++ src/modules/@core/post/use-case/index.ts
@@ -0,0 +1,6 @@
+export * from './create.use-case';
+export * from './update.use-case';
+export * from './delete.use-case';
+export * from './list_user_posts';
+export * from './list_posts.use-case';
+export * from './post_details.use-case';
