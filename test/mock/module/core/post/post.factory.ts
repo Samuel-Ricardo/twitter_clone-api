a96bbce8c732f7d9f67864a0dd1e124980348bdc
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-08-09 14:18:05 -0300

    [:clown_face:] | update: factory - post (mock/module)

--- test/mock/module/core/post/post.factory.ts
+++ test/mock/module/core/post/post.factory.ts
@@ -1,4 +1,3 @@
-import { PostModule } from '@Post';
 import {
   CreatePostUseCase,
   UpdatePostUseCase,
@@ -9,27 +8,42 @@ import {
 } from '@Post/use-case';
 import { DeepMockProxy } from 'jest-mock-extended';
 import { PostMockRegistry } from './post.registry';
+import { PostService } from '@Post/service/post.service';
+import { PostMockModule } from './post.module';
+import { ISimulatePostService } from '@test/@types/simulate/post.service';
 
 export const PostMockFactory = {
+  SERVICE: {
+    DEFAULT: {
+      MOCK: () =>
+        PostMockModule.get<DeepMockProxy<PostService>>(
+          PostMockRegistry.SERVICE.DEFAULT.MOCK,
+        ),
+      SIMULATE: () =>
+        PostMockModule.get<ISimulatePostService>(
+          PostMockRegistry.SERVICE.DEFAULT.SIMULATE,
+        ),
+    },
+  },
   USE_CASE: {
-    CREATE: PostModule.get<DeepMockProxy<CreatePostUseCase>>(
+    CREATE: PostMockModule.get<DeepMockProxy<CreatePostUseCase>>(
       PostMockRegistry.USE_CASE.CREATE,
     ),
-    UPDATE: PostModule.get<DeepMockProxy<UpdatePostUseCase>>(
+    UPDATE: PostMockModule.get<DeepMockProxy<UpdatePostUseCase>>(
       PostMockRegistry.USE_CASE.UPDATE,
     ),
-    DELETE: PostModule.get<DeepMockProxy<DeletePostUseCase>>(
+    DELETE: PostMockModule.get<DeepMockProxy<DeletePostUseCase>>(
       PostMockRegistry.USE_CASE.DELETE,
     ),
     FIND: {
-      ALL: PostModule.get<DeepMockProxy<ListPostsUseCase>>(
+      ALL: PostMockModule.get<DeepMockProxy<ListPostsUseCase>>(
         PostMockRegistry.USE_CASE.FIND.ALL,
       ),
       BY: {
-        ID: PostModule.get<DeepMockProxy<DetailPostsUseCase>>(
+        ID: PostMockModule.get<DeepMockProxy<DetailPostsUseCase>>(
           PostMockRegistry.USE_CASE.FIND.BY.ID,
         ),
-        AUTHOR: PostModule.get<DeepMockProxy<ListUserPostsUseCase>>(
+        AUTHOR: PostMockModule.get<DeepMockProxy<ListUserPostsUseCase>>(
           PostMockRegistry.USE_CASE.FIND.BY.AUTHOR,
         ),
       },
