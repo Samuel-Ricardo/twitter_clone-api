282f5c0fd7d7e2d0eeb6a2e9ab8df9ee03ae73e5
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-08-09 10:53:51 -0300

    [:clown_face:] | create: registry - post (mock/module)

---
+++ test/mock/module/core/post/post.registry.ts
@@ -0,0 +1,14 @@
+export const PostMockRegistry = {
+  USE_CASE: {
+    CREATE: Symbol('POST.USE_CASE.CREATE'),
+    UPDATE: Symbol('POST.USE_CASE.UPDATE'),
+    DELETE: Symbol('POST.USE_CASE.DELETE'),
+    FIND: {
+      ALL: Symbol('POST.USE_CASE.FIND.ALL'),
+      BY: {
+        ID: Symbol('POST.USE_CASE.FIND.BY.ID'),
+        AUTHOR: Symbol('POST.USE_CASE.FIND.BY.AUTHOR'),
+      },
+    },
+  },
+};
