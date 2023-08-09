2d1ed134f7ed599b9024c8160714106ca9fd082e
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-08-09 15:13:24 -0300

    [:clown_face:] | update: factory - app (mock/module)

--- test/mock/module/app.factory.ts
+++ test/mock/module/app.factory.ts
@@ -2,10 +2,12 @@ import { PrismaMockFactory } from './prisma';
 import { PrismaDevFactory } from './prisma/prisma-dev';
 import { RepositoryMockFactory as REPOSITORY } from './repository/repository.module';
 import { UserMockFactory as USER } from './core/user';
+import { PostMockFactory as POST } from './core/post';
 
 export const MockFactory = {
   ...PrismaMockFactory,
   ...PrismaDevFactory,
   REPOSITORY,
   USER,
+  POST,
 };
