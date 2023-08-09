6cad189859a4b5ef5a49a6edb18c900f85178864
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-08-09 16:53:55 -0300

    [:white_check_mark:] | create: unit test - service > post (module)

--- test/module/core/post/post.controller.spec.ts
+++ test/module/core/post/post.controller.spec.ts
@@ -2,8 +2,10 @@ import { ISimulatePostController } from '@test/@types';
 import { MockFactory } from '@test/mock';
 import {
   CREATE_POST_DATA,
+  UPDATE_POST_DATA,
   VALID_POST,
   VALID_POST_DATA,
+  VALID_UPDATED_POST,
 } from '@test/mock/data/post';
 
 describe('[CONTROLLER] | POST', () => {
@@ -37,5 +39,16 @@ describe('[CONTROLLER] | POST', () => {
 
     expect(module.service.create).toHaveBeenCalledTimes(1);
     expect(module.service.create).toHaveBeenCalledWith(CREATE_POST_DATA);
+  });
+
+  it('[UNIT] | Should: update => [POST]', async () => {
+    module.service.update.mockResolvedValue(VALID_UPDATED_POST);
+
+    const result = await module.controller.update(UPDATE_POST_DATA);
+
+    expect(result).toEqual({ post: VALID_UPDATED_POST });
+
+    expect(module.service.update).toHaveBeenCalledTimes(1);
+    expect(module.service.update).toHaveBeenCalledWith(UPDATE_POST_DATA);
   });
 });
