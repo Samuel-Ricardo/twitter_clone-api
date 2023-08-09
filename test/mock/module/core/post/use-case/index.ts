6b6c30f44a4e3337365e9bbbd47d27dc18a16e5c
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-08-09 10:51:38 -0300

    [:clown_face:] | create: index - use-case > post (mock/module)

---
+++ test/mock/module/core/post/use-case/index.ts
@@ -0,0 +1,6 @@
+export * from './create.use-case';
+export * from './delete.use-case';
+export * from './update.use-case';
+export * from './detail.use-case';
+export * from './list.use-case';
+export * from './list_user_posts.use-case';
