952282d8d67476ae4e8d1498a39d28bb0e84f884
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-08-10 16:50:22 -0300

    [:gear:] | create: scripts - mongo_setup (docker)

---
+++ .docker/mongo/scripts/mongo_setup.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# echo "sleeping for 10 seconds"
+# sleep 10
+
+echo mongo_setup.sh time now: `date +"%T" `
+mongosh --host mongo1:27017 <<EOF
+  var cfg = {
+    "_id": "rs0",
+    "version": 1,
+    "members": [
+      {
+        "_id": 0,
+        "host": "mongo:27017",
+      },
+      {
+        "_id": 1,
+        "host": "host.docker.internal:27017",
+      }    
+    ]
+  };
+  rs.initiate(cfg);
+EOF
+
+sleep 100000
