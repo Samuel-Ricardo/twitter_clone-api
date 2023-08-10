598cf93e40b87fe6ed0a12569fd03bc9ca82b97d
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-08-10 11:49:21 -0300

    [:white_check_mark:] | create: E2E test - post > detail (module)

--- test/E2E/module/post.spec.ts
+++ test/E2E/module/post.spec.ts
@@ -9,15 +9,7 @@ import supertest from 'supertest';
 import { CREATE_POST_DATA, VALID_POST, VALID_USER } from '@test/mock/data/post';
 
 describe('[MODULE] | Post', () => {
-  let createdPost: Post | undefined = undefined;
-
-  beforeEach(async () => {
-    if (createdPost)
-      createdPost = (
-        await supertest(app).get(`${post.prefix}/${createdPost.id}`)
-      ).body.post;
-    console.log({ createdPost }, 'BEFPRRRREEEEEEEE');
-  });
+  let posted: any;
 
   it('[E2E] | Should: Create => [POST]', async () => {
     const response = await supertest(app)
@@ -30,7 +22,7 @@ describe('[MODULE] | Post', () => {
     expect(body.post._authorId).toBe(CREATE_POST_DATA.authorId);
     expect(body.post._body).toBe(CREATE_POST_DATA.body);
 
-    createdPost = body.post;
+    posted = body.post;
   });
 
   it('[E2E] | Should: list [all] => [POST]', async () => {
@@ -49,8 +41,6 @@ describe('[MODULE] | Post', () => {
       .send(CREATE_POST_DATA);
     const createBody: { post: any } = createResponse.body;
 
-    console.log({ createdPost }, 'AUTHOOOOOOOOOOOOOOOOOOOOR');
-
     expect(createResponse.status).toBe(201);
     expect(createBody.post).toHaveProperty('_id');
     expect(createBody.post._authorId).toBe(CREATE_POST_DATA.authorId);
@@ -66,5 +56,14 @@ describe('[MODULE] | Post', () => {
     expect(body.posts.length).toBeGreaterThanOrEqual(1);
     expect(body.posts[0]._authorId).toBe(VALID_POST.authorId);
     expect(body.posts[0]._authorId).toBe(VALID_USER.id);
+  });
+
+  it('[E2E] | Should: detail => [POST]', async () => {
+    const response = await supertest(app).get(post.prefix + '/' + posted._id);
+    const body: { post: any } = response.body;
+
+    expect(response.status).toBe(200);
+    expect(body.post).toHaveProperty('_id');
+    expect(body.post._id).toBe(posted._id);
   });
 });
