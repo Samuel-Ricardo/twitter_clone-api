111fafbf39e20d5a20064efe44d83a2c3f8f1a6e
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-08-11 11:17:32 -0300

    [:sparkles:] | create: module - like (core)

---
+++ src/modules/@core/like/like.module.ts
@@ -0,0 +1,17 @@
+import { Container } from 'inversify';
+import { LikeRegistry } from './like.registry';
+import { CreateLikeUseCase } from './use-case/create.use-case';
+import { DeleteLikeUseCase } from './use-case/delete.use-case';
+import { GetPostLikesUseCase } from './use-case/get_post_likes.use-case';
+import { GetUserLikesUseCase } from './use-case/get_user_likes.use-case';
+import { GetCommentLikesUseCase } from './use-case/get_comment_likes.use-case';
+
+export const LikeModule = new Container({ autoBindInjectable: true });
+
+LikeModule.bind(LikeRegistry.USE_CASE.CREATE).to(CreateLikeUseCase);
+LikeModule.bind(LikeRegistry.USE_CASE.DELETE).to(DeleteLikeUseCase);
+LikeModule.bind(LikeRegistry.USE_CASE.GET.BY.POST).to(GetPostLikesUseCase);
+LikeModule.bind(LikeRegistry.USE_CASE.GET.BY.USER).to(GetUserLikesUseCase);
+LikeModule.bind(LikeRegistry.USE_CASE.GET.BY.COMMENT).to(
+  GetCommentLikesUseCase,
+);
