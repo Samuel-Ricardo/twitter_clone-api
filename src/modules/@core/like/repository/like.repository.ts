0219e2b4280bb32d524214d5e92e00e6d5f4531f
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-08-11 01:32:53 -0300

    [:card_file_box:] | create: repository - like (module)

---
+++ src/modules/@core/like/repository/like.repository.ts
@@ -0,0 +1,12 @@
+import { ICreateLikeDTO } from '../DTO/create.dto';
+import { IDeleteLikeDTO } from '../DTO/delete.dto';
+import { IGetLikesOfPostDTO } from '../DTO/get_by_post.dto';
+import { IGetLikesOfUserDTO } from '../DTO/get_by_user.dto';
+import { Like } from '../entity/like.entity';
+
+export interface ILikeRepository {
+  create(data: ICreateLikeDTO): Promise<Like>;
+  delete(data: IDeleteLikeDTO): Promise<void>;
+  getLikesOfPost(data: IGetLikesOfPostDTO): Promise<Like[]>;
+  getLikesOfUser(data: IGetLikesOfUserDTO): Promise<Like[]>;
+}
