cc20e957f3cd0bb6bbc0f256c39fc37424f3b095
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-08-11 17:02:18 -0300

    [:sparkles:] | create: controller - like > dislike (module)

--- src/modules/@core/like/controller/like.controller.ts
+++ src/modules/@core/like/controller/like.controller.ts
@@ -2,6 +2,7 @@ import { inject, injectable } from 'inversify';
 import { LikeService } from '../service/like.service';
 import { LikeRegistry } from '../like.registry';
 import { ICreateLikeDTO } from '../DTO/create.dto';
+import { IDeleteLikeDTO } from '../DTO/delete.dto';
 
 @injectable()
 export class LikeController {
@@ -12,5 +13,9 @@ export class LikeController {
 
   async save(data: ICreateLikeDTO) {
     return await this.service.like(data);
+  }
+
+  async dislike(data: IDeleteLikeDTO) {
+    return await this.service.dislike(data);
   }
 }
