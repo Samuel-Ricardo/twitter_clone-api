226c3de0f7210af72081e5b8bbd2d12c9ffc8fca
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-08-11 01:27:13 -0300

    [:safety_vest:] | create: schema - get of user > like (module)

--- src/modules/@core/like/DTO/get_by_user.dto.ts
+++ src/modules/@core/like/DTO/get_by_user.dto.ts
@@ -4,3 +4,9 @@ import { z } from 'zod';
 export interface IGetLikesOfUserDTO {
   id: string;
 }
+
+export const GetLikesOfUserSchema = z
+  .object({
+    id: z.string().catch(catchZod),
+  })
+  .catch(catchZod);
