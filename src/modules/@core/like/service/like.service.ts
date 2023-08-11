a51142150eb4be223e4a8f4dcbb6d2133c4937a1
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-08-11 11:40:34 -0300

    [:sparkles:] | create: service - like > unlike (module)

--- src/modules/@core/like/service/like.service.ts
+++ src/modules/@core/like/service/like.service.ts
@@ -5,18 +5,24 @@ import { GetPostLikesUseCase } from '../use-case/get_post_likes.use-case';
 import { GetUserLikesUseCase } from '../use-case/get_user_likes.use-case';
 import { GetCommentLikesUseCase } from '../use-case/get_comment_likes.use-case';
 import { ICreateLikeDTO } from '../DTO/create.dto';
+import { IDeleteLikeDTO } from '../DTO/delete.dto';
+import { IGetLikesOfPostDTO } from '../DTO/get_by_post.dto';
 
 @injectable()
 export class LikeService {
   constructor(
     private readonly giveLike: CreateLikeUseCase,
-    private readonly unlike: DeleteLikeUseCase,
+    private readonly giveUnlike: DeleteLikeUseCase,
     private readonly getPostLikes: GetPostLikesUseCase,
     private readonly getUserLikes: GetUserLikesUseCase,
     private readonly getCommentLikes: GetCommentLikesUseCase,
   ) {}
 
-  async create(data: ICreateLikeDTO) {
+  async like(data: ICreateLikeDTO) {
     await this.giveLike.execute(data);
+  }
+
+  async unlike(data: IDeleteLikeDTO) {
+    await this.giveUnlike.execute(data);
   }
 }
