f9236f79954fb027ecbb02a4492670e77b0f98dd
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-08-11 10:58:35 -0300

    [:safety_vest:] | create: schema - get of comment > like (module)

--- src/modules/@core/like/DTO/get_by_comment.dto.ts
+++ src/modules/@core/like/DTO/get_by_comment.dto.ts
@@ -1,3 +1,12 @@
+import { catchZod } from '@modules/util/validator';
+import { z } from 'zod';
+
 export interface IGetLikesOfCommentDTO {
   likedId: string;
 }
+
+export const GetLikesOfCommentSchema = z
+  .object({
+    likedId: z.string().catch(catchZod),
+  })
+  .catch(catchZod);
