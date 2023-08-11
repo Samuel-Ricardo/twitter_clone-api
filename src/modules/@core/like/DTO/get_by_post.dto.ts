aac53d63c78d2d199745ff4a3bf4989320691741
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-08-11 01:18:54 -0300

    [:safety_vest:] | create: schema - get of post > like (module)

--- src/modules/@core/like/DTO/get_by_post.dto.ts
+++ src/modules/@core/like/DTO/get_by_post.dto.ts
@@ -1,3 +1,12 @@
+import { catchZod } from '@modules/util/validator';
+import { z } from 'zod';
+
 export interface IGetLikesOfPost {
   id: string;
 }
+
+export const GetLikesOfPostSchema = z
+  .object({
+    id: z.string().catch(catchZod),
+  })
+  .catch(catchZod);
