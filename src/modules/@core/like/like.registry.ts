a2a424d6d455d1e38a8c8050dc32208baf364206
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-08-11 11:53:30 -0300

    [:sparkles:] | update: registry - like (module)

--- src/modules/@core/like/like.registry.ts
+++ src/modules/@core/like/like.registry.ts
@@ -2,7 +2,9 @@ export const LikeRegistry = {
   SERVICE: {
     DEFAULT: Symbol('LIKE.SERVICE.DEFAULT'),
   },
-
+  CONTROLLER: {
+    DEFAULT: Symbol('LIKE.CONTROLLER.DEFAULT'),
+  },
   USE_CASE: {
     CREATE: Symbol('LIKE.USE_CASE.CREATE'),
     DELETE: Symbol('LIKE.USE_CASE.DELETE'),
