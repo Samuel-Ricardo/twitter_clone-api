a9c352e431ff6fc767d5cc3610e1b03994ec991e
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-08-11 16:51:57 -0300

    [:sparkles:] | create: controller - like > save (module)

--- src/modules/@core/like/controller/like.controller.ts
+++ src/modules/@core/like/controller/like.controller.ts
@@ -1,6 +1,7 @@
 import { inject, injectable } from 'inversify';
 import { LikeService } from '../service/like.service';
 import { LikeRegistry } from '../like.registry';
+import { ICreateLikeDTO } from '../DTO/create.dto';
 
 @injectable()
 export class LikeController {
@@ -8,4 +9,8 @@ export class LikeController {
     @inject(LikeRegistry.SERVICE.DEFAULT)
     private readonly service: LikeService,
   ) {}
+
+  async save(data: ICreateLikeDTO) {
+    return await this.service.like(data);
+  }
 }
