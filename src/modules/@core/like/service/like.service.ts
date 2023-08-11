401e6001e6db84352c1980658fe65e8a59945b11
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-08-11 11:36:42 -0300

    [:sparkles:] | create: service - like (module)

---
+++ src/modules/@core/like/service/like.service.ts
@@ -0,0 +1,18 @@
+import { injectable } from 'inversify';
+import { CreateLikeUseCase } from '../use-case/create.use-case';
+import { DeleteLikeUseCase } from '../use-case/delete.use-case';
+import { GetPostLikesUseCase } from '../use-case/get_post_likes.use-case';
+import { GetUserLikesUseCase } from '../use-case/get_user_likes.use-case';
+import { GetCommentLikesUseCase } from '../use-case/get_comment_likes.use-case';
+import { ICreateLikeDTO } from '../DTO/create.dto';
+
+@injectable()
+export class LikeService {
+  constructor(
+    private readonly giveLike: CreateLikeUseCase,
+    private readonly unlike: DeleteLikeUseCase,
+    private readonly getPostLikes: GetPostLikesUseCase,
+    private readonly getUserLikes: GetUserLikesUseCase,
+    private readonly getCommentLikes: GetCommentLikesUseCase,
+  ) {}
+}
