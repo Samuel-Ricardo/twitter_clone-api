ff91cc11ba03fca35a29f45f9f0b093f300208c0
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-08-11 01:08:46 -0300

    [:safety_vest:] | create: schema - delete > like (module)

--- src/modules/@core/like/DTO/delete.dto.ts
+++ src/modules/@core/like/DTO/delete.dto.ts
@@ -1,3 +1,12 @@
+import { catchZod } from '@modules/util/validator';
+import { z } from 'zod';
+
 export interface IDeleteLikeDTO {
   id: string;
 }
+
+export const DeleteLikeSchema = z
+  .object({
+    id: z.string().catch(catchZod),
+  })
+  .catch(catchZod);
