ead5013be1f9437307d8b38c90b582c933dbc5e9
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-08-11 00:55:09 -0300

    [:safety_vest:] | create: schema - like (module)

--- src/modules/@core/like/DTO/like.dto.ts
+++ src/modules/@core/like/DTO/like.dto.ts
@@ -1,6 +1,18 @@
+import { catchZod } from '@modules/util/validator';
+import { z } from 'zod';
+
 export interface ILikeDTO {
   id: string;
   userId: string;
   likedId: string;
   createdAt: Date;
 }
+
+export const LikeSchema = z
+  .object({
+    id: z.string().catch(catchZod),
+    userId: z.string().catch(catchZod),
+    likedId: z.string().catch(catchZod),
+    createdAt: z.date().catch(catchZod),
+  })
+  .catch(catchZod);
