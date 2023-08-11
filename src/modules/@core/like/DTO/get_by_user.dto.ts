c00bf243a68dc7b584a685fb4c4eb7001e95faa1
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-08-11 10:47:30 -0300

    [:recycle:] | refact: rename - DTO

--- src/modules/@core/like/DTO/get_by_user.dto.ts
+++ src/modules/@core/like/DTO/get_by_user.dto.ts
@@ -2,11 +2,11 @@ import { catchZod } from '@modules/util/validator';
 import { z } from 'zod';
 
 export interface IGetLikesOfUserDTO {
-  id: string;
+  userID: string;
 }
 
 export const GetLikesOfUserSchema = z
   .object({
-    id: z.string().catch(catchZod),
+    userID: z.string().catch(catchZod),
   })
   .catch(catchZod);
