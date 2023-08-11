4da1866075d237ac81d32b1a9dc5d8a63cb2020d
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-08-11 11:28:55 -0300

    [:sparkles:] | create: factory - like (module)

---
+++ src/modules/@core/like/like.factory.ts
@@ -0,0 +1,32 @@
+import { LikeModule } from './like.module';
+import { LikeRegistry } from './like.registry';
+import { CreateLikeUseCase } from './use-case/create.use-case';
+import { DeleteLikeUseCase } from './use-case/delete.use-case';
+import { GetCommentLikesUseCase } from './use-case/get_comment_likes.use-case';
+import { GetPostLikesUseCase } from './use-case/get_post_likes.use-case';
+import { GetUserLikesUseCase } from './use-case/get_user_likes.use-case';
+
+export const LikeFactory = {
+  USE_CASE: {
+    CREATE: () =>
+      LikeModule.get<CreateLikeUseCase>(LikeRegistry.USE_CASE.CREATE),
+    DELETE: () =>
+      LikeModule.get<DeleteLikeUseCase>(LikeRegistry.USE_CASE.DELETE),
+    GET: {
+      BY: {
+        POST: () =>
+          LikeModule.get<GetPostLikesUseCase>(
+            LikeRegistry.USE_CASE.GET.BY.POST,
+          ),
+        USER: () =>
+          LikeModule.get<GetUserLikesUseCase>(
+            LikeRegistry.USE_CASE.GET.BY.USER,
+          ),
+        COMMENT: () =>
+          LikeModule.get<GetCommentLikesUseCase>(
+            LikeRegistry.USE_CASE.GET.BY.COMMENT,
+          ),
+      },
+    },
+  },
+};
