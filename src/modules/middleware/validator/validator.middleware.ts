c562e45a0d9105d4830c19327b0b2177f5ae2d75
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-08-12 00:32:28 -0300

    [:safety_vest:] | fix: middleware - validator (module)

--- src/modules/middleware/validator/validator.middleware.ts
+++ src/modules/middleware/validator/validator.middleware.ts
@@ -2,10 +2,13 @@ import { z } from 'zod';
 import { Request, Response, NextFunction } from 'express';
 
 export const validate =
-  (schema: z.ZodAny) => (req: Request, res: Response, next: NextFunction) => {
+  (schema: z.ZodSchema) =>
+  (req: Request, res: Response, next: NextFunction) => {
     try {
-      return schema.parse(req.body);
+      schema.parse(req.body);
     } catch (e) {
       return next(e);
+    } finally {
+      next();
     }
   };
