546f3b3aed3e3e17e60008e10790904a5e37d544
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-08-12 12:42:42 -0300

    [:card_file_box:] | create: repository - prisma > like :> create (module)

--- src/modules/repository/prisma/like/like.repository.ts
+++ src/modules/repository/prisma/like/like.repository.ts
@@ -1,3 +1,9 @@
+import { ICreateLikeDTO } from '@Core/like/DTO/create.dto';
+import { IDeleteLikeDTO } from '@Core/like/DTO/delete.dto';
+import { IGetLikesOfCommentDTO } from '@Core/like/DTO/get_by_comment.dto';
+import { IGetLikesOfPostDTO } from '@Core/like/DTO/get_by_post.dto';
+import { IGetLikesOfUserDTO } from '@Core/like/DTO/get_by_user.dto';
+import { Like } from '@Core/like/entity/like.entity';
 import { ILikeRepository } from '@Core/like/repository/like.repository';
 import { MODULE } from '@modules';
 import { PrismaClient } from '@prisma/client';
@@ -9,4 +15,21 @@ export class PrismaLikeRepository implements ILikeRepository {
     @inject(MODULE.PRISMA)
     private readonly prisma: PrismaClient,
   ) {}
+
+  async create(data: ICreateLikeDTO) {
+    return Like.fromPrisma(await this.prisma.like.create({ data }));
+  }
+
+  delete(data: IDeleteLikeDTO): Promise<void> {
+    throw new Error('Method not implemented.');
+  }
+  getLikesOfPost(data: IGetLikesOfPostDTO): Promise<Like[]> {
+    throw new Error('Method not implemented.');
+  }
+  getLikesOfUser(data: IGetLikesOfUserDTO): Promise<Like[]> {
+    throw new Error('Method not implemented.');
+  }
+  getLikesOfComment(data: IGetLikesOfCommentDTO): Promise<Like[]> {
+    throw new Error('Method not implemented.');
+  }
 }
