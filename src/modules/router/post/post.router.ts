3444f951f008d09daea1aba3f19c4b1ce9da1f27
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-08-12 00:34:33 -0300

    [:safety_vest:] | apply: middleware - validator > post (module)

--- src/modules/router/post/post.router.ts
+++ src/modules/router/post/post.router.ts
@@ -1,5 +1,13 @@
+import {
+  CreatePostSchema,
+  DeletePostSchema,
+  FindPostByAuthorIdSchema,
+  FindPostByIdSchema,
+  UpdatePostSchema,
+} from '@Post';
 import { MODULES } from '../../app.factory';
 import express from 'express';
+import { validate } from '../../middleware/validator/validator.middleware';
 
 const Post = MODULES.POST.CONTROLLER.DEFAULT();
 
@@ -14,7 +22,7 @@ router.get(prefix, async (req, res, next) => {
   }
 });
 
-router.post(prefix, async (req, res, next) => {
+router.post(prefix, validate(CreatePostSchema), async (req, res, next) => {
   try {
     return res.status(201).json(await Post.create(req.body));
   } catch (e) {
@@ -22,15 +30,19 @@ router.post(prefix, async (req, res, next) => {
   }
 });
 
-router.get(`${prefix}/:id`, async (req, res, next) => {
-  try {
-    return res.status(200).json(await Post.details({ id: req.params.id }));
-  } catch (e) {
-    return next(e);
-  }
-});
+router.get(
+  `${prefix}/:id`,
+  validate(FindPostByIdSchema),
+  async (req, res, next) => {
+    try {
+      return res.status(200).json(await Post.details({ id: req.params.id }));
+    } catch (e) {
+      return next(e);
+    }
+  },
+);
 
-router.patch(prefix, async (req, res, next) => {
+router.patch(prefix, validate(UpdatePostSchema), async (req, res, next) => {
   try {
     return res.status(201).json(await Post.update(req.body));
   } catch (e) {
@@ -38,22 +50,30 @@ router.patch(prefix, async (req, res, next) => {
   }
 });
 
-router.delete(`${prefix}/:id`, async (req, res, next) => {
-  try {
-    return res.status(204).json(await Post.delete({ id: req.params.id }));
-  } catch (e) {
-    return next(e);
-  }
-});
+router.delete(
+  `${prefix}/:id`,
+  validate(DeletePostSchema),
+  async (req, res, next) => {
+    try {
+      return res.status(204).json(await Post.delete({ id: req.params.id }));
+    } catch (e) {
+      return next(e);
+    }
+  },
+);
 
-router.get(`${prefix}/author/:id`, async (req, res, next) => {
-  try {
-    return res
-      .status(200)
-      .json(await Post.listUserPosts({ id: req.params.id }));
-  } catch (e) {
-    return next(e);
-  }
-});
+router.get(
+  `${prefix}/author/:id`,
+  validate(FindPostByAuthorIdSchema),
+  async (req, res, next) => {
+    try {
+      return res
+        .status(200)
+        .json(await Post.listUserPosts({ id: req.params.id }));
+    } catch (e) {
+      return next(e);
+    }
+  },
+);
 
 export const post = { router, prefix };
