9c42a22adb1ff8d02927dfdf818aa34ba615538c
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-08-12 01:07:22 -0300

    [:truck:] | create: router - like > get (module)

--- src/modules/router/like/like.router.ts
+++ src/modules/router/like/like.router.ts
@@ -1,4 +1,6 @@
 import { CreateLikeSchema } from '@Core/like/DTO/create.dto';
+import { DeleteLikeSchema } from '@Core/like/DTO/delete.dto';
+import { GetLikesOfPostSchema } from '@Core/like/DTO/get_by_post.dto';
 import { MODULES } from '@modules/app.factory';
 import { validate } from '@modules/middleware/validator';
 import { Router } from 'express';
@@ -14,5 +16,25 @@ router.post(prefix, validate(CreateLikeSchema), (req, res, next) => {
     next(error);
   }
 });
+
+router.delete(`${prefix}/:id`, validate(DeleteLikeSchema), (req, res, next) => {
+  try {
+    res.status(204).json(Like.dislike({ id: req.params.id }));
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.get(
+  `${prefix}/:likedId`,
+  validate(GetLikesOfPostSchema),
+  (req, res, next) => {
+    try {
+      res.status(200).json(Like.getPostLikes({ likedId: req.params.likedId }));
+    } catch (error) {
+      next(error);
+    }
+  },
+);
 
 export const like = { router, prefix };
