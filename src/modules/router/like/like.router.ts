21875f32cda9e1ab96c3d5b90e7aac52d6060e5f
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-08-12 01:09:54 -0300

    [:truck:] | create: router - like > get (module)

--- src/modules/router/like/like.router.ts
+++ src/modules/router/like/like.router.ts
@@ -1,6 +1,7 @@
 import { CreateLikeSchema } from '@Core/like/DTO/create.dto';
 import { DeleteLikeSchema } from '@Core/like/DTO/delete.dto';
 import { GetLikesOfPostSchema } from '@Core/like/DTO/get_by_post.dto';
+import { GetLikesOfUserSchema } from '@Core/like/DTO/get_by_user.dto';
 import { MODULES } from '@modules/app.factory';
 import { validate } from '@modules/middleware/validator';
 import { Router } from 'express';
@@ -9,28 +10,46 @@ const router = Router();
 const prefix = '/like';
 const Like = MODULES.LIKE.DEFAULT();
 
-router.post(prefix, validate(CreateLikeSchema), (req, res, next) => {
+router.post(prefix, validate(CreateLikeSchema), async (req, res, next) => {
   try {
-    res.status(201).json(Like.save(req.body));
+    res.status(201).json(await Like.save(req.body));
   } catch (error) {
     next(error);
   }
 });
 
-router.delete(`${prefix}/:id`, validate(DeleteLikeSchema), (req, res, next) => {
-  try {
-    res.status(204).json(Like.dislike({ id: req.params.id }));
-  } catch (error) {
-    next(error);
-  }
-});
+router.delete(
+  `${prefix}/:id`,
+  validate(DeleteLikeSchema),
+  async (req, res, next) => {
+    try {
+      res.status(204).json(await Like.dislike({ id: req.params.id }));
+    } catch (error) {
+      next(error);
+    }
+  },
+);
 
 router.get(
   `${prefix}/:likedId`,
   validate(GetLikesOfPostSchema),
-  (req, res, next) => {
+  async (req, res, next) => {
     try {
       res.status(200).json(Like.getPostLikes({ likedId: req.params.likedId }));
+    } catch (error) {
+      next(error);
+    }
+  },
+);
+
+router.get(
+  `${prefix}/:userId`,
+  validate(GetLikesOfUserSchema),
+  async (req, res, next) => {
+    try {
+      res
+        .status(200)
+        .json(await Like.getUserLikes({ userId: req.params.userId }));
     } catch (error) {
       next(error);
     }
