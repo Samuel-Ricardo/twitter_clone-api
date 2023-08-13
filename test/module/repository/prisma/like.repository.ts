b2656ad6eff4bd9409bfc7746b7420a070c8105c
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-08-13 12:22:17 -0300

    [:white_check_mark:] | create: unit test - repository > like =:> create (prisma)

--- test/module/repository/prisma/like.repository.ts
+++ test/module/repository/prisma/like.repository.ts
@@ -1,5 +1,10 @@
 import { ISimulatePrismaLikeRepository } from '@test/@types/simulate/like/repository';
 import { MockFactory } from '@test/mock';
+import {
+  CREATE_POST_LIKE_DATA,
+  VALID_POST_LIKE,
+  VALID_POST_LIKE_DATA,
+} from '@test/mock/data/like';
 
 describe('[REPOSITORY] | LIKE', () => {
   let module: ISimulatePrismaLikeRepository;
@@ -12,5 +17,17 @@ describe('[REPOSITORY] | LIKE', () => {
     expect(module).toBeDefined();
     expect(module.repository).toBeDefined();
     expect(module.prisma).toBeDefined();
+  });
+
+  it('[UNIT] | Should: create => [LIKE]', async () => {
+    module.prisma.like.create.mockResolvedValue(VALID_POST_LIKE_DATA);
+
+    const result = await module.repository.create(CREATE_POST_LIKE_DATA);
+
+    expect(result).toEqual(VALID_POST_LIKE);
+    expect(module.prisma.like.create).toHaveBeenCalledTimes(1);
+    expect(module.prisma.like.create).toHaveBeenCalledWith({
+      data: CREATE_POST_LIKE_DATA,
+    });
   });
 });
