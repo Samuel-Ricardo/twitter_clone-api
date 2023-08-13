d5bee2d3d1e75ee5ff6e7cd48554a41e2cfd45fa
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-08-12 22:44:58 -0300

    [:sparkles:] | update: use-case - like (module)

--- src/modules/@core/like/use-case/get_comment_likes.use-case.ts
+++ src/modules/@core/like/use-case/get_comment_likes.use-case.ts
@@ -1,6 +1,8 @@
+import { injectable } from 'inversify';
 import { IGetLikesOfCommentDTO } from '../DTO/get_by_comment.dto';
 import { LikeRepositoryAccess } from '../repository/repository_access';
 
+@injectable()
 export class GetCommentLikesUseCase extends LikeRepositoryAccess {
   async execute(data: IGetLikesOfCommentDTO) {
     return await this.repository.getLikesOfComment(data);
