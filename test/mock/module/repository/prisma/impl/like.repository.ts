95c87d2be0badeddf3996670c3031b488b0c3c4b
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-08-13 11:01:48 -0300

    [:clown_face:] | simulate: repository - like (prisma)

--- test/mock/module/repository/prisma/impl/like.repository.ts
+++ test/mock/module/repository/prisma/impl/like.repository.ts
@@ -1,4 +1,11 @@
 import { PrismaLikeRepository } from '@modules/repository/prisma/like';
+import { MOCK_MODULE } from '@test/mock/module/app.registry';
+import { interfaces } from 'inversify';
 import { mockDeep } from 'jest-mock-extended';
 
 export const mockPrismaLikeRepository = () => mockDeep<PrismaLikeRepository>();
+
+export const simulatePrismaLikeRepository = ({
+  container,
+}: interfaces.Context) =>
+  new PrismaLikeRepository(container.get(MOCK_MODULE.PRISMA));
