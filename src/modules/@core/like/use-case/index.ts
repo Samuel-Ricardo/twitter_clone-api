533c683286bf86772767900c4024ef495cdf8eeb
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-08-13 10:27:54 -0300

    [:truck:] | create: index - like > use-case (module)

---
+++ src/modules/@core/like/use-case/index.ts
@@ -0,0 +1,5 @@
+export * from './create.use-case';
+export * from './delete.use-case';
+export * from './get_post_likes.use-case';
+export * from './get_user_likes.use-case';
+export * from './get_comment_likes.use-case';
