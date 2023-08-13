9d0bc995e8472fde14f8cdc468b59f6359d4d35e
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-08-13 10:32:07 -0300

    [:truck:] | create: index - like > DTO (module)

---
+++ src/modules/@core/like/DTO/index.ts
@@ -0,0 +1,6 @@
+export * from './like.dto';
+export * from './create.dto';
+export * from './delete.dto';
+export * from './get_by_post.dto';
+export * from './get_by_user.dto';
+export * from './get_by_comment.dto';
