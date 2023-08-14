1875ff31fb3276ebb80ee79074b5dfc300ab0b77
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-08-14 12:31:28 -0300

    [:clown_face:] | mock: controller - like (mock/module)

---
+++ test/mock/module/core/like/controller/like.controller.ts
@@ -0,0 +1,4 @@
+import { LikeController } from '../../../../../../src/modules/@core/like';
+import { mockDeep } from 'jest-mock-extended';
+
+export const mockLikeController = () => mockDeep<LikeController>();
