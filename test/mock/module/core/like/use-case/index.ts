0bcf407a18d66c26157c0620fb81c33fd3b0f6bf
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-08-14 11:11:11 -0300

    [:clown_face:] | create: index - like > use-case (core)

---
+++ test/mock/module/core/like/use-case/index.ts
@@ -0,0 +1,5 @@
+export * from './create.use-case';
+export * from './delete.use-case';
+export * from './get_like_of_post.use-case';
+export * from './get_likes_of_comment.use-case';
+export * from './get_like_of_user.use-case';
