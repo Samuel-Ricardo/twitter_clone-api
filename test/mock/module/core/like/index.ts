4095597bf1806f713a5518d76e1d5e25aa044134
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-08-14 13:05:26 -0300

    [:clown_face:] | create: index - like (mock/core)

---
+++ test/mock/module/core/like/index.ts
@@ -0,0 +1,6 @@
+export * from './use-case';
+export * from './service';
+export * from './controller';
+export * from './like.registry';
+export * from './like.module';
+export * from './like.factory';
