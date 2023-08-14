0f723a94e953c9b6ac76e98736ad955707ae4598
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-08-14 11:20:00 -0300

    [:clown_face:] | update: registry - like (mock/module)

--- test/mock/module/core/like/like.registry.ts
+++ test/mock/module/core/like/like.registry.ts
@@ -1,11 +1,13 @@
-export const LikeRegistry = {
-  CREATE: Symbol.for('MOCK:LIKE.USE_CASE.CREATE'),
-  DELETE: Symbol.for('MOCK:LIKE.USE_CASE.DELETE'),
-  GET: {
-    BY: {
-      POST: Symbol.for('MOCK:LIKE.USE_CASE.GET.LIKE.OF.POST'),
-      COMMENT: Symbol.for('MOCK:LIKE.USE_CASE.GET.LIKE.OF.COMMENT'),
-      USER: Symbol.for('MOCK:LIKE.USE_CASE.GET.LIKE.OF.USER'),
+export const MockLikeRegistry = {
+  USE_CASE: {
+    CREATE: Symbol.for('MOCK:LIKE.USE_CASE.CREATE'),
+    DELETE: Symbol.for('MOCK:LIKE.USE_CASE.DELETE'),
+    GET: {
+      BY: {
+        POST: Symbol.for('MOCK:LIKE.USE_CASE.GET.LIKE.OF.POST'),
+        COMMENT: Symbol.for('MOCK:LIKE.USE_CASE.GET.LIKE.OF.COMMENT'),
+        USER: Symbol.for('MOCK:LIKE.USE_CASE.GET.LIKE.OF.USER'),
+      },
     },
   },
 };
