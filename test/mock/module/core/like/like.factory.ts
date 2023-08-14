b6115e3a9307c0524c02bae1a58ba754c8c2e85f
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-08-14 12:57:44 -0300

    [:clown_face:] | updateate: factory - like (mock/module)

--- test/mock/module/core/like/like.factory.ts
+++ test/mock/module/core/like/like.factory.ts
@@ -1,31 +1,52 @@
+import { DeepMockProxy } from 'jest-mock-extended';
 import {
   CreateLikeUseCase,
   DeleteLikeUseCase,
   GetCommentLikesUseCase,
   GetPostLikesUseCase,
   GetUserLikesUseCase,
+  LikeController,
+  LikeService,
 } from '../../../../../src/modules/@core/like';
 import { MockLikeModule } from './like.module';
 import { MockLikeRegistry } from './like.registry';
+import { ISimulateLikeService } from '@test/@types/simulate/like';
+import { ISimulateLikeController } from '@test/@types/simulate/like/controller';
 
 export const MockLikeFactory = {
+  SERVICE: () =>
+    MockLikeModule.get<DeepMockProxy<LikeService>>(MockLikeRegistry.SERVICE),
+  SERVICE_DEV: () =>
+    MockLikeModule.get<ISimulateLikeService>(MockLikeRegistry.SERVICE_DEV),
+  CONTROLLER: () =>
+    MockLikeModule.get<DeepMockProxy<LikeController>>(
+      MockLikeRegistry.CONTROLLER,
+    ),
+  CONTROLLER_DEV: () =>
+    MockLikeModule.get<ISimulateLikeController>(
+      MockLikeRegistry.CONTROLLER_DEV,
+    ),
   USE_CASE: {
     CREATE: () =>
-      MockLikeModule.get<CreateLikeUseCase>(MockLikeRegistry.USE_CASE.CREATE),
+      MockLikeModule.get<DeepMockProxy<CreateLikeUseCase>>(
+        MockLikeRegistry.USE_CASE.CREATE,
+      ),
     DELETE: () =>
-      MockLikeModule.get<DeleteLikeUseCase>(MockLikeRegistry.USE_CASE.DELETE),
+      MockLikeModule.get<DeepMockProxy<DeleteLikeUseCase>>(
+        MockLikeRegistry.USE_CASE.DELETE,
+      ),
     GET: {
       BY: {
         POST: () =>
-          MockLikeModule.get<GetPostLikesUseCase>(
+          MockLikeModule.get<DeepMockProxy<GetPostLikesUseCase>>(
             MockLikeRegistry.USE_CASE.GET.BY.POST,
           ),
         USER: () =>
-          MockLikeModule.get<GetUserLikesUseCase>(
+          MockLikeModule.get<DeepMockProxy<GetUserLikesUseCase>>(
             MockLikeRegistry.USE_CASE.GET.BY.USER,
           ),
         COMMENT: () =>
-          MockLikeModule.get<GetCommentLikesUseCase>(
+          MockLikeModule.get<DeepMockProxy<GetCommentLikesUseCase>>(
             MockLikeRegistry.USE_CASE.GET.BY.COMMENT,
           ),
       },
