3154c83a4d9e5f6d44adca4f90f81997bc8b8c33
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-08-14 11:13:49 -0300

    [:clown_face:] | create: registry - like (mock/module)

---
+++ test/mock/module/core/like/like.registry.ts
@@ -0,0 +1,11 @@
+export const LikeRegistry = {
+  CREATE: Symbol.for('MOCK:LIKE.USE_CASE.CREATE'),
+  DELETE: Symbol.for('MOCK:LIKE.USE_CASE.DELETE'),
+  GET: {
+    BY: {
+      POST: Symbol.for('MOCK:LIKE.USE_CASE.GET.LIKE.OF.POST'),
+      COMMENT: Symbol.for('MOCK:LIKE.USE_CASE.GET.LIKE.OF.COMMENT'),
+      USER: Symbol.for('MOCK:LIKE.USE_CASE.GET.LIKE.OF.USER'),
+    },
+  },
+};
