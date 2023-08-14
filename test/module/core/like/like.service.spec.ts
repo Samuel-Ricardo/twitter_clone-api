06041f0f451c48eb1ade661a48f0ea40d044dd41
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-08-14 13:27:24 -0300

    [:white_check_mark:] | create: test - service > like =:> delete (core)

--- test/module/core/like/like.service.spec.ts
+++ test/module/core/like/like.service.spec.ts
@@ -1,6 +1,7 @@
 import { ISimulateLikeService } from '@test/@types/simulate/like';
 import { MockFactory } from '@test/mock';
 import { VALID_POST_LIKE, VALID_POST_LIKE_DATA } from '@test/mock/data/like';
+import { VALID_POST } from '@test/mock/data/post';
 
 describe('[SERVICE] | LIKE', () => {
   let module: ISimulateLikeService;
@@ -25,5 +26,17 @@ describe('[SERVICE] | LIKE', () => {
     expect(module.use_case.create.execute).toHaveBeenCalledWith(
       VALID_POST_LIKE_DATA,
     );
+  });
+
+  it('[UNIT] | Should: delete => Like', async () => {
+    module.use_case.deleteLike.execute.mockResolvedValue();
+
+    const result = await module.service.dislike({ id: VALID_POST.id });
+
+    expect(result).toEqual(undefined);
+    expect(module.use_case.deleteLike.execute).toHaveBeenCalledTimes(1);
+    expect(module.use_case.deleteLike.execute).toHaveBeenCalledWith({
+      id: VALID_POST.id,
+    });
   });
 });
