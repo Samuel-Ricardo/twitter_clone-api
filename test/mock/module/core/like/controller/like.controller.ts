123dfaa7f7ab3fad51e2b2a35a3d5164564daef8
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-08-14 12:37:11 -0300

    [:clown_face:] | simulate: controller - like (mock/module)

--- test/mock/module/core/like/controller/like.controller.ts
+++ test/mock/module/core/like/controller/like.controller.ts
@@ -1,4 +1,21 @@
-import { LikeController } from '../../../../../../src/modules/@core/like';
-import { mockDeep } from 'jest-mock-extended';
+import {
+  LikeController,
+  LikeService,
+} from '../../../../../../src/modules/@core/like';
+import { DeepMockProxy, mockDeep } from 'jest-mock-extended';
+import { interfaces } from 'inversify';
+import { MockLikeRegistry } from '../like.registry';
+import { ISimulateLikeController } from '@test/@types/simulate/like/controller';
 
 export const mockLikeController = () => mockDeep<LikeController>();
+
+export const simulateLikeController = ({
+  container,
+}: interfaces.Context): ISimulateLikeController => {
+  const service = container.get<DeepMockProxy<LikeService>>(
+    MockLikeRegistry.SERVICE,
+  );
+  const controller = new LikeController(service);
+
+  return { controller, service };
+};
