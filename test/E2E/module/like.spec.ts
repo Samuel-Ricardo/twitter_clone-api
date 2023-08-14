0313e5cbd5710cac60d4d61dd47e70bb27c097b8
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-08-14 15:35:52 -0300

    [:white_check_mark:] | create: test - module > like => get from post (E2E)

--- test/E2E/module/like.spec.ts
+++ test/E2E/module/like.spec.ts
@@ -7,6 +7,7 @@ import { like } from '../../../src/modules/router/like';
 import { CREATE_POST_LIKE_DATA } from '@test/mock/data/like';
 import supertest from 'supertest';
 import { ILikeDTO } from '@Like';
+import { VALID_POST } from '@test/mock/data/post';
 
 describe('[MODULE] | LIKE', () => {
   let liked: ILikeDTO;
@@ -24,6 +25,22 @@ describe('[MODULE] | LIKE', () => {
     expect(body.like.likedId).toEqual(CREATE_POST_LIKE_DATA.likedId);
 
     liked = body.like;
+  });
+
+  it('[E2E] | Should: get from post => [Like]', async () => {
+    const response = await supertest(app).get(
+      `${like.prefix}/post/${VALID_POST.id}`,
+    );
+    const body = response.body;
+
+    console.log({ body });
+
+    expect(response.status).toBe(200);
+    expect(body.likes).toBeDefined();
+    expect(body.likes).toBeInstanceOf(Array);
+    expect(body.likes.length).toBeGreaterThanOrEqual(1);
+    expect(body.likes[0]).toHaveProperty('id');
+    expect(body.likes[0].likedId).toEqual(VALID_POST.id);
   });
 
   it('[E2E] | Should: delete => [Like]', async () => {
