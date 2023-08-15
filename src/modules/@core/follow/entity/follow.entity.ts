74c05a4e10cfbf05c883b4dbaf1980369f92919b
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-08-15 00:14:06 -0300

    [:sparkles:] | create: entity - Follow > create (module)

--- src/modules/@core/follow/entity/follow.entity.ts
+++ src/modules/@core/follow/entity/follow.entity.ts
@@ -3,15 +3,29 @@ import { FollowSchema } from '../validator';
 
 export class Follow {
   constructor(
-    public id: string,
-    public followerId: string,
-    public followingId: string,
-    public createdAt: Date,
+    private _id: string,
+    private _followerId: string,
+    private _followingId: string,
+    private _createdAt: Date,
   ) {
-    Follow.validate(this);
+    Follow.validate({
+      id: this._id,
+      followerId: this._followerId,
+      followingId: this._followingId,
+      createdAt: this._createdAt,
+    });
   }
 
   static validate(follow: IFollowDTO) {
     return FollowSchema.parse(follow);
+  }
+
+  static create(data: IFollowDTO) {
+    return new Follow(
+      data.id,
+      data.followerId,
+      data.followingId,
+      data.createdAt,
+    );
   }
 }
