bc790c0ef2cfb3fa41a3085848c93a37098c6d34
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-08-15 15:32:14 -0300

    [:sparkles:] | update: index - DTO (follow)

--- src/modules/@core/follow/DTO/index.ts
+++ src/modules/@core/follow/DTO/index.ts
@@ -1,1 +1,7 @@
 export * from './follow.dto';
+export * from './create.dto';
+export * from './delete.dto';
+export * from './get_followers.dto';
+export * from './get_followings.dto';
+export * from './count_followers.dto';
+export * from './count_followings.dto';
