bb930945b4579da2ec2afdbd195486ee44680e3b
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-08-15 16:59:56 -0300

    [:sparkles:] | update: service - follow (module)

---
+++ src/modules/@core/follow/repository/service/follow.service.ts
@@ -0,0 +1,33 @@
+import {
+  CountFollowersUseCase,
+  CountFollowingsUseCase,
+  CreateFollowUseCase,
+  GetFollowersUseCase,
+  GetFollowingsUseCase,
+  UnFollowUseCase,
+} from '@Core/follow/use-case';
+import { MODULE } from '../../../../app.registry';
+import { inject, injectable } from 'inversify';
+import { ICreateFollowDTO } from '@Core/follow/DTO';
+
+@injectable()
+export class FollowService {
+  constructor(
+    @inject(MODULE.FOLLOW.USE_CASE.CREATE)
+    private readonly create: CreateFollowUseCase,
+    @inject(MODULE.FOLLOW.USE_CASE.DELETE)
+    private readonly unfollow: UnFollowUseCase,
+    @inject(MODULE.FOLLOW.USE_CASE.GET.FOLLOWERS)
+    private readonly getFollowers: GetFollowersUseCase,
+    @inject(MODULE.FOLLOW.USE_CASE.GET.FOLLOWINGS)
+    private readonly getFollowings: GetFollowingsUseCase,
+    @inject(MODULE.FOLLOW.USE_CASE.COUNT.FOLLOWINGS)
+    private readonly countFollowings: CountFollowingsUseCase,
+    @inject(MODULE.FOLLOW.USE_CASE.COUNT.FOLLOWERS)
+    private readonly countFollowers: CountFollowersUseCase,
+  ) {}
+
+  async follow(data: ICreateFollowDTO) {
+    return await this.create.execute(data);
+  }
+}
