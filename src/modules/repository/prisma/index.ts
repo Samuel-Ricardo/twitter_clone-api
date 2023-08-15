11b0247b8ef41c05ad8df70149f25066d40d7a0d
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-08-15 16:10:10 -0300

    [:sparkles:] | update: index - module (repository/prisma)

--- src/modules/repository/prisma/index.ts
+++ src/modules/repository/prisma/index.ts
@@ -1,5 +1,6 @@
 export * from './post/post.repository';
 export * from './user/user.repository';
+export * from './post/post.repository';
 export * from './prisma-repository.module';
 export * from './prisma-repository.factory';
 export * from './prisma-repository.registry';
