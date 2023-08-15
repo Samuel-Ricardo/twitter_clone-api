84fa60007ddf2eb1985db2ea6572ace79167e7a5
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-08-15 08:13:11 -0300

    [:sparkles:] | create: use-case - follow > delete (module/core)

--- src/modules/@core/follow/use-case/delete.use-case.ts
+++ src/modules/@core/follow/use-case/delete.use-case.ts
@@ -1,6 +1,8 @@
 import { IDeleteLikeDTO } from '@Like';
 import { FollowRepositoryAccess } from '../repository';
+import { injectable } from 'inversify';
 
+@injectable()
 export class UnFollowUseCase extends FollowRepositoryAccess {
   async execute(data: IDeleteLikeDTO) {
     return await this.repository.delete(data);
