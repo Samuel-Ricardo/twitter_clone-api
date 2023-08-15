efb9c6363c9d837e5b639bc422f1245d355a8cd6
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-08-15 16:37:25 -0300

    [:sparkles:] | update: index - module (follow)

--- src/modules/@core/follow/index.ts
+++ src/modules/@core/follow/index.ts
@@ -3,3 +3,6 @@ export * from './DTO';
 export * from './validator';
 export * from './repository';
 export * from './use-case';
+export * from './follow.registry';
+export * from './follow.module';
+export * from './follow.factory';
