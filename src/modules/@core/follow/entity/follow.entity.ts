219c5d139732639bc177331102bc1eaf5163507a
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-08-14 23:58:54 -0300

    [:safety_vest:] | apply: validator - follow (module)

--- src/modules/@core/follow/entity/follow.entity.ts
+++ src/modules/@core/follow/entity/follow.entity.ts
@@ -1,8 +1,17 @@
+import { IFollowDTO } from '../DTO/follow.dto';
+import { FollowSchema } from '../validator';
+
 export class Follow {
   constructor(
     public id: string,
     public followerId: string,
     public followingId: string,
     public createdAt: Date,
-  ) {}
+  ) {
+    Follow.validate(this);
+  }
+
+  static validate(follow: IFollowDTO) {
+    return FollowSchema.parse(follow);
+  }
 }
