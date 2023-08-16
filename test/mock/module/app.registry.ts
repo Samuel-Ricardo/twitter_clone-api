00af702caf1897c519c56944d52716082ded4e61
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-08-16 18:20:19 -0300

    [:clown_face:] | update: registry - app (mock/module)

--- test/mock/module/app.registry.ts
+++ test/mock/module/app.registry.ts
@@ -3,6 +3,7 @@ import { PrismaRepositoryMockRegistry as REPOSITORY } from './repository';
 import { UserMockRegistry as USER } from './core/user';
 import { PostMockRegistry as POST } from './core/post';
 import { MockLikeRegistry as LIKE } from './core/like';
+import { FollowMockRegistry as FOLLOW } from './core/follow';
 
 export const MOCK_MODULE = {
   ...PrismaMockResgistry,
@@ -10,4 +11,5 @@ export const MOCK_MODULE = {
   USER,
   POST,
   LIKE,
+  FOLLOW,
 };
