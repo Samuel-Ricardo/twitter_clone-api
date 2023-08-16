dbead24021816c4b8a499ba42aa6d16037ae5356
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-08-15 21:39:00 -0300

    [:truck:] | create: router - follow > delete (module)

--- src/modules/router/follow/follow.router.ts
+++ src/modules/router/follow/follow.router.ts
@@ -1,4 +1,5 @@
 import { CreateFollowSchema } from '@Core/follow/validator/create.validator';
+import { DeleteFollowSchema } from '@Core/follow/validator/delete.validator';
 import { MODULES } from '@modules/app.factory';
 import { validate } from '@modules/middleware/validator';
 import { Router } from 'express';
@@ -11,6 +12,14 @@ const module = MODULES.FOLLOW.DEFAULT();
 router.post(prefix, validate(CreateFollowSchema), (req, res, next) => {
   try {
     res.status(201).json(module.follow(req.body));
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.delete(prefix, validate(DeleteFollowSchema), (req, res, next) => {
+  try {
+    res.status(204).json(module.unfollow(req.body));
   } catch (error) {
     next(error);
   }
