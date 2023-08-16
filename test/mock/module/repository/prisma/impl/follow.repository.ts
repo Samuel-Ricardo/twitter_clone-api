33e961aa48f56465235e55a65a882f6432ffbd98
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-08-16 18:15:30 -0300

    [:clown_face:] | simulate: repository - prisma > follow (mock/module)

--- test/mock/module/repository/prisma/impl/follow.repository.ts
+++ test/mock/module/repository/prisma/impl/follow.repository.ts
@@ -1,5 +1,17 @@
 import { PrismaFollowRepository } from '@modules/repository/prisma/follow';
-import { mockDeep } from 'jest-mock-extended';
+import { PrismaClient } from '@prisma/client';
+import { ISimulateFollowRepository } from '@test/@types/simulate/follow/repository';
+import { MOCK_MODULE } from '@test/mock/module/app.registry';
+import { interfaces } from 'inversify';
+import { DeepMockProxy, mockDeep } from 'jest-mock-extended';
 
 export const mockPrismaFollowRepository = () =>
   mockDeep<PrismaFollowRepository>();
+
+export const simulatePrismaFollowRepository = ({
+  container,
+}: interfaces.Context): ISimulateFollowRepository => {
+  const prisma = container.get<DeepMockProxy<PrismaClient>>(MOCK_MODULE.PRISMA);
+  const repository = new PrismaFollowRepository(prisma);
+  return { repository, prisma };
+};
