9862f3debac0bedc20a13162c100387122e29b8a
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-08-16 17:51:53 -0300

    [:clown_face:] | mock: controller - follow (mock/module)

---
+++ test/mock/module/core/follow/controller/follow.controller.ts
@@ -0,0 +1,4 @@
+import { FollowController } from '@Core/follow/controller';
+import { mockDeep } from 'jest-mock-extended';
+
+export const mockFollowController = () => mockDeep<FollowController>();
