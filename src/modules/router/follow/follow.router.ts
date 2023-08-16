6c2b5581a4e7d29d9f78def8e701515194ebc901
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-08-15 21:41:36 -0300

    [:truck:] | create: router - follow > count (module)

--- src/modules/router/follow/follow.router.ts
+++ src/modules/router/follow/follow.router.ts
@@ -1,3 +1,4 @@
+import { CountFollowersSchema } from '@Core/follow/validator/count_followers.validator';
 import { CreateFollowSchema } from '@Core/follow/validator/create.validator';
 import { DeleteFollowSchema } from '@Core/follow/validator/delete.validator';
 import { MODULES } from '@modules/app.factory';
@@ -24,3 +25,17 @@ router.delete(prefix, validate(DeleteFollowSchema), (req, res, next) => {
     next(error);
   }
 });
+
+router.get(
+  `${prefix}/count/followers/:followingId`,
+  validate(CountFollowersSchema),
+  (req, res, next) => {
+    try {
+      res
+        .status(200)
+        .json(module.countFollowers({ followingId: req.params.followingId }));
+    } catch (error) {
+      next(error);
+    }
+  },
+);
