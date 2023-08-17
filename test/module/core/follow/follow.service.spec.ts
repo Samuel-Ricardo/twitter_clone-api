6edf996790bedf2a16a79cfe102a136a77517085
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-08-16 22:38:26 -0300

    [:white_check_mark:] | create: unit test - service > follow => unfollow (core)

--- test/module/core/follow/follow.service.spec.ts
+++ test/module/core/follow/follow.service.spec.ts
@@ -2,7 +2,11 @@ import { Follow } from '../../../../src/modules/@core/follow';
 import { FollowService } from '../../../../src/modules/@core/follow/service';
 import { ISimulateFollowService } from '@test/@types/simulate/follow/service';
 import { MockFactory } from '@test/mock';
-import { CREATE_FOLLOW_DATA, VALID_FOLLOW } from '@test/mock/data/follow';
+import {
+  CREATE_FOLLOW_DATA,
+  FOLLOW_DATA,
+  VALID_FOLLOW,
+} from '@test/mock/data/follow';
 
 describe('[SERVICE] | FOLLOW ', () => {
   let module: ISimulateFollowService;
@@ -27,5 +31,18 @@ describe('[SERVICE] | FOLLOW ', () => {
 
     expect(module.use_case.create.execute).toBeCalledTimes(1);
     expect(module.use_case.create.execute).toBeCalledWith(CREATE_FOLLOW_DATA);
+  });
+
+  it('[UNIT] | Should: be able to => [UNFOLLOW]', async () => {
+    module.use_case.unfollow.execute.mockResolvedValue();
+
+    expect(
+      module.service.unfollow({ id: VALID_FOLLOW.id }),
+    ).resolves.not.toThrow();
+
+    expect(module.use_case.unfollow.execute).toBeCalledTimes(1);
+    expect(module.use_case.unfollow.execute).toBeCalledWith({
+      id: VALID_FOLLOW.id,
+    });
   });
 });
