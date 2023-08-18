5bfb475da8c736515e9377d21b5785658134d303
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-08-18 11:11:36 -0300

    [:card_file_box:] | create: repository - comment > create (prisma)

--- src/modules/repository/prisma/comment/comment.repository.ts
+++ src/modules/repository/prisma/comment/comment.repository.ts
@@ -1,7 +1,10 @@
+import { MODULE } from '../../../app.registry';
 import { inject, injectable } from 'inversify';
 import { PrismaClient } from '@prisma/client';
 import { ICommentRepository } from '../../../@core/comment/repository/comment.repository';
-import { MODULE } from '../../../app.registry';
+import { Comment } from '../../../@core/comment/entity';
+import { Comment as PrismaComment } from '@prisma/client';
+import { ICreateCommentDTO } from '../../../@core/comment/DTO';
 
 @injectable()
 export class PrismaCommentRepository implements ICommentRepository {
@@ -9,4 +12,12 @@ export class PrismaCommentRepository implements ICommentRepository {
     @inject(MODULE.PRISMA)
     private readonly prisma: PrismaClient,
   ) {}
+
+  async create(comment: ICreateCommentDTO) {
+    const result = await this.prisma.comment.create({
+      data: comment,
+    });
+
+    return Comment.fromPrisma(result);
+  }
 }
