4ed289d65ce65805274eca5643c70e4c6f37fb80
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-08-18 10:54:10 -0300

    [:safety_vest:] | update: index - comment > validator (module)

--- src/modules/@core/comment/validator/index.ts
+++ src/modules/@core/comment/validator/index.ts
@@ -1,1 +1,6 @@
 export * from './comment.validator';
+export * from './create.validator';
+export * from './update.validator';
+export * from './delete.validator';
+export * from './get_by_post.validator';
+export * from './get_by_user.validator';
