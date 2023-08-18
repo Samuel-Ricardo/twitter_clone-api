01b05972f819975ccfc091e6bf0f84fdad8bee95
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-08-18 16:12:12 -0300

    [:sparkles:] | create: module - comment (module)

---
+++ src/modules/@core/comment/comment.module.ts
@@ -0,0 +1,21 @@
+import { Container } from 'inversify';
+import { CommentRegistry } from './comment.registry';
+import {
+  CreateCommentUseCase,
+  DeleteCommentUseCase,
+  GetPostCommentUseCase,
+  GetUserCommnetsUseCase,
+  UpdateCommentUseCase,
+} from './use-case';
+
+export const CommentModule = new Container({ autoBindInjectable: true });
+
+CommentModule.bind(CommentRegistry.USE_CASE.CREATE).to(CreateCommentUseCase);
+CommentModule.bind(CommentRegistry.USE_CASE.DELETE).to(DeleteCommentUseCase);
+CommentModule.bind(CommentRegistry.USE_CASE.UPDATE).to(UpdateCommentUseCase);
+CommentModule.bind(CommentRegistry.USE_CASE.GET.BY.POST).to(
+  GetPostCommentUseCase,
+);
+CommentModule.bind(CommentRegistry.USE_CASE.GET.BY.AUTHOR).to(
+  GetUserCommnetsUseCase,
+);
