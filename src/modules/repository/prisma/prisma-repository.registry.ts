9bb9887cdce213f7e12ecae4517a4592b83531c9
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-08-18 11:26:48 -0300

    [:card_file_box:] | update: registry - repository (prisma)

--- src/modules/repository/prisma/prisma-repository.registry.ts
+++ src/modules/repository/prisma/prisma-repository.registry.ts
@@ -3,4 +3,5 @@ export const PrismaRepositoryRegistry = {
   POST: Symbol.for('PRISMA_POST_REPOSITORY'),
   LIKE: Symbol.for('PRISMA_LIKE_REPOSITORY'),
   FOLLOW: Symbol.for('PRISMA_FOLLOW_REPOSITORY'),
+  COMMENT: Symbol.for('PRISMA_COMMENT_REPOSITORY'),
 };
