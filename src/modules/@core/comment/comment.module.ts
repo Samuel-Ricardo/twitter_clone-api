2196b7c1ab48d1516e5f5ae9a3770db8fc076128
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-08-18 16:49:49 -0300

    [:sparkles:] | update: module - comment (module)

--- src/modules/@core/comment/comment.module.ts
+++ src/modules/@core/comment/comment.module.ts
@@ -8,22 +8,20 @@ import {
   UpdateCommentUseCase,
 } from './use-case';
 import { RepositoryModule } from '../../repository/repository.module';
-// import { CommentService } from './service/comment.service';
+import { CommentService } from './service/comment.service';
 
 const Module = new Container({ autoBindInjectable: true });
 
-const aCommentModule = Container.merge(Module, RepositoryModule);
+export const CommentModule = Container.merge(Module, RepositoryModule);
 
-aCommentModule.bind(CommentRegistry.USE_CASE.CREATE).to(CreateCommentUseCase);
-aCommentModule.bind(CommentRegistry.USE_CASE.DELETE).to(DeleteCommentUseCase);
-aCommentModule.bind(CommentRegistry.USE_CASE.UPDATE).to(UpdateCommentUseCase);
-aCommentModule
-  .bind(CommentRegistry.USE_CASE.GET.BY.POST)
-  .to(GetPostCommentUseCase);
-aCommentModule
-  .bind(CommentRegistry.USE_CASE.GET.BY.AUTHOR)
-  .to(GetUserCommnetsUseCase);
+CommentModule.bind(CommentRegistry.USE_CASE.CREATE).to(CreateCommentUseCase);
+CommentModule.bind(CommentRegistry.USE_CASE.DELETE).to(DeleteCommentUseCase);
+CommentModule.bind(CommentRegistry.USE_CASE.UPDATE).to(UpdateCommentUseCase);
+CommentModule.bind(CommentRegistry.USE_CASE.GET.BY.POST).to(
+  GetPostCommentUseCase,
+);
+CommentModule.bind(CommentRegistry.USE_CASE.GET.BY.AUTHOR).to(
+  GetUserCommnetsUseCase,
+);
 
-// CommentModule.bind(CommentRegistry.SERVICE).to(CommentService);
-
-export { Module as CommentModule };
+CommentModule.bind(CommentRegistry.SERVICE).to(CommentService);
