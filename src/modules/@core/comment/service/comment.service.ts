94e3f9a704b756b3dab5ea4bfd2f4c6dc71fb5cd
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-08-18 16:25:38 -0300

    [:sparkles:] | create: service - comment (module)

---
+++ src/modules/@core/comment/service/comment.service.ts
@@ -0,0 +1,25 @@
+import { inject, injectable } from 'inversify';
+import {
+  CreateCommentUseCase,
+  DeleteCommentUseCase,
+  GetPostCommentUseCase,
+  GetUserCommnetsUseCase,
+  UpdateCommentUseCase,
+} from '../use-case';
+import { MODULE } from '@modules/app.registry';
+
+@injectable()
+export class CommentService {
+  constructor(
+    @inject(MODULE.COMMENT.USE_CASE.CREATE)
+    private readonly create: CreateCommentUseCase,
+    @inject(MODULE.COMMENT.USE_CASE.UPDATE)
+    private readonly update: UpdateCommentUseCase,
+    @inject(MODULE.COMMENT.USE_CASE.DELETE)
+    private readonly deleteComment: DeleteCommentUseCase,
+    @inject(MODULE.COMMENT.USE_CASE.GET.BY.POST)
+    private readonly getPostComment: GetPostCommentUseCase,
+    @inject(MODULE.COMMENT.USE_CASE.GET.BY.AUTHOR)
+    private readonly getUserCommnets: GetUserCommnetsUseCase,
+  ) {}
+}
