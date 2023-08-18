8e2496b7c70b1e11bf1287f6bbb061fbc1bc831d
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-08-18 11:13:16 -0300

    [:card_file_box:] | create: repository - comment > update (prisma)

--- src/modules/repository/prisma/comment/comment.repository.ts
+++ src/modules/repository/prisma/comment/comment.repository.ts
@@ -4,7 +4,10 @@ import { PrismaClient } from '@prisma/client';
 import { ICommentRepository } from '../../../@core/comment/repository/comment.repository';
 import { Comment } from '../../../@core/comment/entity';
 import { Comment as PrismaComment } from '@prisma/client';
-import { ICreateCommentDTO } from '../../../@core/comment/DTO';
+import {
+  ICreateCommentDTO,
+  IUpdateCommentDTO,
+} from '../../../@core/comment/DTO';
 
 @injectable()
 export class PrismaCommentRepository implements ICommentRepository {
@@ -16,6 +19,17 @@ export class PrismaCommentRepository implements ICommentRepository {
   async create(comment: ICreateCommentDTO) {
     const result = await this.prisma.comment.create({
       data: comment,
+    });
+
+    return Comment.fromPrisma(result);
+  }
+
+  async update(comment: IUpdateCommentDTO) {
+    const result = await this.prisma.comment.update({
+      where: {
+        id: comment.id,
+      },
+      data: { body: comment.body },
     });
 
     return Comment.fromPrisma(result);
