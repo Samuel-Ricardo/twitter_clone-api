29009acb8631c3b33833aca0099ad13ba1e165ce
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-08-20 16:13:30 -0300

    [:clown_face:] | simulate: service - comment (mock/module)

--- test/mock/module/core/comment/service/comment.service.ts
+++ test/mock/module/core/comment/service/comment.service.ts
@@ -1,4 +1,53 @@
-import { mockDeep } from 'jest-mock-extended';
+import { DeepMockProxy, mockDeep } from 'jest-mock-extended';
 import { CommentService } from '../../../../../../src/modules/@core/comment/service/comment.service';
+import { interfaces } from 'inversify';
+import { ISimulateCommentService } from '@test/@types/simulate/comment';
+import { MOCK_MODULE } from '@test/mock/module/app.registry';
+import {
+  CreateCommentUseCase,
+  DeleteCommentUseCase,
+  GetPostCommentUseCase,
+  GetUserCommnetsUseCase,
+  UpdateCommentUseCase,
+} from '@Core/comment/use-case';
 
 export const mockCommentService = () => mockDeep<CommentService>();
+
+export const simulateCommentService = ({
+  container,
+}: interfaces.Context): ISimulateCommentService => {
+  const create = container.get<DeepMockProxy<CreateCommentUseCase>>(
+    MOCK_MODULE.COMMENT.USE_CASE.CREATE,
+  );
+  const update = container.get<DeepMockProxy<UpdateCommentUseCase>>(
+    MOCK_MODULE.COMMENT.USE_CASE.UPDATE,
+  );
+  const deleteComment = container.get<DeepMockProxy<DeleteCommentUseCase>>(
+    MOCK_MODULE.COMMENT.USE_CASE.DELETE,
+  );
+  const get_user_comments = container.get<
+    DeepMockProxy<GetUserCommnetsUseCase>
+  >(MOCK_MODULE.COMMENT.USE_CASE.GET.USER);
+  const get_post_comments = container.get<DeepMockProxy<GetPostCommentUseCase>>(
+    MOCK_MODULE.COMMENT.USE_CASE.GET.POST,
+  );
+
+  const service = new CommentService(
+    create,
+    update,
+    deleteComment,
+    get_post_comments,
+    get_user_comments,
+  );
+
+  return {
+    service,
+    use_case: {
+      create,
+      update,
+      deleteComment,
+      get_post_comments,
+      get_user_comments,
+    },
+  };
+};
