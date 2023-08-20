e08ddc1a6c2fd61c62a42e47044b5ef82f7424ca
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-08-20 14:43:25 -0300

    [:clown_face:] | create: index - use-case > comment (mock/module)

---
+++ test/mock/module/core/comment/use-case/index.ts
@@ -0,0 +1,5 @@
+export * from './create.use-case';
+export * from './update.use-case';
+export * from './delete.use-case';
+export * from './get_by_post.use-case';
+export * from './get_by_user.use-case';
