5aa00b0e8ab10cfbae97b80350283c20bff62a8b
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-08-20 17:46:05 -0300

    [:white_check_mark:] | test: fin by [post] - repository > comment (core)

--- test/module/repository/prisma/integration/comment.repository.spec.ts
+++ test/module/repository/prisma/integration/comment.repository.spec.ts
@@ -7,10 +7,14 @@ import {
   Comment,
   ICommentRepository,
 } from '../../../../../src/modules';
-import { CREATE_POST_COMMENT_DATA } from '@test/mock/data/comment';
+import {
+  CREATE_POST_COMMENT_DATA,
+  UPDATE_POST_COMMENT_DATA,
+} from '@test/mock/data/comment';
 
 describe('[REPOSITORY] | PRISMA => [COMMENT]', () => {
   let repositoy: ICommentRepository;
+  let commented: Comment;
 
   beforeAll(() => {
     repositoy = MODULES.REPOSITORY.PRISMA.COMMENT();
@@ -27,5 +31,38 @@ describe('[REPOSITORY] | PRISMA => [COMMENT]', () => {
     expect(result.authorId).toBe(CREATE_POST_COMMENT_DATA.authorId);
     expect(result.body).toBe(CREATE_POST_COMMENT_DATA.body);
     expect(result.postId).toBe(CREATE_POST_COMMENT_DATA.postId);
+
+    commented = result;
+  });
+
+  it('[INTEGRATION] | Shoudl: find by [POST] => [COMMENT]', async () => {
+    const result = await repositoy.getPostComments({
+      postId: commented.postId,
+    });
+
+    expect(result).toBeDefined();
+    expect(result).toBeInstanceOf(Array);
+    expect(result).toHaveLength(1);
+    expect(result[0]).toBeInstanceOf(Comment);
+
+    expect(result[0]).toHaveProperty('_id');
+    expect(result[0]).toStrictEqual(commented);
+  });
+
+  it('[INTEGRATION] | Should: Update => [COMMENT]', async () => {
+    const result = await repositoy.update({
+      id: commented.id,
+      body: UPDATE_POST_COMMENT_DATA.body,
+    });
+
+    expect(result).toBeDefined();
+    expect(result).toBeInstanceOf(Comment);
+
+    expect(result).toHaveProperty('_id');
+    expect(result.id).toBe(commented.id);
+
+    expect(result.body).toBe(UPDATE_POST_COMMENT_DATA.body);
+
+    commented = result;
   });
 });
