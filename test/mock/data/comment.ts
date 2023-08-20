6bcf0400d3191d852633cce910ca71d77771ce39
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-08-20 12:56:55 -0300

    [:clown_face:] | mock: data - comment (test)

---
+++ test/mock/data/comment.ts
@@ -0,0 +1,26 @@
+import { VALID_USER as USER } from './user';
+import { VALID_POST as POST } from './post';
+import { randomID } from '../../../src/modules/util/mongo';
+import {
+  ICommentDTO,
+  ICreateCommentDTO,
+} from '../../../src/modules/@core/comment/DTO';
+import { Comment } from '../../../src/modules/@core/comment/entity';
+
+export const VALID_USER = USER;
+export const VALID_POST = POST;
+
+export const CREATE_POST_COMMENT_DATA: ICreateCommentDTO = {
+  body: 'Hello World Comment! :D',
+  postId: VALID_POST.id,
+  authorId: VALID_USER.id,
+};
+
+export const VALID_POST_COMMENT_DATA: ICommentDTO = {
+  ...CREATE_POST_COMMENT_DATA,
+  id: randomID(),
+  createdAt: new Date(),
+  updatedAt: new Date(),
+};
+
+export const VALID_POST_LIKE = Comment.create(VALID_POST_COMMENT_DATA);
