e16457e1024979887d7ac2015ef709b8ec2fdbd6
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-08-20 14:45:37 -0300

    [:clown_face:] | create: registry - comment (mock/module)

---
+++ test/mock/module/core/comment/comment.registry.ts
@@ -0,0 +1,13 @@
+export const CommentMockRegistry = {
+  USE_CASE: {
+    CREATE: Symbol.for('MOCK:>COMMENT.USE_CASE.CREATE'),
+    UPDATE: Symbol.for('MOCK:>COMMENT.USE_CASE.UPDATE'),
+    DELETE: Symbol.for('MOCK:>COMMENT.USE_CASE.DELETE'),
+    GET: {
+      BY: {
+        POST: Symbol.for('MOCK:>COMMENT.USE_CASE.GET.BY.POST'),
+        AUTHOR: Symbol.for('MOCK:>COMMENT.USE_CASE.GET.BY.AUTHOR'),
+      },
+    },
+  },
+};
