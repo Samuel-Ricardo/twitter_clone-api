58df076593e24e60bce612dc15fc9ebeea9d3f79
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-08-20 13:26:00 -0300

    [:clown_face:] | simulate: repository - prisma > comment (mock/module)

--- test/mock/module/repository/prisma/impl/comment.repository.ts
+++ test/mock/module/repository/prisma/impl/comment.repository.ts
@@ -1,5 +1,21 @@
+import { interfaces } from 'inversify';
 import { PrismaCommentRepository } from '../../../../../../src/modules/repository/prisma/comment';
-import { mockDeep } from 'jest-mock-extended';
+import { DeepMockProxy, mockDeep } from 'jest-mock-extended';
+import { PrismaClient } from '@prisma/client';
+import { PrismaMockResgistry } from '@test/mock/module/prisma';
+import { ISimulatePrismaRepository } from '@test/@types/simulate/repository';
 
 export const mockPrismaCommentRepository = () =>
   mockDeep<PrismaCommentRepository>();
+
+export const simulatePrismaCommentRepository = ({
+  container,
+}: interfaces.Context): ISimulatePrismaRepository<PrismaCommentRepository> => {
+  const prisma = container.get<DeepMockProxy<PrismaClient>>(
+    PrismaMockResgistry.PRISMA,
+  );
+
+  const repository = new PrismaCommentRepository(prisma);
+
+  return { prisma, repository };
+};
