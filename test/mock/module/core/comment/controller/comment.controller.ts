82030af6edf108dadfb81612905a14bb42600c28
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-08-20 16:23:52 -0300

    [:clown_face:] | simulate: controller - comment (mock/module)

--- test/mock/module/core/comment/controller/comment.controller.ts
+++ test/mock/module/core/comment/controller/comment.controller.ts
@@ -1,4 +1,22 @@
-import { mockDeep } from 'jest-mock-extended';
+import { DeepMockProxy, mockDeep } from 'jest-mock-extended';
 import { CommentController } from '../../../../../../src/modules/@core/comment/controller/comment.controller';
+import { interfaces } from 'inversify';
+import { ISimulateController } from '@test/@types/simulate/controller';
+import { CommentService } from '../../../../../../src/modules/@core/comment/service';
+import { MOCK_MODULE } from '@test/mock/module/app.registry';
 
 export const mockCommentController = () => mockDeep<CommentController>();
+
+export const simulateCommentController = ({
+  container,
+}: interfaces.Context): ISimulateController<
+  CommentService,
+  CommentController
+> => {
+  const service = container.get<DeepMockProxy<CommentService>>(
+    MOCK_MODULE.COMMENT.SERVICE,
+  );
+  const controller = new CommentController(service);
+
+  return { service, controller };
+};
