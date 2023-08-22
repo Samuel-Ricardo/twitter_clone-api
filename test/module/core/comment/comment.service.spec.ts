cf3ceb0520e7f062d3d87a0dd137aad8e32d9a1c
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-08-21 22:49:26 -0300

    [:white_check_mark:] | test: create - service > comment (unit)

--- test/module/core/comment/comment.service.spec.ts
+++ test/module/core/comment/comment.service.spec.ts
@@ -1,5 +1,9 @@
 import { ISimulateCommentService } from '@test/@types/simulate/comment';
 import { MockFactory } from '@test/mock';
+import {
+  VALID_POST_COMMENT,
+  VALID_POST_COMMENT_DATA,
+} from '@test/mock/data/comment';
 
 describe('[SERVICE] | COMMENT', () => {
   let module: ISimulateCommentService;
@@ -15,6 +19,14 @@ describe('[SERVICE] | COMMENT', () => {
   });
 
   it('[UNIT] | Should: create => Comment', async () => {
-    expect(true).toBe(true);
+    module.use_case.create.execute.mockResolvedValue(VALID_POST_COMMENT);
+
+    const result = await module.service.comment(VALID_POST_COMMENT_DATA);
+
+    expect(result).toStrictEqual(VALID_POST_COMMENT);
+    expect(module.use_case.create.execute).toBeCalledTimes(1);
+    expect(module.use_case.create.execute).toBeCalledWith(
+      VALID_POST_COMMENT_DATA,
+    );
   });
 });
