0f2005675bd58f8f959b585f3c6788400492be00
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-08-22 16:22:42 -0300

    [:closed_lock_with_key:] | update: env (confih)

--- src/modules/config/env/env.module.ts
+++ src/modules/config/env/env.module.ts
@@ -5,6 +5,7 @@ if (process.env.NODE_ENV === 'production') dotnev.config({ path: '.env.prod' });
 
 export const ENV = {
   ...process.env,
+  NODE_ENV: process.env.NODE_ENV,
 
   ENVIRONMENT: process.env.ENVIRONMENT || 'dev',
   PORT: process.env.PORT || 3000,
