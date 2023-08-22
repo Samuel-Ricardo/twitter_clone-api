c63e27f76226aa22108ba86382fbf969fba19c42
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-08-22 16:30:52 -0300

    [:recycle:] | refact: rename

--- src/modules/reactive/socket_io/socekt.module.ts
+++
@@ -1,7 +0,0 @@
-import { Container } from 'inversify';
-import { SocketIORegistry } from './socket.registry';
-import { setup } from './socket';
-
-export const SocketIOModule = new Container({ autoBindInjectable: true });
-
-SocketIOModule.bind<typeof setup>(SocketIORegistry.IO).toConstantValue(setup);
