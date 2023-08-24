710c1340998d21bbcf39f6914181ed8986264c45
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-08-24 16:56:04 -0300

    [:sparkles:] | update: registry - socket (module)

--- src/modules/reactive/socket_io/socket.registry.ts
+++ src/modules/reactive/socket_io/socket.registry.ts
@@ -1,5 +1,8 @@
 export const SocketRegistry = {
   IO: Symbol.for('socket.io'),
+  INSTANCE: {
+    IO: Symbol.for('instamce.socket.io'),
+  },
   FUNCTIONAL: {
     IO: Symbol.for('functional.socket.io'),
   },
