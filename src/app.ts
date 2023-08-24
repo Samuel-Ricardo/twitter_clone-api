534914d9871ef3e4034409b92f11e8d1d0cb05e4
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-08-24 16:03:43 -0300

    [:sparkles:] | update: app

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,13 @@
 import 'reflect-metadata';
 import { SwaggerModule } from './modules/documentation';
 import { MODULES } from '@modules';
+import { EVENTS } from './modules/reactive/reactive.config';
 
 const app = MODULES.APP();
+const socket = MODULES.REACTIVE.SOCKET.IO;
+const server = socket.server;
 
-SwaggerModule.setup({ app });
+SwaggerModule.setup({ app: app });
 
 app.use(MODULES.BODY_PARSER());
 app.use(MODULES.MIDDLEWARE.LOGGER.APP());
@@ -12,4 +15,9 @@ app.use(MODULES.ROUTER());
 app.use(MODULES.MIDDLEWARE.LOGGER.ERROR());
 app.use(MODULES.MIDDLEWARE.ERROR());
 
-export { app };
+socket.io.on(EVENTS.START.CONNECTION, (data) => {
+  console.log({ data });
+  socket.io.emit(EVENTS.HEALTH_CHECK, { setup: true });
+});
+
+export { app, socket, server };
