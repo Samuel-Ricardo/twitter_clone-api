730c72f3fd881f117c8e15e511ad86b30dafc3ac
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-08-24 15:56:14 -0300

    [:sparkles:] | update: factory - socket (module)

--- src/modules/reactive/socket_io/socket.factory.ts
+++ src/modules/reactive/socket_io/socket.factory.ts
@@ -1,9 +1,12 @@
 import { SocketModule } from './socket.module';
-import { setup } from './socket';
+import { SocketIO, setup } from './socket';
 import { SocketRegistry } from './socket.registry';
 import { ISocketIOConfig } from '@Type/socket/config';
 
 export const SocketFactory = {
-  IO: (config: ISocketIOConfig) =>
-    SocketModule.get<typeof setup>(SocketRegistry.IO)(config),
+  IO: SocketModule.get<SocketIO>(SocketRegistry.IO),
+  FUNCTIONAL: {
+    IO: (config: ISocketIOConfig) =>
+      SocketModule.get<typeof setup>(SocketRegistry.FUNCTIONAL.IO)(config),
+  },
 };
