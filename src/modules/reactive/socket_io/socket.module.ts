d9a64e4737fbabf0ee35c838370ba9117617484f
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-08-24 16:56:27 -0300

    [:sparkles:] | update: module - socket (module)

--- src/modules/reactive/socket_io/socket.module.ts
+++ src/modules/reactive/socket_io/socket.module.ts
@@ -8,4 +8,7 @@ const Module = new Container({ autoBindInjectable: true });
 export const SocketModule = Container.merge(Module, ServerModule);
 
 SocketModule.bind(SocketRegistry.FUNCTIONAL.IO).toConstantValue(setup);
-SocketModule.bind(SocketRegistry.IO).to(SocketIO);
+SocketModule.bind(SocketRegistry.INSTANCE.IO).to(SocketIO);
+SocketModule.bind(SocketRegistry.IO).toConstantValue(
+  SocketModule.get(SocketRegistry.INSTANCE.IO),
+);
