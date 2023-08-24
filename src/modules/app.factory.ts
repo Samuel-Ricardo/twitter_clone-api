27c19f7575be348be2bdba54a75b40ff1ef975c5
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-08-24 14:50:02 -0300

    [:sparkles:] | update: factory - app (module)

--- src/modules/app.factory.ts
+++ src/modules/app.factory.ts
@@ -11,6 +11,7 @@ import { FollowFactory as FOLLOW } from './@core/follow/follow.factory';
 import { MiddlewareFactory as MIDDLEWARE } from './middleware/middleware.factory';
 import { CommentFactory as COMMENT } from './@core/comment/comment.factory';
 import { ReactiveFactory as REACTIVE } from './reactive/reactive.factory';
+import { ServerFactory as SERVER } from './server/server.factory';
 
 import { Express } from 'express';
 import { RoutesFactory } from './router/router.factory';
@@ -26,6 +27,7 @@ export const MODULES = {
   REPOSITORY,
   MIDDLEWARE,
   REACTIVE,
+  SERVER,
   APP: () => AppModule.get<Express>(MODULE.APP),
   BODY_PARSER: () => AppModule.get<any>(MODULE.BODY_PARSER),
 };
