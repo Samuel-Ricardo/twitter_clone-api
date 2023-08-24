3c66a25ea10e2ef73b35f2176f5c8508602418e3
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-08-24 15:55:52 -0300

    [:sparkles:] | update: module - socket (module)

--- src/modules/reactive/socket_io/socket.module.ts
+++ src/modules/reactive/socket_io/socket.module.ts
@@ -1,8 +1,11 @@
 import { Container } from 'inversify';
 import { SocketRegistry } from './socket.registry';
 import { SocketIO, setup } from './socket';
+import { ServerModule } from '../../server/server.module';
 
-export const SocketModule = new Container({ autoBindInjectable: true });
+const Module = new Container({ autoBindInjectable: true });
+
+export const SocketModule = Container.merge(Module, ServerModule);
 
 SocketModule.bind(SocketRegistry.FUNCTIONAL.IO).toConstantValue(setup);
 SocketModule.bind(SocketRegistry.IO).to(SocketIO);
