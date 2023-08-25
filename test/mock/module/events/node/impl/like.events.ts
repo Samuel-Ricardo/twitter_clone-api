95748628e084b8fb1dffe4b5a532bd0b28c1a3a1
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-08-25 14:10:13 -0300

    [:clown_face:] | mock: events - node > like (mock/module)

---
+++ test/mock/module/events/node/impl/like.events.ts
@@ -0,0 +1,4 @@
+import { NodeLikeEvents } from '../../../../../../src/modules/event/node/like';
+import { mockDeep } from 'jest-mock-extended';
+
+export const mockNodeLikeEvents = () => mockDeep<NodeLikeEvents>();
