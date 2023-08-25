8579eb5901eeba264b5e391008e0e7658596e371
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-08-25 16:41:51 -0300

    [:card_file_box:] | create: repository - notification > getByUser (prisma)

--- src/modules/repository/prisma/notification/notification.repository.ts
+++ src/modules/repository/prisma/notification/notification.repository.ts
@@ -2,7 +2,10 @@ import { PrismaClient } from '@prisma/client';
 import { INotificationRepository } from '../../../@core/notification/repository';
 import { inject, injectable } from 'inversify';
 import { MODULE } from '@modules';
-import { ICreateNotificationDTO } from '../../../@core/notification/DTO';
+import {
+  ICreateNotificationDTO,
+  IGetNotificationByUserDTO,
+} from '../../../@core/notification/DTO';
 import { Notification } from '../../../@core/notification/entity';
 
 @injectable()
@@ -18,5 +21,13 @@ export class PrismaNotificationRepository implements INotificationRepository {
     });
 
     return Notification.fromPrisma(result);
+  }
+
+  async getByUser(notification: IGetNotificationByUserDTO) {
+    const result = await this.prisma.notification.findMany({
+      where: notification,
+    });
+
+    return Notification.fromPrismaArray(result);
   }
 }
