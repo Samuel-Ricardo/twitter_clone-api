bac0c92027fa1f8b75371a0ba63317bbc1aaa49f
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-08-25 17:13:26 -0300

    [:sparkles:] | create: module - notification (module)

---
+++ src/modules/@core/notification/notification.module.ts
@@ -0,0 +1,27 @@
+import { Container } from 'inversify';
+import { RepositoryModule as REPOSITORY } from '../../repository/repository.module';
+import { NotificationRegistry } from './notification.registry';
+import { CreateNotificationUseCase } from './use-case/create.use-case';
+import { DeleteNotificationUseCase } from './use-case/delete.use-case';
+import { VisualizeNotificationUseCase } from './use-case/set_visualized.use-case';
+import { GetUserNotificationsUseCase } from './use-case/get_by_user.use-case';
+
+const MODULE = new Container({ autoBindInjectable: true });
+
+export const NotificationModule = Container.merge(MODULE, REPOSITORY);
+
+NotificationModule.bind(NotificationRegistry.USE_CASE.CREATE).to(
+  CreateNotificationUseCase,
+);
+
+NotificationModule.bind(NotificationRegistry.USE_CASE.DELETE).to(
+  DeleteNotificationUseCase,
+);
+
+NotificationModule.bind(NotificationRegistry.USE_CASE.VISUALIZE).to(
+  VisualizeNotificationUseCase,
+);
+
+NotificationModule.bind(NotificationRegistry.USE_CASE.GET.BY.USER).to(
+  GetUserNotificationsUseCase,
+);
