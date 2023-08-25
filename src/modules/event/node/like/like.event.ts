36c49e6cd23e5e971950a86c291a323a6676d2fa
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-08-25 12:11:34 -0300

    [:sparkles:] | update: Events - Like > node (module)

--- src/modules/event/node/like/like.event.ts
+++ src/modules/event/node/like/like.event.ts
@@ -1,7 +1,12 @@
 import { inject, injectable } from 'inversify';
-import { ILikeEvents } from '../../../@core/like';
+import {
+  ICreateLikeEventDTO,
+  ILikeDTO,
+  ILikeEvents,
+} from '../../../@core/like';
 import { Events } from '../event_emmiter';
 import { MODULE } from '@modules/app.registry';
+import { EVENT } from '../../event.config';
 
 @injectable()
 export class NodeLikeEvents implements ILikeEvents {
@@ -9,4 +14,8 @@ export class NodeLikeEvents implements ILikeEvents {
     @inject(MODULE.EVENTS.NODE.EMITTER)
     private events: Events,
   ) {}
+
+  subscribeCreateLike(job: ICreateLikeEventDTO) {
+    this.events.on(EVENT.LIKE.CREATE, job.execute);
+  }
 }
