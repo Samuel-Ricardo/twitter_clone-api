fbed4127b34b1295769baf556983b5c10cf2a678
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-08-25 17:08:45 -0300

    [:sparkles:] | create: registry - notification (module)

---
+++ src/modules/@core/notification/notification.registry.ts
@@ -0,0 +1,16 @@
+export const NotificationRegistry = {
+  USE_CASE: {
+    EVENTS: {
+      CREATE: Symbol.for('USE_CASE.EVENTS.CREATE.NOTIFICATION'),
+      VISUALIZE: Symbol.for('USE_CASE.EVENTS.VISUALIZE.NOTIFICATION'),
+    },
+    CREATE: Symbol.for('USE_CASE.CREATE.NOTIFICATION'),
+    DELETE: Symbol.for('USE_CASE.DELETE.NOTIFICATION'),
+    VISUALIZE: Symbol.for('USE_CASE.VISUALIZE.NOTIFICATION'),
+    GET: {
+      BY: {
+        USER: Symbol.for('USE_CASE.GET.NOTIFICATION.BY.USER'),
+      },
+    },
+  },
+};
