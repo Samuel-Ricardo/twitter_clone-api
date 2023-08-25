2eb3c9185c8ce5b66e6ad10d91d9f52ec3e82090
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-08-25 17:22:46 -0300

    [:sparkles:] | create: service - notification (module)

---
+++ src/modules/@core/notification/service/notification.service.ts
@@ -0,0 +1,22 @@
+import { MODULE } from '@modules';
+import { inject, injectable } from 'inversify';
+import {
+  CreateNotificationUseCase,
+  VisualizeNotificationUseCase,
+  GetUserNotificationsUseCase,
+  DeleteNotificationUseCase,
+} from '../use-case';
+
+@injectable()
+export class NotificationService {
+  constructor(
+    @inject(MODULE.NOTIFICATION.USE_CASE.CREATE)
+    private create: CreateNotificationUseCase,
+    @inject(MODULE.NOTIFICATION.USE_CASE.VISUALIZE)
+    private visualize: VisualizeNotificationUseCase,
+    @inject(MODULE.NOTIFICATION.USE_CASE.GET.BY.USER)
+    private getUserNotifications: GetUserNotificationsUseCase,
+    @inject(MODULE.NOTIFICATION.USE_CASE.DELETE)
+    private deleteNotification: DeleteNotificationUseCase,
+  ) {}
+}
