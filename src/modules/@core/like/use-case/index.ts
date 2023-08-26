140ecc54e9efb1ad455e20dfa584b40fa85fd1fe
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-08-26 13:02:20 -0300

    [:sparkles:] | index

--- src/modules/@core/like/use-case/index.ts
+++ src/modules/@core/like/use-case/index.ts
@@ -3,3 +3,4 @@ export * from './delete.use-case';
 export * from './get_post_likes.use-case';
 export * from './get_user_likes.use-case';
 export * from './get_comment_likes.use-case';
+export * from './events';
