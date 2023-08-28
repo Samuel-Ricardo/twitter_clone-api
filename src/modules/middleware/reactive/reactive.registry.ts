c35a1cb4edd88c7e066972c9e2dda19191c481ad
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-08-28 15:06:00 -0300

    [:sparkles:] | create: registry - reactive (module/middleware)

---
+++ src/modules/middleware/reactive/reactive.registry.ts
@@ -0,0 +1,7 @@
+export const ReactiveMiddlewareRegistry = {
+  ERROR: Symbol('reactive.middleware.ERROR'),
+  LOGGER: {
+    APP: Symbol('reactive.middleware.LOGGER.APP'),
+    ERROR: Symbol('reactive.middleware.LOGGER.ERROR'),
+  },
+};
