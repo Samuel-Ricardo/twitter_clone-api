faec795fd0aba9ed9bc6410f8ac2398048164fe6
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-08-28 16:33:05 -0300

    [:truck:] | create: factory - socket (modules/router)

--- src/modules/router/reactive/socket/socket.factory.ts
+++ src/modules/router/reactive/socket/socket.factory.ts
@@ -2,6 +2,8 @@ import { NotificationSocket } from './notification/notification.socket';
 import { ReactiveSocketRouterModule } from './socket.module';
 
 export const ReactiveSocketRouterRegistry = {
+  ALL: () =>
+    ReactiveSocketRouterModule.getAll(ReactiveSocketRouterRegistry.ALL),
   NOTIFICATION: () =>
     ReactiveSocketRouterModule.get<NotificationSocket>(
       ReactiveSocketRouterRegistry.NOTIFICATION,
