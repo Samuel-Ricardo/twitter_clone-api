66a13fd3f192fc46c4832df6c080dab25b54d3b8
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-08-28 10:50:31 -0300

    [:recycle:] | refact: move - DTO > app (module/events)

--- src/modules/event/app/DTO/error.dto.ts
+++ src/modules/event/app/DTO/error.dto.ts
@@ -2,7 +2,7 @@ import { AppError } from '@modules/error';
 import { Socket } from 'socket.io';
 
 export interface IErrorEvent {
-  job: (data: IErrorEventDTO) => Promise<void> | Promise<any>;
+  (data: IErrorEventDTO): Promise<void> | Promise<any>;
 }
 
 export interface IErrorEventDTO {
