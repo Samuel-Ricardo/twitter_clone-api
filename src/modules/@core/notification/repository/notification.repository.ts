13229c584795425877bf3cb7d775760a7b93bd2d
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-08-28 10:00:18 -0300

    [:card_file_box:] | update: repository (notification)

--- src/modules/@core/notification/repository/notification.repository.ts
+++ src/modules/@core/notification/repository/notification.repository.ts
@@ -8,7 +8,9 @@ import {
 
 export interface INotificationRepository {
   create(notification: ICreateNotificationDTO): Promise<Notification>;
-  setVisualized(notification: ISetNotificationVisualizedDTO): Promise<void>;
+  setVisualized(
+    notification: ISetNotificationVisualizedDTO,
+  ): Promise<Notification>;
   delete(notification: IDeleteNotificationDTO): Promise<void>;
   getByUser(notification: IGetNotificationByUserDTO): Promise<Notification[]>;
 }
