2b1ab09bb906a6a6688fa42f569098faa19ddd71
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-08-28 16:30:13 -0300

    [:truck:] | update: registry - socket (modules/router)

--- src/modules/router/reactive/socket/socket.registry.ts
+++ src/modules/router/reactive/socket/socket.registry.ts
@@ -1,3 +1,4 @@
-export const RouterSocketRegistry = {
+export const ReactiveSocketRouterRegistry = {
+  ALL: Symbol.for('router.reactive.socket.all'),
   NOTIFIACTION: Symbol.for('router.reactive.socket.notification'),
 };
