fbf291093f67a67128a52f660ca244bdb706af25
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-08-28 10:52:36 -0300

    [:sparkles:] | update: event - app (node)

--- src/modules/event/node/app/app.events.ts
+++ src/modules/event/node/app/app.events.ts
@@ -1,18 +1,19 @@
 import { MODULE } from '@modules/app.registry';
 import EventEmitter2 from 'eventemitter2';
 import { inject, injectable } from 'inversify';
-import { IErrorEvent, IErrorEventDTO } from './DTO/error.dto';
 import { EVENT } from '@modules/event/event.config';
+import { IErrorEvent, IErrorEventDTO } from '../../app/DTO';
+import { IAppEvents } from '../../app';
 
 @injectable()
-export class NodeAppEvents {
+export class NodeAppEvents implements IAppEvents {
   constructor(
     @inject(MODULE.EVENTS.NODE.EMITTER)
     protected readonly events: EventEmitter2,
   ) {}
 
-  async supscribeErrorEvents(schedule: IErrorEvent) {
-    this.events.on(EVENT.ERROR.APP, schedule.job);
+  async supscribeErrorEvents(job: IErrorEvent) {
+    this.events.on(EVENT.ERROR.APP, job);
   }
 
   async publishErrorEvent(data: IErrorEventDTO) {
