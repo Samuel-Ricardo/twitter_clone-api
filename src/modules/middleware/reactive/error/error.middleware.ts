42bcb0289b557e59ba4567fb866ada9452f896f2
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-08-27 23:47:55 -0300

    [:sparkles:] | create: middleware - error  (reactive)

--- src/modules/middleware/reactive/error/error.middleware.ts
+++ src/modules/middleware/reactive/error/error.middleware.ts
@@ -1,16 +1,16 @@
-import { MODULE } from '@modules';
-import { SocketIO } from '@modules/reactive/socket_io/socket';
-import { inject, injectable } from 'inversify';
+import { AppError } from '@modules/error';
+import { EVENTS } from '@modules/reactive/reactive.config';
+import { Socket } from 'socket.io';
 
-@injectable()
-export class ReactiveErrorMiddleware {
-  constructor(
-    @inject(MODULE.REACTIVE.SOCKET.IO)
-    private readonly socket: SocketIO,
-  ) {
-    this.setup();
-  }
-  setup() {
-    throw new Error('Method not implemented.');
-  }
-}
+export const PUBLISH_ERROR = (error: Error | AppError, socket: Socket) => {
+  if (error instanceof AppError)
+    socket.emit(EVENTS.ERROR.APP, error.toStruct());
+
+  socket.emit(EVENTS.ERROR.APP, {
+    name: error.name,
+    status: 500,
+    message: error.message,
+    cause: error.cause,
+    error: true,
+  });
+};
