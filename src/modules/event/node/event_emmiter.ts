a3c30cebc17c44ca854a434dbe258dc98ea88968
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-08-28 10:16:24 -0300

    [:sparkles:] | update: class - Event (module)

--- src/modules/event/node/event_emmiter.ts
+++ src/modules/event/node/event_emmiter.ts
@@ -1,8 +1,9 @@
+import EventEmitter2 from 'eventemitter2';
 import { injectable } from 'inversify';
 import { EventEmitter } from 'node:events';
 
 @injectable()
-export class Events extends EventEmitter {
+export class Events extends EventEmitter2 {
   constructor() {
     super();
   }
