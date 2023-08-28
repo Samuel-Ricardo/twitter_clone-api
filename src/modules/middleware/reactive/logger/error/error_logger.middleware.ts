af019204e48ee8f7f26e99339444eae039eaa789
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-08-28 00:00:33 -0300

    [:sparkles:] | create: middleware - logger > error (reactive)

---
+++ src/modules/middleware/reactive/logger/error/error_logger.middleware.ts
@@ -0,0 +1,28 @@
+import { logger } from '@logger';
+import { MODULE } from '@modules/app.registry';
+import { EVENTS } from '@modules/reactive/reactive.config';
+import { SocketIO } from '@modules/reactive/socket_io/socket';
+import { inject, injectable } from 'inversify';
+import { Socket } from 'socket.io';
+
+@injectable()
+export class ReactiveErrorLoggerMiddleware {
+  constructor(
+    @inject(MODULE.REACTIVE.SOCKET.IO)
+    private readonly socket: SocketIO,
+  ) {
+    this.setup();
+  }
+
+  async setup() {
+    this.socket.io.on(EVENTS.CONNECTION, (socket) =>
+      this.subscribeErrorLogs(socket),
+    );
+  }
+  subscribeErrorLogs(socket: Socket) {
+    socket.onAnyOutgoing((event: string, error: Error) => {
+      if (event == EVENTS.ERROR.APP)
+        logger.error({ context: 'WEBSOCKET', message: error.message, error });
+    });
+  }
+}
