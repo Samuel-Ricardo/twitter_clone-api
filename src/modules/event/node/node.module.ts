53fe8a9a9f956bbaece94415f97ae65244755e35
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-08-28 10:16:43 -0300

    [:sparkles:] | update: module - node (module/event)

--- src/modules/event/node/node.module.ts
+++ src/modules/event/node/node.module.ts
@@ -1,14 +1,14 @@
 import { Container } from 'inversify';
 import { NodeEventsRegistry } from './node.registry';
 import { Events } from './event_emmiter';
-import { EventEmitter } from 'node:events';
+import { EventEmitter2 } from 'eventemitter2';
 import { NodeLikeEvents } from './like';
 
 export const NodeEventsModule = new Container({ autoBindInjectable: true });
 
 NodeEventsModule.bind(NodeEventsRegistry.INSTANCE.EMITTER).to(Events);
 NodeEventsModule.bind(NodeEventsRegistry.EMITTER).toConstantValue(
-  new EventEmitter(),
+  new EventEmitter2(),
 );
 
 NodeEventsModule.bind(NodeEventsRegistry.LIKE).to(NodeLikeEvents);
