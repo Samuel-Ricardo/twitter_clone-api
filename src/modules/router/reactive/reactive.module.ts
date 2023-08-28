de3a085767477fc1ee7693ba5b55a92080b8090e
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-08-28 16:43:06 -0300

    [:truck:] | update: module - router (modules)

--- src/modules/router/reactive/reactive.module.ts
+++ src/modules/router/reactive/reactive.module.ts
@@ -1,6 +1,6 @@
 import { Container } from 'inversify';
 import { ReactiveSocketRouterModule as SOCKET } from './socket/socket.module';
 
-export const Module = new Container({ autoBindInjectable: true });
+const Module = new Container({ autoBindInjectable: true });
 
-export const ReactiveSocketRouterRegistry = Container.merge(Module, SOCKET);
+export const ReactiveRouterRegistry = Container.merge(Module, SOCKET);
--- src/modules/router/router.module.ts
+++ src/modules/router/router.module.ts
@@ -1,7 +1,10 @@
 import { Container } from 'inversify';
 import { routes } from './app.router';
 import { RoutesRegistry } from './router.registry';
+import { ReactiveSocketRouterModule as REACTIVE } from './reactive/socket/socket.module';
 
-export const RoutesModule = new Container({ autoBindInjectable: true });
+const Module = new Container({ autoBindInjectable: true });
+
+export const RoutesModule = Container.merge(Module, REACTIVE);
 
 RoutesModule.bind(RoutesRegistry.ROUTES).toConstantValue(routes);
