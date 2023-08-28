6012796d10d3af348ab270d962885ac433e7f482
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-08-28 10:41:17 -0300

    [:sparkles:] | create: event - App (node)

---
+++ src/modules/event/node/app/app.events.ts
@@ -0,0 +1,21 @@
+import { MODULE } from '@modules/app.registry';
+import EventEmitter2 from 'eventemitter2';
+import { inject, injectable } from 'inversify';
+import { IErrorEvent, IErrorEventDTO } from './DTO/error.dto';
+import { EVENT } from '@modules/event/event.config';
+
+@injectable()
+export class AppEvents {
+  constructor(
+    @inject(MODULE.EVENTS.NODE.EMITTER)
+    protected readonly events: EventEmitter2,
+  ) {}
+
+  async supscribeErrorEvents(schedule: IErrorEvent) {
+    this.events.on(EVENT.ERROR.APP, schedule.job);
+  }
+
+  async publishErrorEvent(data: IErrorEventDTO) {
+    this.events.emit(EVENT.ERROR.APP, data);
+  }
+}
