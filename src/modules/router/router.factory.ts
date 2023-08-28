987058a6d8823d47c2e246cffe9cef04e967a54f
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-08-28 16:43:56 -0300

    [:truck:] | update: factory - router (modules)

--- src/modules/router/router.factory.ts
+++ src/modules/router/router.factory.ts
@@ -1,7 +1,9 @@
 import { Router } from 'express';
 import { RoutesModule } from './router.module';
 import { RoutesRegistry } from './router.registry';
+import { ReactiveRouterFactory as REACTIVE } from './reactive/reactive.factory';
 
 export const RoutesFactory = {
+  REACTIVE,
   ROUTER: () => RoutesModule.get<Router>(RoutesRegistry.ROUTES),
 };
