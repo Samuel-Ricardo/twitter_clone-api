cb79e768b3d7710a27fcfa8c63048dffa0aaed3b
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-08-29 16:34:26 -0300

    [:clown_face:] | simulate: controller - notification (mock/module)

--- test/mock/module/core/notification/controller/notification.controller.ts
+++ test/mock/module/core/notification/controller/notification.controller.ts
@@ -1,5 +1,17 @@
+import { interfaces } from 'inversify';
 import { NotificationController } from '../../../../../../src/modules/@core/notification/controller';
 import { mockDeep } from 'jest-mock-extended';
+import { NotificationMockRegistry } from '../notification.registry';
+import { ISimulatedNotificationController } from '@test/@types/simulate/notification/controller';
 
 export const mockNotificationController = () =>
   mockDeep<NotificationController>();
+
+export const simulateNotificationController = ({
+  container,
+}: interfaces.Context): ISimulatedNotificationController => {
+  const service = container.get<any>(NotificationMockRegistry.SERVICE);
+  const controller = new NotificationController(service);
+
+  return { controller, service };
+};
