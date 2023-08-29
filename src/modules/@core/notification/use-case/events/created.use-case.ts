98ca35d1cf3886c738bcc1d3b38d94f2927b5e43
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-08-29 09:35:53 -0300

    [:sparkles:] | create: events - Notification > emit (use-case)

--- src/modules/@core/notification/use-case/events/created.use-case.ts
+++ src/modules/@core/notification/use-case/events/created.use-case.ts
@@ -1,6 +1,8 @@
+import { injectable } from 'inversify';
 import { INotificationDTO } from '../../DTO';
 import { NotificationEventSupport } from '../../events';
 
+@injectable()
 export class EmitNotificationCreatedEventUseCase extends NotificationEventSupport {
   emit(data: INotificationDTO) {
     this.events.emitNotificationCreated(data);
