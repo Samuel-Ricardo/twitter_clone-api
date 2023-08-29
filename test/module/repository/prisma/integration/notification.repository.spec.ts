95b8da8f72fd79d406cbef33cc822b44968d869c
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-08-29 20:52:46 -0300

    [:white_check_mark:] | test: create - repository > notification (integration)

--- test/module/repository/prisma/integration/notification.repository.spec.ts
+++ test/module/repository/prisma/integration/notification.repository.spec.ts
@@ -5,9 +5,21 @@
 import { INotificationRepository } from '../../../../../src/modules/@core/notification/repository';
 import { MODULES } from '@modules';
 import { PrismaNotificationRepository } from '../../../../../src/modules/repository/prisma/notification';
+import {
+  VALID_POST_NOTIFICATION_DATA,
+  VALID_POST_NOTIFICATION,
+  SET_VISUALIZED_POST_NOTIFICATION,
+  SET_VISUALIZED_POST_NOTIFICATION_DATA,
+  CREATE_POST_NOTIFICATION_DATA,
+  VALID_USER,
+  VALID_POST,
+} from '@test/mock/data/notification';
+
+import { Notification } from '../../../../../src/modules/@core/notification/entity';
 
 describe('[REPOSITORY] | PRISMA => [NOTIFICATION]', () => {
   let repository: INotificationRepository;
+  let notification: Notification;
 
   beforeEach(() => {
     repository = MODULES.REPOSITORY.PRISMA.NOTIFICATION();
@@ -17,6 +29,13 @@ describe('[REPOSITORY] | PRISMA => [NOTIFICATION]', () => {
   });
 
   it('[INTEGRATION] | Should: Create => [NOTIFICATION]', async () => {
-    expect(true).toBeTruthy();
+    const result = await repository.create(VALID_POST_NOTIFICATION_DATA);
+
+    expect(result).toHaveProperty('_id');
+    expect(result).toBeInstanceOf(Notification);
+    expect(result.userId).toBe(VALID_POST_NOTIFICATION_DATA.userId);
+    expect(result.eventId).toBe(VALID_POST_NOTIFICATION_DATA.eventId);
+
+    notification = result;
   });
 });
