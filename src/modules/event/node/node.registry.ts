20b24d8120a117c6b680b9663d449a560c5a7e87
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-08-29 08:02:45 -0300

    [:sparkles:] | update: registry - node (module/event)

--- src/modules/event/node/node.registry.ts
+++ src/modules/event/node/node.registry.ts
@@ -3,6 +3,7 @@ export const NodeEventsRegistry = {
     EMITTER: Symbol.for('Insatance.Events.Node.EventEmitter'),
   },
   EMITTER: Symbol.for('Events.Node.EventEmitter'),
+  NOTIFICATION: Symbol.for('Events.Node.Notification'),
   LIKE: Symbol.for('Events.Node.Like'),
   APP: Symbol.for('Events.Node.App'),
 };
