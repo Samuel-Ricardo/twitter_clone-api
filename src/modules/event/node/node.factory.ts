89cf6fad72692ffabbfa401c9976a41f23d81eb1
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-08-29 08:04:33 -0300

    [:sparkles:] | update: factory - node (module/event)

--- src/modules/event/node/node.factory.ts
+++ src/modules/event/node/node.factory.ts
@@ -3,9 +3,14 @@ import { Events } from './event_emmiter';
 import { NodeLikeEvents } from './like';
 import { NodeEventsModule } from './node.module';
 import { NodeEventsRegistry } from './node.registry';
+import { NodeNotificationEvents } from './notification';
 
 export const NodeEventsFactory = {
   EMITTER: () => NodeEventsModule.get<Events>(NodeEventsRegistry.EMITTER),
+  NOTIFICATION: () =>
+    NodeEventsModule.get<NodeNotificationEvents>(
+      NodeEventsRegistry.NOTIFICATION,
+    ),
   LIKE: () => NodeEventsModule.get<NodeLikeEvents>(NodeEventsRegistry.LIKE),
   APP: () => NodeEventsModule.get<NodeAppEvents>(NodeEventsRegistry.APP),
 };
