0f67cb1a449702ed26c2c9caa1f77e8f4b6521a4
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-08-29 16:48:12 -0300

    [:clown_face:] | simulate: repository - notification (mock/prisma)

--- test/mock/module/repository/prisma/impl/notification.repository.ts
+++ test/mock/module/repository/prisma/impl/notification.repository.ts
@@ -1,5 +1,20 @@
+import { ISimulatedPrismaNotificationRepository } from '@test/@types/simulate/notification/repository';
 import { PrismaNotificationRepository } from '../../../../../../src/modules/repository/prisma/notification';
-import { mockDeep } from 'jest-mock-extended';
+import { DeepMockProxy, mockDeep } from 'jest-mock-extended';
+import { interfaces } from 'inversify';
+import { PrismaClient } from '@prisma/client';
+import { PrismaMockResgistry } from '@test/mock/module/prisma';
 
 export const mockPrismaNotificationRepository = () =>
   mockDeep<PrismaNotificationRepository>();
+
+export const simulatePrismaNotificationRepository = ({
+  container,
+}: interfaces.Context): ISimulatedPrismaNotificationRepository => {
+  const prisma = container.get<DeepMockProxy<PrismaClient>>(
+    PrismaMockResgistry.PRISMA,
+  );
+  const repository = new PrismaNotificationRepository(prisma);
+
+  return { prisma, repository };
+};
