9752d6dae6ec25d3211c83403016089d9e738777
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-08-29 12:23:28 -0300

    [:sparkles:] | update: module - notification (router/socket)

--- src/modules/router/reactive/socket/socket.module.ts
+++ src/modules/router/reactive/socket/socket.module.ts
@@ -2,22 +2,20 @@ import { Container } from 'inversify';
 import { ReactiveSocketRouterRegistry } from './socket.registry';
 import { NotificationSocket } from './notification/notification.socket';
 import { ReactiveModule as REACTIVE } from '../../../reactive/reactive.module';
-import { EventsModule as EVENTS } from '../../../event/event.module';
 import { NotificationModule as NOTIFICATION } from '../../../@core/notification/notification.module';
 
 const Module = new Container({ autoBindInjectable: true });
 
-Module.bind(ReactiveSocketRouterRegistry.NOTIFICATION)
-  .to(NotificationSocket)
-  .inSingletonScope();
-
-Module.bind(ReactiveSocketRouterRegistry.ALL)
-  .to(NotificationSocket)
-  .inSingletonScope();
-
 export const ReactiveSocketRouterModule = Container.merge(
   Module,
   REACTIVE,
-  EVENTS,
   NOTIFICATION,
+);
+
+ReactiveSocketRouterModule.bind(ReactiveSocketRouterRegistry.NOTIFICATION).to(
+  NotificationSocket,
+);
+
+ReactiveSocketRouterModule.bind(ReactiveSocketRouterRegistry.ALL).to(
+  NotificationSocket,
 );
