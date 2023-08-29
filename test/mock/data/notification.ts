eb62f65ac963cec158d7473365d1ba5b35a6260d
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-08-29 12:37:33 -0300

    [:clown_face:] | mock: data - notification (test)

---
+++ test/mock/data/notification.ts
@@ -0,0 +1,41 @@
+import { VALID_USER as USER } from './user';
+import { VALID_POST as POST } from './post';
+import { randomID } from '../../../src/modules/util/mongo';
+import {
+  INotificationDTO,
+  ICreateNotificationDTO,
+  ISetNotificationVisualizedDTO,
+} from '../../../src/modules/@core/notification/DTO';
+import { Notification } from '../../../src/modules/@core/notification/entity';
+
+export const VALID_USER = USER;
+export const VALID_POST = POST;
+
+export const CREATE_POST_NOTIFICATION_DATA: ICreateNotificationDTO = {
+  body: '[user] Post a tweet',
+  userId: VALID_USER.id,
+  eventId: VALID_POST.id,
+  type: 'POST',
+};
+
+export const VALID_POST_NOTIFICATION_DATA: INotificationDTO = {
+  ...CREATE_POST_NOTIFICATION_DATA,
+  id: randomID(),
+  createdAt: new Date(),
+  updatedAt: new Date(),
+};
+
+export const VALID_POST_COMMENT = Notification.create(
+  VALID_POST_NOTIFICATION_DATA,
+);
+
+export const SET_VISUALIZED_POST_NOTIFICATION_DATA: ISetNotificationVisualizedDTO =
+  {
+    id: VALID_POST_NOTIFICATION_DATA.id,
+    visualizedAt: new Date(),
+  };
+
+export const SET_VISUALIZED_POST_NOTIFICATION = Notification.create({
+  ...VALID_POST_NOTIFICATION_DATA,
+  visualizedAt: SET_VISUALIZED_POST_NOTIFICATION_DATA.visualizedAt,
+});
