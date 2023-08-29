6f604ad93be981a529719f244cf2db4778be676e
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-08-29 16:04:52 -0300

    [:clown_face:] | simulate: service - notification (mock/module)

--- test/mock/module/core/notification/service/notification.service.ts
+++ test/mock/module/core/notification/service/notification.service.ts
@@ -1,4 +1,51 @@
-import { NotificationService } from '@Core/notification/service';
+import { ISimulatedNotificationService } from '@test/@types/simulate/notification/service';
+import { NotificationService } from '../../../../../../src/modules/@core/notification/service/notification.service';
+import { interfaces } from 'inversify';
 import { mockDeep } from 'jest-mock-extended';
+import { NotificationMockRegistry } from '../notification.registry';
 
 export const mockNotificationService = () => mockDeep<NotificationService>();
+
+export const simulateNotificationService = ({
+  container,
+}: interfaces.Context): ISimulatedNotificationService => {
+  const create = container.get<any>(NotificationMockRegistry.USE_CASE.CREATE);
+  const delete_notification = container.get<any>(
+    NotificationMockRegistry.USE_CASE.DELETE,
+  );
+  const visualize = container.get<any>(
+    NotificationMockRegistry.USE_CASE.VISUALIZE,
+  );
+  const user = container.get<any>(
+    NotificationMockRegistry.USE_CASE.GET.BY.USER,
+  );
+  const visualized = container.get<any>(
+    NotificationMockRegistry.USE_CASE.EMIT.VISUALIZED,
+  );
+  const created = container.get<any>(
+    NotificationMockRegistry.USE_CASE.EMIT.CREATED,
+  );
+
+  const service = new NotificationService(
+    create,
+    visualize,
+    user,
+    delete_notification,
+    created,
+    visualized,
+  );
+
+  return {
+    service,
+    use_case: {
+      create,
+      visualize,
+      get: { by: { user } },
+      delete_notification,
+      emit: {
+        created,
+        visualized,
+      },
+    },
+  };
+};
