f72e17c81183d47a4ccbf716d63b5a3a1dc34a08
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-08-29 11:47:50 -0300

    [:sparkles:] | create: router - notification > post (express)

--- src/modules/router/notification/notification.router.ts
+++ src/modules/router/notification/notification.router.ts
@@ -1,6 +1,20 @@
+import { CreateNotificationSchema } from '@Core/notification/validator';
 import { MODULES } from '@modules';
+import { validate } from '../../middleware/validator';
 import { Router } from 'express';
 
 const prefix = '/notifications';
 const router = Router();
 const notification = MODULES.NOTIFICATION.DEFAULT();
+
+router.post(
+  prefix,
+  validate(CreateNotificationSchema),
+  async (req, res, next) => {
+    try {
+      res.status(201).json(await notification.create(req.body));
+    } catch (error) {
+      next(error);
+    }
+  },
+);
