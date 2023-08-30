2913b40eeff08b4fd72e2f350a9b510e7ecdad50
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-08-29 21:51:47 -0300

    [:white_check_mark:] | test: unit - controller > notification (module)

--- test/module/core/notification/notification.controller.spec.ts
+++ test/module/core/notification/notification.controller.spec.ts
@@ -1,5 +1,9 @@
 import { ISimulatedNotificationController } from '@test/@types/simulate/notification/controller';
 import { MockFactory } from '@test/mock';
+import {
+  VALID_POST_NOTIFICATION,
+  VALID_POST_NOTIFICATION_DATA,
+} from '@test/mock/data/notification';
 
 describe('[CONTROLLER] | NOTIFICATION', () => {
   let module: ISimulatedNotificationController;
@@ -15,6 +19,19 @@ describe('[CONTROLLER] | NOTIFICATION', () => {
   });
 
   it('[UNIT] | Should: create => [NOTIFICATION]', async () => {
-    expect(true).toBe(true);
+    module.service.createNotification.mockResolvedValue(
+      VALID_POST_NOTIFICATION,
+    );
+
+    const result = await module.controller.create(VALID_POST_NOTIFICATION_DATA);
+
+    expect(result).toStrictEqual({
+      notification: VALID_POST_NOTIFICATION.toStruct(),
+    });
+
+    expect(module.service.createNotification).toHaveBeenCalledTimes(1);
+    expect(module.service.createNotification).toHaveBeenCalledWith(
+      VALID_POST_NOTIFICATION_DATA,
+    );
   });
 });
