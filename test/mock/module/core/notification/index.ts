5b7adeb7862a23a23c9b13f755a4d3996afca12b
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-08-29 21:07:03 -0300

    [:sparkles:] | create: index

---
+++ test/mock/module/core/notification/index.ts
@@ -0,0 +1,6 @@
+export * from './notification.module';
+export * from './notification.factory';
+export * from './notification.registry';
+export * from './service';
+export * from './use-case';
+export * from './controller';
