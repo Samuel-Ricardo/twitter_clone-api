94dece996e2c99769cf8d07ff1b72d60143e9f6f
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-09-25 12:32:36 -0300

    [:card_file_box:] | update: repository - user (module)

--- src/modules/@core/user/user.repository.ts
+++ src/modules/@core/user/user.repository.ts
@@ -1,12 +1,14 @@
 import { CreateUserDTO, SelectUserByIdDTO, UpdateUserDTO, User } from '@User';
 import { IDeleteuserDTO } from './DTO/delete.dto';
-import { ISelectUserByCredentials } from './DTO/select_by_credentials.dto';
+import { ISelectUserByCredentialsDTO } from './DTO/select_by_credentials.dto';
+import { ISelectUserByEmailDTO } from './DTO/select_by_email.dto';
 
 export interface IUserRepository {
   create(user: CreateUserDTO): Promise<User>;
   selectAll(): Promise<User[]>;
   selectById(props: SelectUserByIdDTO): Promise<User>;
-  selectByCredentials(props: ISelectUserByCredentials): Promise<User>;
+  selectByCredentials(props: ISelectUserByCredentialsDTO): Promise<User>;
+  selectByEmail(props: ISelectUserByEmailDTO): Promise<User>;
   update(props: UpdateUserDTO): Promise<User>;
   delete(props: IDeleteuserDTO): Promise<boolean>;
 }
