d491fe9f4e88a6ac394a7f115ba7b9ec018d4650
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-10-21 09:37:17 -0300

    [:loud_sound:] | log: middleware - validation (module)

--- src/modules/middleware/validator/validator.middleware.ts
+++ src/modules/middleware/validator/validator.middleware.ts
@@ -1,11 +1,17 @@
 import { z } from 'zod';
 import { Request, Response, NextFunction } from 'express';
 import { NoDataProvidedError } from '@modules/error/data';
+import { logger } from '@logger';
 
 export const validate =
   (schema: z.ZodSchema) =>
   (req: Request, res: Response, next: NextFunction) => {
     try {
+      logger.info(
+        { context: 'VALIDATOR', message: 'Data:' },
+        { params: req.params, body: req.body },
+      );
+
       if (Object.keys({ ...req.body }).length > 0)
         return schema.parse(req.body);
       if (Object.keys({ ...req.params }).length > 0)
