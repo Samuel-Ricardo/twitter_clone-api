deb81f1b1cb4a031c90460a5448da99a30b9b1df
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-10-21 09:32:58 -0300

    [:safety_vest:] | update: schema - set [viewed] > notification (module)

--- src/modules/@core/notification/validator/set_visualized.validator.ts
+++ src/modules/@core/notification/validator/set_visualized.validator.ts
@@ -4,6 +4,6 @@ import { z } from 'zod';
 export const SetVisualizedSchema = z
   .object({
     id: z.string().catch(catchZod),
-    visualizedAt: z.date().catch(catchZod),
+    visualizedAt: z.date().or(z.string()).catch(catchZod),
   })
   .catch(catchZod);
