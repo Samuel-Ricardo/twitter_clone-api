a98c7eac0524e0d12755e4a3e24d2b5169355fe2
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-10-21 09:34:28 -0300

    [:recycle:] | refact: rename

--- src/modules/@core/user/DTO/select_by_credentials.dto.ts
+++ src/modules/@core/user/DTO/select_by_credentials.dto.ts
@@ -1,7 +1,7 @@
 import { catchZod } from '@modules/util/validator';
 import { z } from 'zod';
 
-export interface ISelectUserByCredentials {
+export interface ISelectUserByCredentialsDTO {
   email: string;
   password: string;
 }
