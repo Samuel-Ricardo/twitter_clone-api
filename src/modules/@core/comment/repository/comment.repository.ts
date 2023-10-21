309ec2bab2329a32d5f5063125379aa7107ecd1d
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-10-21 09:28:24 -0300

    [:card_file_box:] | update: repository - comment] (module)

--- src/modules/@core/comment/repository/comment.repository.ts
+++ src/modules/@core/comment/repository/comment.repository.ts
@@ -5,12 +5,14 @@ import {
   IGetPostCommentsDTO,
   IUpdateCommentDTO,
 } from '../DTO';
+import { IGetCommentByIdDTO } from '../DTO/get_by_id.dto';
 import { Comment } from '../entity';
 
 export interface ICommentRepository {
   create(comment: ICreateCommentDTO): Promise<Comment>;
   update(comment: IUpdateCommentDTO): Promise<Comment>;
   delete(comment: IDeleteCommentDTO): Promise<void>;
+  getById(comment: IGetCommentByIdDTO): Promise<Comment>;
   getPostComments(post: IGetPostCommentsDTO): Promise<Comment[]>;
   getUserComments(user: IGetUserCommentsDTO): Promise<Comment[]>;
 }
