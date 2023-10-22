c913d23bf938355e9a8d6a96842121e3c600bf60
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-10-22 16:12:01 -0300

    [:lock_with_ink_pen:] | create: cypher - user > encrypt [create] (module/turing)

---
+++ src/modules/cypher/cryptographer/user/user.cypher.ts
@@ -0,0 +1,26 @@
+import { IEncriptedIV } from '@Type/security/cryptography/iv/encrypted';
+import { CreateUserDTO } from '@User';
+import { IUserCypher } from '@User/cypher/user.cypher';
+import { MODULE } from '@modules/app.registry';
+import { ICryptographer } from '@modules/security/cryptography/cryptography.contract';
+import { inject, injectable } from 'inversify';
+
+@injectable()
+export class TuringUserCypher implements IUserCypher {
+  constructor(
+    @inject(MODULE.SECURITY.CRYPTOGRAPHY.TURING)
+    protected readonly cryptographer: ICryptographer,
+  ) {}
+
+  async hashPassword(password: string) {
+    return await this.cryptographer.hash(password);
+  }
+
+  async comparePassword(password: string, hashedPassword: string) {
+    return await this.cryptographer.compareHash(password, hashedPassword);
+  }
+
+  encryptCreateUserDTO(user: CreateUserDTO): IEncriptedIV {
+    return this.cryptographer.encryptIV(JSON.stringify(user));
+  }
+}
