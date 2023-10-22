07884b37c1af72cc4016e21c0b89eb7378cf0a9e
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-10-22 13:32:13 -0300

    [:lock_with_ink_pen:] | create: get - iv > crypto (module/security/algorithm)

--- src/modules/security/cryptography/algorithm/crypto/crypto.algorithm.ts
+++ src/modules/security/cryptography/algorithm/crypto/crypto.algorithm.ts
@@ -4,9 +4,15 @@ import { MODULE } from '@modules/app.registry';
 import { injectable } from 'inversify';
 
 import crypto from 'node:crypto';
+import { IEncriptedIV } from '@Type/security/cryptography/iv/encrypted';
+import { ENV } from '@env';
 
 @injectable()
 export class Crypto implements ICryptographyIVAlgotihm {
   @injectCrypto(MODULE.CRYPTO)
   protected readonly crypto: typeof crypto;
+
+  get initialVector() {
+    return this.crypto.randomBytes(32);
+  }
 }
