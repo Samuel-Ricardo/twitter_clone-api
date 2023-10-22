b46dddcfb33bc3b03508ecbe3234f4fa270ab25a
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-10-22 15:04:59 -0300

    [:lock_with_ink_pen:] | update: cryptographer - turing (module/security)

--- src/modules/security/cryptography/turing/turing.cryptographer.ts
+++ src/modules/security/cryptography/turing/turing.cryptographer.ts
@@ -1,6 +1,25 @@
+import { IEncriptedIV } from '@Type/security/cryptography/iv/encrypted';
 import { ICryptographer } from '../cryptography.contract';
 
+import { inject, injectable } from 'inversify';
+import { injectCrypto } from '@modules/crypto/crypto.module';
+import { injectArgon } from '@modules/argon/argon.module';
+
+import crypto from 'crypto';
+import argon2 from 'argon2';
+import { ICryptographyIVAlgotihm } from '../algorithm/cryptography.algorithm';
+import { IHashAlgorithm } from '../algorithm/hash/hash.algorithm';
+import { MODULE } from '@modules/app.registry';
+
+@injectable()
 export class TuringCryptographer implements ICryptographer {
+  constructor(
+    @inject(MODULE.SECURITY.CRYPTOGRAPHY.ALGORITHM.IV.CRYPTO)
+    protected readonly crypto: ICryptographyIVAlgotihm,
+    @inject(MODULE.SECURITY.CRYPTOGRAPHY.ALGORITHM.HASH.ARGON[2])
+    protected readonly hashAlgorithm: IHashAlgorithm,
+  ) {}
+
   hash(word: string): string | Promise<string> {
     throw new Error('Method not implemented.');
   }
