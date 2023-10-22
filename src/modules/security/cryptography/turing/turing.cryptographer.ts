694d04874b65c8c085425f9dcb593851e8a6c155
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-10-22 15:07:24 -0300

    [:lock_with_ink_pen:] | implements: ICryptogeaphrt - turing >  hash (module/security)

--- src/modules/security/cryptography/turing/turing.cryptographer.ts
+++ src/modules/security/cryptography/turing/turing.cryptographer.ts
@@ -20,16 +20,7 @@ export class TuringCryptographer implements ICryptographer {
     protected readonly hashAlgorithm: IHashAlgorithm,
   ) {}
 
-  hash(word: string): string | Promise<string> {
-    throw new Error('Method not implemented.');
-  }
-  compareHash(word: string, hash: string): boolean | Promise<boolean> {
-    throw new Error('Method not implemented.');
-  }
-  encryptIV(word: string): IEncriptedIV | Promise<IEncriptedIV> {
-    throw new Error('Method not implemented.');
-  }
-  decryptIV(word: IEncriptedIV): string | Promise<string> {
-    throw new Error('Method not implemented.');
+  hash(plain: string) {
+    return this.hashAlgorithm.hash(plain);
   }
 }
