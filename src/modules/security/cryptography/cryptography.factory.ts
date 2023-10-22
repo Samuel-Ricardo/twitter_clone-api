6c0299b3c070214cbae74307c8ec4ce381777ad9
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-10-22 15:18:21 -0300

    [:lock_with_ink_pen:] | update: factory - cryptography (module/security)

--- src/modules/security/cryptography/cryptography.factory.ts
+++ src/modules/security/cryptography/cryptography.factory.ts
@@ -1,5 +1,9 @@
 import { CRYPTOGRAPHY_ALGORITHM_FACTORY as ALGORITHM } from './algorithm/algorithm.factory';
+import { CRYPTOGRAPHY_MODULE } from './cryptography.module';
+import { CRYPTOGRAPHY_REGISTRY } from './cryptography.registry';
+import { Turing } from './turing/turing.cryptographer';
 
 export const CRYPTOGRAPHY_FACTORY = {
   ALGORITHM,
+  TURING: () => CRYPTOGRAPHY_MODULE.get<Turing>(CRYPTOGRAPHY_REGISTRY.TURING),
 };
