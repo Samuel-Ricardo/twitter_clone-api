8198c490f8aab41636f93c9e84ef846c0817d9f6
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-10-22 16:07:54 -0300

    [:lock_with_ink_pen:] | create: cypher - user (module/turing)

---
+++ src/modules/cypher/turing/user/user.cypher.ts
@@ -0,0 +1,12 @@
+import { IUserCypher } from '@User/cypher/user.cypher';
+import { MODULE } from '@modules/app.registry';
+import { ICryptographer } from '@modules/security/cryptography/cryptography.contract';
+import { inject, injectable } from 'inversify';
+
+@injectable()
+export class TuringUserCypher implements IUserCypher {
+  constructor(
+    @inject(MODULE.SECURITY.CRYPTOGRAPHY.TURING)
+    protected readonly cryptographer: ICryptographer,
+  ) {}
+}
