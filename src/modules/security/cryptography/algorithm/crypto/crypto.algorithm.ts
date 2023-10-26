1ff5f8fc1324652ae6498c8949106572e7af5844
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-10-26 11:46:29 -0300

    [:lock_with_ink_pen:] | setup: algorithm - crypto (module)

--- src/modules/security/cryptography/algorithm/crypto/crypto.algorithm.ts
+++ src/modules/security/cryptography/algorithm/crypto/crypto.algorithm.ts
@@ -1,60 +1,19 @@
-import { injectCrypto } from '@modules/crypto/crypto.module';
 import { ICryptographyIVAlgotihm } from '../cryptography.algorithm';
 import { MODULE } from '@modules/app.registry';
-import { injectable } from 'inversify';
+import { inject, injectable } from 'inversify';
 
-import crypto from 'node:crypto';
-import { IEncriptedIV } from '@Type/security/cryptography/iv/encrypted';
+import cryptoLib from 'node:crypto';
 import { ENV } from '@env';
 
 @injectable()
 export class Crypto implements ICryptographyIVAlgotihm {
-  @injectCrypto(MODULE.CRYPTO)
-  protected readonly crypto: typeof crypto;
+  protected readonly _breakpoint = ENV.SECURITY.CRYPTOGRAPHY.BREAKPOINT;
+  protected readonly _authBreakpoint =
+    ENV.SECURITY.CRYPTOGRAPHY.AUTH.BREAKPOINT;
+  protected readonly _ivBreakpoint = ENV.SECURITY.CRYPTOGRAPHY.IV.BREAKPOINT;
 
-  encryptIV(plain: string): IEncriptedIV {
-    const { cipher, iv } = this.cipher;
-
-    let data = cipher.update(plain, 'utf8', 'hex');
-    data += cipher.final('hex');
-
-    return { data, iv };
-  }
-
-  decryptIV(encrypted: IEncriptedIV) {
-    const { decipher, iv } = this.decipher;
-
-    let data = decipher.update(encrypted.data, 'hex', 'utf8');
-    data += decipher.final('utf8');
-
-    return data;
-  }
-
-  get cipher() {
-    const iv = this.initialVector;
-    return {
-      cipher: this.crypto.createCipheriv(
-        ENV.SECURITY.CRYPTOGRAPHY.ALGORITHM,
-        ENV.SECURITY.CRYPTOGRAPHY.KEY,
-        iv,
-      ),
-      iv,
-    };
-  }
-
-  get decipher() {
-    const iv = this.initialVector;
-    return {
-      decipher: this.crypto.createDecipheriv(
-        ENV.SECURITY.CRYPTOGRAPHY.ALGORITHM,
-        ENV.SECURITY.CRYPTOGRAPHY.KEY,
-        iv,
-      ),
-      iv,
-    };
-  }
-
-  get initialVector() {
-    return this.crypto.randomBytes(32);
-  }
+  constructor(
+    @inject(MODULE.CRYPTO)
+    protected readonly crypto: typeof cryptoLib,
+  ) {}
 }
