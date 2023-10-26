76f1c0e70062914368bdf485fba0a2a4468bf956
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-10-26 16:52:52 -0300

    [:lock_with_ink_pen:] | setup: algorithm - hash >  argon2 (module)

--- src/modules/security/cryptography/algorithm/hash/argon/argon.algorithm.ts
+++ src/modules/security/cryptography/algorithm/hash/argon/argon.algorithm.ts
@@ -1,18 +1,17 @@
 import { injectable } from 'inversify';
 import { IHashAlgorithm } from '../hash.algorithm';
-import { injectArgon } from '@modules/argon/argon.module';
-import { MODULE } from '@modules/app.registry';
+import { injectArgon } from '../../../../../argon/argon.module';
+import { MODULE } from '../../../../../app.registry';
 import argon2 from 'argon2';
+import { ENV } from '@env';
+import { randomUUID, randomBytes } from 'node:crypto';
 
 @injectable()
 export class Argon2 implements IHashAlgorithm {
   @injectArgon(MODULE.ARGON[2])
   protected readonly argon: typeof argon2;
 
-  async hash(word: string) {
-    return await this.argon.hash(word, { type: this.argon.argon2id });
-  }
-  async compareHash(word: string | Buffer, hash: string) {
-    return await this.argon.verify(hash, word, { type: this.argon.argon2id });
-  }
+  protected readonly breakpoint = ENV.SECURITY.CRYPTOGRAPHY.HASH.BREAKPOINT;
+  protected readonly saltBreakpoint =
+    ENV.SECURITY.CRYPTOGRAPHY.HASH.SALT.BREAKPOINT;
 }
