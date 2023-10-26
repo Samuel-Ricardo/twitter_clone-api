4c928d03afd210743dd32eb4db9607d5f6bfe516
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-10-26 09:24:57 -0300

    [:clown_face:] | mock: policy - encrypt > user (mock/module)

---
+++ test/mock/module/core/user/policy/security/encrypt/user.policy.ts
@@ -0,0 +1,5 @@
+import { EncryptUserBeforeSendPolicy } from '@User/policy/security/encrypt/user.policy';
+import { mockDeep } from 'jest-mock-extended';
+
+export const mockEncryptUserBeforeSendPolicy =
+  mockDeep<EncryptUserBeforeSendPolicy>();
