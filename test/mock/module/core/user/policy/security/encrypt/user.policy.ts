0b00a60521f8a3224468bc30645878bb3a7675f9
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-10-26 11:30:07 -0300

    [:recycle:] | refact: imports

--- test/mock/module/core/user/policy/security/encrypt/user.policy.ts
+++ test/mock/module/core/user/policy/security/encrypt/user.policy.ts
@@ -1,4 +1,4 @@
-import { EncryptUserBeforeSendPolicy } from '@User/policy/security/encrypt/user.policy';
+import { EncryptUserBeforeSendPolicy } from '../../../../../../../../src/modules/@core/user/policy/security/encrypt/user.policy';
 import { mockDeep } from 'jest-mock-extended';
 
 export const mockEncryptUserBeforeSendPolicy = () =>
