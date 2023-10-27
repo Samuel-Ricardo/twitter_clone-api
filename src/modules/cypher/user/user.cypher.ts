2c39ce527174fe11c037142f040be2c14702b696
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-10-26 23:05:07 -0300

    [:sparkles:] | update: cypher - user (module)

--- src/modules/cypher/user/user.cypher.ts
+++ src/modules/cypher/user/user.cypher.ts
@@ -1,5 +1,10 @@
-import { IEncriptedIV } from '@Type/security/cryptography/iv/encrypted';
-import { CreateUserDTO } from '@User';
+import {
+  CreateUserDTO,
+  IDeleteuserDTO,
+  SelectUserByIdDTO,
+  UpdateUserDTO,
+} from '@User';
+import { IUserDTO } from '@User/DTO/user.dto';
 import { IUserCypher } from '@User/cypher/user.cypher';
 import { MODULE } from '@modules/app.registry';
 import { ICryptographer } from '@modules/security/cryptography/cryptography.contract';
@@ -11,6 +16,38 @@ export class UserCypher implements IUserCypher {
     @inject(MODULE.SECURITY.CRYPTOGRAPHY.TURING)
     protected readonly cryptographer: ICryptographer,
   ) {}
+
+  encryptUser(user: IUserDTO): string {
+    return this.cryptographer.encryptIV(JSON.stringify(user));
+  }
+
+  decryptUser(user: string): IUserDTO {
+    return JSON.parse(this.cryptographer.decryptIV(user));
+  }
+
+  encryptUpdateUserDTO(updateUserDTO: UpdateUserDTO): string {
+    throw new Error('Method not implemented.');
+  }
+
+  decryptUpdateUserDTO(updateUserDTO: string): UpdateUserDTO {
+    throw new Error('Method not implemented.');
+  }
+
+  ecnryptDeleteUserDTO(user: IDeleteuserDTO): string {
+    throw new Error('Method not implemented.');
+  }
+
+  decryptDeleteUserDTO(user: string): IDeleteuserDTO {
+    throw new Error('Method not implemented.');
+  }
+
+  encryptSelectUserByIdDTO(user: SelectUserByIdDTO): string {
+    throw new Error('Method not implemented.');
+  }
+
+  decryptSelectUserByIdDTO(user: string): SelectUserByIdDTO {
+    throw new Error('Method not implemented.');
+  }
 
   async hashPassword(password: string) {
     return await this.cryptographer.hash(password);
@@ -24,7 +61,7 @@ export class UserCypher implements IUserCypher {
     return this.cryptographer.encryptIV(JSON.stringify(user));
   }
 
-  decryptCreateUserDTO(user: IEncriptedIV): CreateUserDTO {
+  decryptCreateUserDTO(user: string): CreateUserDTO {
     return JSON.parse(this.cryptographer.decryptIV(user));
   }
 }
