7ade93bb720ef5fd826a3a4ff36fc33ef0aba508
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-10-26 23:21:10 -0300

    [:lock_with_ink_pen:] | update: registry - security (module)

--- src/modules/security/security.registry.ts
+++ src/modules/security/security.registry.ts
@@ -2,5 +2,4 @@ import { CRYPTOGRAPHY_REGISTRY as CRYPTOGRAPHY } from './cryptography/cryptograp
 
 export const SECURITY_REGISTRY = {
   CRYPTOGRAPHY,
-  TURING: Symbol.for('MODULE:SECURITY.CRYPTOGRAPHY.TURING'),
 };
