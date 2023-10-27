58c2416f8b1ea9f9473acad124481c0945aa436f
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-10-26 23:10:34 -0300

    [:lock_with_ink_pen:] | create: algorithm - cryptographyIV (module)

--- src/modules/security/cryptography/algorithm/cryptography.algorithm.ts
+++ src/modules/security/cryptography/algorithm/cryptography.algorithm.ts
@@ -1,6 +1,4 @@
-import { IEncriptedIV } from '@Type/security/cryptography/iv/encrypted';
-
 export interface ICryptographyIVAlgotihm {
-  encryptIV(word: string): IEncriptedIV | Promise<IEncriptedIV>;
-  decryptIV(word: IEncriptedIV): string | Promise<string>;
+  encryptIV(plain: string): string;
+  decryptIV(encrypted: string): string;
 }
