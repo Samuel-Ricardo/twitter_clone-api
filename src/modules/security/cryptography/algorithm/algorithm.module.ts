d5149364a27a299b6568f5aa30e46b548a875dd7
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-10-26 23:09:51 -0300

    [:lock_with_ink_pen:] | update: module - algorithm (module/security)

--- src/modules/security/cryptography/algorithm/algorithm.module.ts
+++ src/modules/security/cryptography/algorithm/algorithm.module.ts
@@ -3,10 +3,18 @@ import { CRYPTOGRAPHY_ALGORITHM_REGISTRY } from './algorithm.registry';
 import { Argon2 } from './hash/argon/argon.algorithm';
 import { Crypto } from './crypto/crypto.algorithm';
 import getDecorators from 'inversify-inject-decorators';
+import { CRYPTO_MODULE } from '../../../crypto/crypto.module';
+import { ARGON_MODULE } from '../../../argon/argon.module';
 
-export const CRYPTOGRAPHY_ALGORITHM_MODULE = new Container({
+const MODULE = new Container({
   autoBindInjectable: true,
 });
+
+export const CRYPTOGRAPHY_ALGORITHM_MODULE = Container.merge(
+  MODULE,
+  CRYPTO_MODULE,
+  ARGON_MODULE,
+);
 
 CRYPTOGRAPHY_ALGORITHM_MODULE.bind(
   CRYPTOGRAPHY_ALGORITHM_REGISTRY.HASH.ARGON[2],
