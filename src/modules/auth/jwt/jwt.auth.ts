76d0880d85b5a9ddaa524f6b2f213d3e7cac23ed
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-10-27 15:00:39 -0300

    [:passport_control:] | setup: authorizer - jwt (module)

--- src/modules/auth/jwt/jwt.auth.ts
+++ src/modules/auth/jwt/jwt.auth.ts
@@ -1,14 +1,18 @@
 import { jsonwebtoken } from '@Type/core/auth/jwt';
-import { injectable } from 'inversify';
+import { inject, injectable } from 'inversify';
 import { ENV } from '@env';
 import { IAuthorizationData } from '@Type/core/auth/jwt/data';
 import { IAuthorizer } from '../generic.auth';
+import { MODULE } from '@modules/app.registry';
 
 @injectable()
 export class JWT implements IAuthorizer {
   protected readonly _secret = ENV.AUTH.TOKEN.SECRET;
 
-  constructor(protected readonly _jwt: jsonwebtoken) {}
+  constructor(
+    @inject(MODULE.JWT)
+    protected readonly _jwt: jsonwebtoken,
+  ) {}
 
   generateToken({ id }: IAuthorizationData) {
     return this._jwt.sign(id, this._secret);
