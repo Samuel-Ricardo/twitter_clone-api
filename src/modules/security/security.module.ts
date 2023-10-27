9f18a656c3f051a90d3527f440babae0e66be21b
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-10-26 23:20:55 -0300

    [:lock_with_ink_pen:] | update: module - security (module)

--- src/modules/security/security.module.ts
+++ src/modules/security/security.module.ts
@@ -2,13 +2,9 @@ import { Container } from 'inversify';
 import { CRYPTOGRAPHY_MODULE } from './cryptography/cryptography.module';
 
 import getDecorators from 'inversify-inject-decorators';
-import { SECURITY_REGISTRY } from './security.registry';
-import { Turing } from './cryptography/turing/turing.cryptographer';
 
 const MODULE = new Container({ autoBindInjectable: true });
 
 export const SECURITY_MODULE = Container.merge(MODULE, CRYPTOGRAPHY_MODULE);
-
-SECURITY_MODULE.bind(SECURITY_REGISTRY.TURING).to(Turing);
 
 export const { lazyInject: injectSecurity } = getDecorators(SECURITY_MODULE);
