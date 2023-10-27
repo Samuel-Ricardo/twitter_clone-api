5b6cabab84d49e901a7580a62c4da1615eb5556e
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-10-26 23:02:09 -0300

    [:sparkles:] | update: factory - cypher (module)

--- src/modules/cypher/cypher.factory.ts
+++ src/modules/cypher/cypher.factory.ts
@@ -3,5 +3,5 @@ import { CYPHER_REGISTRY } from './cypher.registry';
 import { UserCypher } from './user/user.cypher';
 
 export const CYPHER_FACTORY = {
-  USER: CYPHER_MODULE.get<UserCypher>(CYPHER_REGISTRY.USER),
+  USER: () => CYPHER_MODULE.get<UserCypher>(CYPHER_REGISTRY.USER),
 };
