81777ecd57b6599c445259ed17fe8ecedfdd6a91
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-10-28 12:18:37 -0300

    [:passport_control:] | update: policy - authorize - user (module)

--- src/modules/@core/user/policy/authorization/authorization.policy.ts
+++ src/modules/@core/user/policy/authorization/authorization.policy.ts
@@ -1,9 +1,20 @@
 import { IIsUserAuthorizedDTO } from '@User/DTO/authorizer/user.dto';
-import { AuthorizerAccess } from '../../authorizer/authorizer.access';
-import { injectable } from 'inversify';
+//import { AuthorizerAccess } from '../../authorizer/authorizer.access';
+import { inject, injectable } from 'inversify';
+import { MODULE } from '@modules/app.registry';
+import { IAuthorizer } from '@modules/auth/generic.auth';
+import { SelectUserByIdUseCase } from '@User/use-case';
+import { AuthorizerAccess } from '@User/authorizer/authorizer.access';
 
 @injectable()
 export class AuthorizeAllExistingUserPolicy extends AuthorizerAccess {
+  //   constructor(
+  //     @inject(MODULE.AUTH.JWT)
+  //     protected readonly authorizer: IAuthorizer,
+  //     @inject(MODULE.USER.USE_CASE.SELECT.BY_ID)
+  //     protected readonly selectUserById: SelectUserByIdUseCase,
+  //   ) {}
+
   async execute(data: IIsUserAuthorizedDTO) {
     try {
       const { id } = this.authorizer.validateToken(data.id);
