861da2998095efac7311f2c2f6015e5b9698bfa7
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-10-28 12:21:16 -0300

    [:recycle:] | refact: rename - policy (user)

--- src/modules/@core/user/policy/security/encrypt/password.policy.ts
+++ src/modules/@core/user/policy/security/encrypt/password.policy.ts
@@ -2,7 +2,7 @@ import { UserCypherAccess } from '@User/cypher/cypher.access';
 import { injectable } from 'inversify';
 
 @injectable()
-export class HashPasswrodBeforeSave extends UserCypherAccess {
+export class HashPasswrodBeforeSavePolicy extends UserCypherAccess {
   async execute(password: string) {
     return await this._cypher.hashPassword(password);
   }
