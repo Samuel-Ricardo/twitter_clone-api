a40cb0360ba0e4a707f22bd862aee0949c101b2a
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-10-27 23:56:59 -0300

    [:sparkles:] | update: use-case - validate [password] > user (module)

--- src/modules/@core/user/use-case/validate_password.use-case.ts
+++ src/modules/@core/user/use-case/validate_password.use-case.ts
@@ -1,9 +1,10 @@
 import { injectable } from 'inversify';
 import { IValidateUserPasswordDTO } from '@User/DTO/validate_password.use-case';
+import { UserCypherAccess } from '@User/cypher/cypher.access';
 
 @injectable()
-export class ValidateUserPasswordUseCase {
+export class ValidateUserPasswordUseCase extends UserCypherAccess {
   execute({ password }: IValidateUserPasswordDTO) {
-    return password.given === password.expected;
+    return this._cypher.comparePassword(password.expected, password.given);
   }
 }
