988dc5f0aa5ba2c51f5221149fe790470148fb19
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-10-27 23:58:34 -0300

    [:passport_control:] | create: use-case - authorize > user (module)

---
+++ src/modules/@core/user/use-case/authorize.use-case.ts
@@ -0,0 +1,17 @@
+import { CreateUserDTO } from '@User/DTO';
+import { IAuthorizeUserDTO } from '@User/DTO/authorizer/authorize.dto';
+import { IUserRepository } from '@User/user.repository';
+import { MODULE } from '@modules/app.registry';
+import { inject, injectable } from 'inversify';
+
+@injectable()
+export class AuthorizeUserUseCase {
+  constructor(
+    @inject(MODULE.REPOSITORY.PRISMA.USER)
+    private repository: IUserRepository,
+  ) {}
+
+  async execute({ id, sessionToken }: IAuthorizeUserDTO) {
+    return await this.repository.update({ id, sessionToken });
+  }
+}
