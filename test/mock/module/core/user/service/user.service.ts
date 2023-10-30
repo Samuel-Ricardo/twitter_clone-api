ebf6edd550aaabc901c5700f4ec308ff629fdee6
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-10-30 15:20:55 -0300

    [:clown_face:] | simulate: service - user (mock/module)

--- test/mock/module/core/user/service/user.service.ts
+++ test/mock/module/core/user/service/user.service.ts
@@ -1,7 +1,7 @@
-import { UserService } from '@User';
 import { interfaces } from 'inversify';
 import { UserMockRegistry } from '../user.resgistry';
 import { mockDeep } from 'jest-mock-extended';
+import { UserService } from '@User';
 
 export const simulateUserService = (context: interfaces.Context) =>
   new UserService(
@@ -14,6 +14,11 @@ export const simulateUserService = (context: interfaces.Context) =>
     context.container.get(UserMockRegistry.USE_CASE.VALIDATE.PASSWORD),
     context.container.get(UserMockRegistry.USE_CASE.SELECT.BY.EMAIL),
     context.container.get(UserMockRegistry.POLICY.SECURITY.ENCRYPT.USER),
+    context.container.get(
+      UserMockRegistry.POLICY.SECURITY.AUTHORIZATION.AFTER.SELECT.BY
+        .CREDENTIALS,
+    ),
+    context.container.get(UserMockRegistry.POLICY.SECURITY.ENCRYPT.PASSWORD),
   );
 
 export const mockUserService = () => mockDeep<UserService>();
