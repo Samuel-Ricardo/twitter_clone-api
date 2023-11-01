0ee7c5ef0f6462198ac12d65ebf5e3bdd17087dd
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-11-01 17:10:06 -0300

    [:lock_with_ink_pen:] | update: factory - cypher > post (module)

--- src/modules/cypher/cypher.factory.ts
+++ src/modules/cypher/cypher.factory.ts
@@ -1,7 +1,9 @@
 import { CYPHER_MODULE } from './cypher.module';
 import { CYPHER_REGISTRY } from './cypher.registry';
+import { PostCypher } from './post/post.cypher';
 import { UserCypher } from './user/user.cypher';
 
 export const CYPHER_FACTORY = {
   USER: () => CYPHER_MODULE.get<UserCypher>(CYPHER_REGISTRY.USER),
+  POST: () => CYPHER_MODULE.get<PostCypher>(CYPHER_REGISTRY.POST),
 };
