2e85630b40439dbee1b290ec190dda529f7149b7
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-11-01 17:09:52 -0300

    [:lock_with_ink_pen:] | update: module - cypher > post (module)

--- src/modules/cypher/cypher.module.ts
+++ src/modules/cypher/cypher.module.ts
@@ -4,11 +4,13 @@ import { UserCypher } from './user/user.cypher';
 
 import getDecorators from 'inversify-inject-decorators';
 import { CYPHER_REGISTRY } from './cypher.registry';
+import { PostCypher } from './post/post.cypher';
 
 const MODULE = new Container({ autoBindInjectable: true });
 
 export const CYPHER_MODULE = Container.merge(MODULE, SECURITY_MODULE);
 
 CYPHER_MODULE.bind(CYPHER_REGISTRY.USER).to(UserCypher);
+CYPHER_MODULE.bind(CYPHER_REGISTRY.POST).to(PostCypher);
 
 export const { lazyInject: injectCypher } = getDecorators(CYPHER_MODULE);
