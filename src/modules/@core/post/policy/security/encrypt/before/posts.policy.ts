2b5ee7126de83317a6af3311af22b74a9c883998
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-11-01 17:19:32 -0300

    [:lock_with_ink_pen:] | create: policy - encrypt > posts (module)

---
+++ src/modules/@core/post/policy/security/encrypt/before/posts.policy.ts
@@ -0,0 +1,16 @@
+import { IPostDTO } from '@Post/DTO';
+import { Post } from '@Post/entity';
+import { PostCypherAccess } from '@Post/cypher/cypher.access';
+
+export class EncryptPostListBeforeSendPolicy extends PostCypherAccess {
+  execute(posts: IPostDTO[] | Post[]) {
+    return posts[0] instanceof Post
+      ? this.cypher.encryptPosts(
+          posts.map((p) => {
+            const post = p as Post;
+            return post.toStruct();
+          }),
+        )
+      : this.cypher.encryptPosts(posts);
+  }
+}
