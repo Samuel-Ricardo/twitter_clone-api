8ef4fdceb13c73af70c58b8180c294b576eaaa24
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-11-01 17:05:58 -0300

    [:lock_with_ink_pen:] | setup: cypher - post (module)

---
+++ src/modules/cypher/post/post.cypher.ts
@@ -0,0 +1,13 @@
+import { IPostDTO } from '@Post';
+import { IPostCypher } from '@Post/cypher/post.cypher';
+import { MODULE } from '@modules/app.registry';
+import { ICryptographer } from '@modules/security/cryptography/cryptography.contract';
+import { inject, injectable } from 'inversify';
+
+@injectable()
+export class PostCypher implements IPostCypher {
+  constructor(
+    @inject(MODULE.SECURITY.CRYPTOGRAPHY.TURING)
+    protected readonly cypher: ICryptographer,
+  ) {}
+}
