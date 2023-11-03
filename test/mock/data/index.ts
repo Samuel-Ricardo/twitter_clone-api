349049bf9eaf2f21428a78eb7ccce3ab5b39b16c
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-11-02 23:02:46 -0300

    [:clown_face:] | update: data - encrypted (mock)

--- test/mock/data/index.ts
+++ test/mock/data/index.ts
@@ -1,1 +1,14 @@
+import { MODULES } from '@modules';
+
 export * from './user';
+
+export const ENCRYPTED_DATA = 'encrypted_data';
+
+export const ENCRYPTED = (data: string) =>
+  MODULES.SECURITY.CRYPTOGRAPHY.TURING().encryptIV(data);
+export const JENCRYPTED = (data: any) =>
+  MODULES.SECURITY.CRYPTOGRAPHY.TURING().encryptIV(JSON.stringify(data));
+export const DECRYPTED = (data: string) =>
+  MODULES.SECURITY.CRYPTOGRAPHY.TURING().decryptIV(data);
+export const JDECRYPTED = <T>(data: string) =>
+  JSON.parse(MODULES.SECURITY.CRYPTOGRAPHY.TURING().decryptIV(data)) as T;
