ec2247bd689735e6f2db8b27b5b88a40af83669a
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-11-03 17:08:16 -0300

    [:lock_with_ink_pen:] | create: cypher - encrypt > follow (module)

---
+++ src/modules/cypher/follow/follow.cypher.ts
@@ -0,0 +1,11 @@
+import { injectable } from 'inversify';
+import { IFollowCypher } from '../../@core/follow/cypher/follow.cypher';
+import { CryptographerAccess } from '../cypher.access';
+import { IFollowDTO } from '../../@core/follow/DTO/follow.dto';
+
+@injectable()
+export class FollowCypher extends CryptographerAccess implements IFollowCypher {
+  encryptFollow(follow: IFollowDTO) {
+    return this.cryptographer.encryptIV(JSON.stringify(follow));
+  }
+}
