3bd15637bacf16c2ca14eaa4c4119709deadece5
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-11-03 16:09:37 -0300

    [:clown_face:] | update: registry - like (mock/module)

--- test/mock/module/core/like/like.registry.ts
+++ test/mock/module/core/like/like.registry.ts
@@ -3,6 +3,16 @@ export const MockLikeRegistry = {
   SERVICE_DEV: Symbol.for('MOCK:LIKE.SERVICE.DEV'),
   CONTROLLER: Symbol.for('MOCK:LIKE.CONTROLLER'),
   CONTROLLER_DEV: Symbol.for('MOCK:LIKE.CONTROLLER.DEV'),
+  POLICY: {
+    SECURITY: {
+      ENCRYPT: {
+        BEFORE: {
+          LIKE: Symbol.for('MOCK:LIKE.POLICY.SECURITY.ENCRYPT.BEFORE.LIKE'),
+          LIKES: Symbol.for('MOCK:LIKE.POLICY.SECURITY.ENCRYPT.BEFORE.LIKES'),
+        },
+      },
+    },
+  },
   USE_CASE: {
     EVENTS: {
       CREATE: Symbol.for('MOCK:LIKE.USE_CASE.EVENTS.CREATE'),
