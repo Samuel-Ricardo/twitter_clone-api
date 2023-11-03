e7f7d1f58b09503fa4049abede8cdbf3e7d38e40
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-11-03 10:49:27 -0300

    [:lock_with_ink_pen:] | setup: cypher - like (module)

---
+++ src/modules/cypher/like/like.cypher.ts
@@ -0,0 +1,12 @@
+import { ILikeCypher } from '@Like/cypher/like.cypher';
+import { MODULE } from '@modules/app.registry';
+import { ICryptographer } from '@modules/security/cryptography/cryptography.contract';
+import { inject, injectable } from 'inversify';
+
+@injectable()
+export class LikeCypher implements ILikeCypher {
+  constructor(
+    @inject(MODULE.SECURITY.CRYPTOGRAPHY.TURING)
+    protected readonly cypher: ICryptographer,
+  ) {}
+}
