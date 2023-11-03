6a1c5112256b7471fb299d7ab4dd897dd074f309
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-11-03 17:12:42 -0300

    [:lock_with_ink_pen:] | update: factory - cypher > follow (module)

--- src/modules/cypher/cypher.factory.ts
+++ src/modules/cypher/cypher.factory.ts
@@ -1,5 +1,6 @@
 import { CYPHER_MODULE } from './cypher.module';
 import { CYPHER_REGISTRY } from './cypher.registry';
+import { FollowCypher } from './follow/follow.cypher';
 import { LikeCypher } from './like/like.cypher';
 import { PostCypher } from './post/post.cypher';
 import { UserCypher } from './user/user.cypher';
@@ -8,4 +9,5 @@ export const CYPHER_FACTORY = {
   USER: () => CYPHER_MODULE.get<UserCypher>(CYPHER_REGISTRY.USER),
   POST: () => CYPHER_MODULE.get<PostCypher>(CYPHER_REGISTRY.POST),
   LIKE: () => CYPHER_MODULE.get<LikeCypher>(CYPHER_REGISTRY.LIKE),
+  FOLLOW: () => CYPHER_MODULE.get<FollowCypher>(CYPHER_REGISTRY.FOLLOW),
 };
