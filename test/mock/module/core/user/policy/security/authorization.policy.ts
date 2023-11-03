364abbce7f5e854bf0a7792bb4b2e5a3c9ceca7f
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-11-02 23:04:23 -0300

    [:fire:] | delete: file

--- test/mock/module/core/user/policy/security/authorization.policy.ts
+++
@@ -1,5 +0,0 @@
-import { mockDeep } from 'jest-mock-extended';
-import { AuthorizeUserAfterSelectByCredentialsPolicy } from '../../../../../../../src/modules/@core/user/policy/authorization/authorize/after/select/credentials.policy';
-
-export const mockAuthorizeUserAfterSelectByCredentialsPolicy = () =>
-  mockDeep<AuthorizeUserAfterSelectByCredentialsPolicy>();
