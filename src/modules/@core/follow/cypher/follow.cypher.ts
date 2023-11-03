2c843f30b1ab6065744a3bfc500c0ea5e6e88b9c
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-11-03 17:01:48 -0300

    [:lock_with_ink_pen:] | create: cypher - follow (module)

---
+++ src/modules/@core/follow/cypher/follow.cypher.ts
@@ -0,0 +1,9 @@
+import { IFollowDTO } from '../DTO';
+
+export interface IFollowCypher {
+  encryptFollow(follow: IFollowDTO): string;
+  encryptFollowers(followers: IFollowDTO[]): string;
+
+  decryptFollow(follow: string): IFollowDTO;
+  decryptFollowers(followers: string[]): IFollowDTO[];
+}
