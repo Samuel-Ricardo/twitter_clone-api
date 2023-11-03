1958d2bc56e570c977206050433f8fca64859822
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-11-03 10:58:29 -0300

    [:lock_with_ink_pen:] | update: module - cypher > like (module)

--- src/modules/cypher/cypher.module.ts
+++ src/modules/cypher/cypher.module.ts
@@ -5,6 +5,7 @@ import { UserCypher } from './user/user.cypher';
 import getDecorators from 'inversify-inject-decorators';
 import { CYPHER_REGISTRY } from './cypher.registry';
 import { PostCypher } from './post/post.cypher';
+import { LikeCypher } from './like/like.cypher';
 
 const MODULE = new Container({ autoBindInjectable: true });
 
@@ -12,5 +13,6 @@ export const CYPHER_MODULE = Container.merge(MODULE, SECURITY_MODULE);
 
 CYPHER_MODULE.bind(CYPHER_REGISTRY.USER).to(UserCypher);
 CYPHER_MODULE.bind(CYPHER_REGISTRY.POST).to(PostCypher);
+CYPHER_MODULE.bind(CYPHER_REGISTRY.LIKE).to(LikeCypher);
 
 export const { lazyInject: injectCypher } = getDecorators(CYPHER_MODULE);
