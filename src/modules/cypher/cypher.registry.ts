723beda887f5a5fec5e14242f53dafb94564e5b3
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-11-03 17:11:49 -0300

    [:lock_with_ink_pen:] | update: registry - cypher > follow (module)

--- src/modules/cypher/cypher.registry.ts
+++ src/modules/cypher/cypher.registry.ts
@@ -2,4 +2,5 @@ export const CYPHER_REGISTRY = {
   USER: Symbol.for('MODULE:CYPHER.USER'),
   POST: Symbol.for('MODULE:CYPHER.POST'),
   LIKE: Symbol.for('MODULE:CYPHER.LIKE'),
+  FOLLOW: Symbol.for('MODULE:CYPHER.FOLLOW'),
 };
