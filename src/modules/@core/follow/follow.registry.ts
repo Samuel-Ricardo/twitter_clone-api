390040ebd63562c6ddc1a0690ebfcbe45d86e1a1
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-11-04 10:26:55 -0300

    [:sparkles:] | update: registry - follow > policy (module)

--- src/modules/@core/follow/follow.registry.ts
+++ src/modules/@core/follow/follow.registry.ts
@@ -1,6 +1,20 @@
 export const FollowRegistry = {
   SERVICE: Symbol.for('CORE.FOLLOW.SERVICE.DEFAULT'),
   CONTROLLER: Symbol.for('CORE.FOLLOW.CONTROLLER.DEFAULT'),
+  POLICY: {
+    SECURITY: {
+      ENCRYPT: {
+        BEFORE: {
+          FOLLOW: Symbol.for(
+            'CORE.FOLLOW.POLICY.SECURITY.ENCRYPT.BEFORE.FOLLOW',
+          ),
+          FOLLOWERS: Symbol.for(
+            'CORE.FOLLOW.POLICY.SECURITY.ENCRYPT.BEFORE.FOLLOWERS',
+          ),
+        },
+      },
+    },
+  },
   USE_CASE: {
     CREATE: Symbol.for('CORE.FOLLOW.USE-CASE.CREATE'),
     DELETE: Symbol.for('CORE.FOLLOW.USE-CASE.DELETE'),
