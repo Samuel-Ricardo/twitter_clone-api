17a45159380424b88b892dc3aee554e1d8a3ba42
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-11-04 16:04:17 -0300

    [:scroll:] | update: factory - comment > policy (module)

--- src/modules/@core/comment/comment.factory.ts
+++ src/modules/@core/comment/comment.factory.ts
@@ -1,6 +1,8 @@
 import { CommentModule } from './comment.module';
 import { CommentRegistry } from './comment.registry';
 import { CommentController } from './controller';
+import { EncryptCommentBeforeSendPolicy } from './policy/security/encrypt/before/comment.policy';
+import { EncryptCommentListBeforeSendPolicy } from './policy/security/encrypt/before/comments.policy';
 import { CommentService } from './service/comment.service';
 import {
   CreateCommentUseCase,
@@ -17,6 +19,22 @@ export const CommentFactory = {
   SERVICE: () => CommentModule.get<CommentService>(CommentRegistry.SERVICE),
   CONTROLLER: () =>
     CommentModule.get<CommentController>(CommentRegistry.CONTROLLER),
+  POLICY: {
+    SECURITY: {
+      ENCRYPT: {
+        BEFORE: {
+          COMMENT: () =>
+            CommentModule.get<EncryptCommentBeforeSendPolicy>(
+              CommentRegistry.POLICY.SECURITY.ENCRYPT.BEFORE.COMMENT,
+            ),
+          COMMENTS: () =>
+            CommentModule.get<EncryptCommentListBeforeSendPolicy>(
+              CommentRegistry.POLICY.SECURITY.ENCRYPT.BEFORE.COMMENTS,
+            ),
+        },
+      },
+    },
+  },
   USE_CASE: {
     CREATE: () =>
       CommentModule.get<CreateCommentUseCase>(CommentRegistry.USE_CASE.CREATE),
