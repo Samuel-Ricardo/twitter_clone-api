6f1dbbbc4b9a5085f73a49e63fe51bdb700f4e7d
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-11-04 11:54:27 -0300

    [:label:] | update: ISimulate - service > follow (test)

--- test/@types/simulate/follow/service.ts
+++ test/@types/simulate/follow/service.ts
@@ -8,9 +8,23 @@ import {
 } from '../../../../src/modules/@core/follow/use-case';
 import { FollowService } from '../../../../src/modules/@core/follow/service';
 import { DeepMockProxy } from 'jest-mock-extended';
+import { FollowPolicy } from '../../../../src/modules/@core/follow/policy/follow.policy';
+import { EncryptFollowBeforeSendPolicy } from '../../../../src/modules/@core/follow/policy/security/encrypt/before/follow.policy';
+import { EncryptFollowListBeforeSendPolicy } from '../../../../src/modules/@core/follow/policy/security/encrypt/before/followers.policy';
 
 export interface ISimulateFollowService {
   service: FollowService;
+  policy: {
+    follow: DeepMockProxy<FollowPolicy>;
+    security: {
+      encrypt: {
+        before: {
+          follow: DeepMockProxy<EncryptFollowBeforeSendPolicy>;
+          followers: DeepMockProxy<EncryptFollowListBeforeSendPolicy>;
+        };
+      };
+    };
+  };
   use_case: {
     create: DeepMockProxy<CreateFollowUseCase>;
     unfollow: DeepMockProxy<UnFollowUseCase>;
