27d94fe736d303744187d8515ff81e52c0e4f4ec
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-11-04 11:45:22 -0300

    [:clown_face:] | update: registry - follow > policy (mock/module)

--- test/mock/module/core/follow/follow.registry.ts
+++ test/mock/module/core/follow/follow.registry.ts
@@ -3,6 +3,21 @@ export const FollowMockRegistry = {
   SERVICE_DEV: Symbol.for('MOCK.FOLLOW.SERVICE_DEV'),
   CONTROLLER: Symbol.for('MOCK.FOLLOW.CONTROLLER'),
   CONTROLLER_DEV: Symbol.for('MOCK.FOLLOW.CONTROLLER_DEV'),
+  POLICY: {
+    SECURITY: {
+      ENCRYPT: {
+        BEFORE: {
+          FOLLOW: Symbol.for(
+            'MOCK.FOLLOW.POLICY.SECURITY.ENCRYPT.BEFORE.FOLLOW',
+          ),
+          FOLLOWERS: Symbol.for(
+            'MOCK.FOLLOW.POLICY.SECURITY.ENCRYPT.BEFORE.FOLLOWERS',
+          ),
+        },
+      },
+    },
+    FOLLOW: Symbol.for('MOCK.FOLLOW.POLICY.FOLLOW'),
+  },
   USE_CASE: {
     CREATE: Symbol.for('MOCK.FOLLOW.USE_CASE.CREATE'),
     DELETE: Symbol.for('MOCK.FOLLOW.USE_CASE.DELETE'),
