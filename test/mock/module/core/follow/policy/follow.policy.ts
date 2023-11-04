1e7fa8b17db87b6be575c79fb8be73026a647e16
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-11-04 11:36:07 -0300

    [:clown_face:] | mock: policy - follow (mock/module)

---
+++ test/mock/module/core/follow/policy/follow.policy.ts
@@ -0,0 +1,4 @@
+import { FollowPolicy } from '../../../../../../src/modules/@core/follow/policy/follow.policy';
+import { mockDeep } from 'jest-mock-extended';
+
+export const mockFollowPolicy = () => mockDeep<FollowPolicy>();
