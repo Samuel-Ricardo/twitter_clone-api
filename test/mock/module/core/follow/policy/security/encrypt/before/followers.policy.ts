998876784ecf6e7ac06cea5bf711255ca0ca3d88
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-11-04 11:42:58 -0300

    [:clown_face:] | mock: policy - emcrypt > follower (mock/module)

---
+++ test/mock/module/core/follow/policy/security/encrypt/before/followers.policy.ts
@@ -0,0 +1,5 @@
+import { EncryptFollowListBeforeSendPolicy } from '../../../../../../../../../src/modules/@core/follow/policy/security/encrypt/before/followers.policy';
+import { mockDeep } from 'jest-mock-extended';
+
+export const mockEncryptFollowBeforeSendPolicy = () =>
+  mockDeep<EncryptFollowListBeforeSendPolicy>();
