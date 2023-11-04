8577d372fb8c2c49ff97272367e4595514cabb50
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-11-04 15:39:50 -0300

    [:white_check_mark:] | fix: test - create > follow (E2E)

--- test/E2E/module/follow.spec.ts
+++ test/E2E/module/follow.spec.ts
@@ -7,15 +7,21 @@ import { Follow, IFollowDTO } from '../../../src/modules/@core/follow';
 import { follow } from '../../../src/modules/router/follow';
 import { CREATE_FOLLOW_DATA } from '@test/mock/data/follow';
 import supertest from 'supertest';
+import { IFollowCypher } from '@Core/follow/cypher/follow.cypher';
+import { MODULES } from '@modules';
 
 describe('[MODULE] | Follow', () => {
   let follow_relation: Follow | IFollowDTO;
+  let cypher: IFollowCypher;
+
+  beforeAll(() => (cypher = MODULES.CYPHER.FOLLOW()));
 
   it('[E2E] | Should: be able to => [FOLLOW]', async () => {
     const resonse = await supertest(app)
       .post(follow.prefix)
       .send(CREATE_FOLLOW_DATA);
-    const body: { follow: IFollowDTO } = resonse.body;
+
+    const body = { follow: cypher.decryptFollow(resonse.body.follow) };
 
     expect(resonse.status).toBe(201);
     expect(body).toHaveProperty('follow');
