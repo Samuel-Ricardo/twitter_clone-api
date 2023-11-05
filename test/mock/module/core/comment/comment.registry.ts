a12a474bc7d2ba9664b8eb91fd6a3f139d94e147
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-11-04 21:20:33 -0300

    [:clown_face:] | update: registry - comment > policy (mock/module)

--- test/mock/module/core/comment/comment.registry.ts
+++ test/mock/module/core/comment/comment.registry.ts
@@ -3,6 +3,16 @@ export const CommentMockRegistry = {
   CONTROLLER_DEV: Symbol.for('MOCK:>COMMENT.CONTROLLER_DEV'),
   SERVICE: Symbol.for('MOCK:>COMMENT.SERVICE'),
   SERVICE_DEV: Symbol.for('MOCK:>COMMENT.SERVICE_DEV'),
+  POLICY: {
+    SECURITY: {
+      ENCRYPT: {
+        BEFORE: {
+          COMMENT: Symbol.for('MOCK:>COMMENT.POLICY.ENCRYPT.BEFORE.COMMENT'),
+          COMMENTS: Symbol.for('MOCK:>COMMENT.POLICY.ENCRYPT.BEFORE.COMMENTS'),
+        },
+      },
+    },
+  },
   USE_CASE: {
     CREATE: Symbol.for('MOCK:>COMMENT.USE_CASE.CREATE'),
     UPDATE: Symbol.for('MOCK:>COMMENT.USE_CASE.UPDATE'),
