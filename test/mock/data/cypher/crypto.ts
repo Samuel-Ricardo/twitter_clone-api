d5c322d86ad8afcfcbb423dd76390d1120e97de5
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-11-11 16:38:30 -0300

    [:clown_face:] | create: data - cypher > BREAKPOINT (mock)

--- test/mock/data/cypher/crypto.ts
+++ test/mock/data/cypher/crypto.ts
@@ -3,3 +3,5 @@ import { mockDeep } from 'jest-mock-extended';
 import { CipherGCM, DecipherGCM } from 'node:crypto';
 
 export const BREAKPOINT = ENV.SECURITY.CRYPTOGRAPHY.BREAKPOINT;
+export const AUTH_BREAKPOINT = ENV.SECURITY.CRYPTOGRAPHY.AUTH.BREAKPOINT;
+export const IV_BREAKPOINT = ENV.SECURITY.CRYPTOGRAPHY.IV.BREAKPOINT;
