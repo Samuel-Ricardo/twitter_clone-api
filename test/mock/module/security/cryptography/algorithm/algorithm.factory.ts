da36c58bee38de8fa23017ced58738183ebe3887
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-11-11 14:13:27 -0300

    [:clown_face:] | update: factory - algorithm (mock:module/security)

--- test/mock/module/security/cryptography/algorithm/algorithm.factory.ts
+++ test/mock/module/security/cryptography/algorithm/algorithm.factory.ts
@@ -3,8 +3,26 @@ import { ALGORITHM_MODULE_MOCK } from './algorithm.module';
 import { Argon2 } from '../../../../../../src/modules/security/cryptography/algorithm/hash/argon/argon.algorithm';
 import { Crypto } from '../../../../../../src/modules/security/cryptography/algorithm/crypto/crypto.algorithm';
 import { ALGORITHM_REGISTRY_MOCK } from './algorithm.registry';
+import { ISimulateArgonAlgorithm } from '@test/@types/simulate/security/cryptography/algorithm/hash/argon';
+import { ISimulateCryptoAlgorithm } from '@test/@types/simulate/security/cryptography/algorithm/crypto';
 
 export const ALGORITHM_FACTORY_MOCK = {
+  SIMULATE: {
+    HASH: {
+      ARGON: {
+        2: () =>
+          ALGORITHM_MODULE_MOCK.get<ISimulateArgonAlgorithm>(
+            ALGORITHM_REGISTRY_MOCK.HASH.ARGON[2],
+          ),
+      },
+    },
+    IV: {
+      CRYPTO: () =>
+        ALGORITHM_MODULE_MOCK.get<ISimulateCryptoAlgorithm>(
+          ALGORITHM_REGISTRY_MOCK.IV.CRYPTO,
+        ),
+    },
+  },
   HASH: {
     ARGON: {
       2: () =>
