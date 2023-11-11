0b20ca4aa02c181e8f937c8195a6837114a00229
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-11-11 14:07:04 -0300

    [:clown_face:] | simulate: algorithm - crypto (mock:module/security)

--- test/mock/module/security/cryptography/algorithm/crypto/crypto.algorithm.ts
+++ test/mock/module/security/cryptography/algorithm/crypto/crypto.algorithm.ts
@@ -1,4 +1,16 @@
 import { mockDeep } from 'jest-mock-extended';
 import { Crypto } from '../../../../../../../src/modules/security/cryptography/algorithm/crypto/crypto.algorithm';
+import { interfaces } from 'inversify';
+import { CRYPTO_REGISTRY_MOCK } from '@test/mock/module/crypto/crypto.registry';
+import { ISimulateCryptoAlgorithm } from '@test/@types/simulate/security/cryptography/algorithm/crypto';
 
 export const mockCryptoAlgorithm = () => mockDeep<Crypto>();
+
+export const simulateCryptoAlgorithm = ({
+  container,
+}: interfaces.Context): ISimulateCryptoAlgorithm => {
+  const engine = container.get<any>(CRYPTO_REGISTRY_MOCK.CRYPTO);
+  const crypto = new Crypto(engine);
+
+  return { crypto, engine };
+};
