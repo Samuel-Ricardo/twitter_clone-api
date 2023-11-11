2465f2766afa3624bfba39b314974a2f2ca519d0
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-11-11 16:41:51 -0300

    [:clown_face:] | create: data - cypher > SIMULATE_ENCRYPT (mock)

--- test/mock/data/cypher/crypto.ts
+++ test/mock/data/cypher/crypto.ts
@@ -10,3 +10,14 @@ export const CIPHERIV = mockDeep<CipherGCM>();
 export const DECIPHER = mockDeep<DecipherGCM>();
 export const AUTH_TAG = Buffer.from('auth_tag', 'utf8');
 export const INITIAL_VECTOR = Buffer.from('initial_vector', 'utf8');
+
+export const SIMULATE_ENCRYPT = (data: string) => {
+  return data
+    .concat(BREAKPOINT)
+    .concat(AUTH_BREAKPOINT)
+    .concat(AUTH_TAG.toString('hex'))
+    .concat(AUTH_BREAKPOINT)
+    .concat(IV_BREAKPOINT)
+    .concat(INITIAL_VECTOR.toString('hex'))
+    .concat(IV_BREAKPOINT);
+};
