43861b5d16b37e01eca313bad7ea4302dd285ba8
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-11-11 12:24:35 -0300

    [:clown_face:] | mock: cryptographer - turing (mock:module/security)

---
+++ test/mock/module/security/cryptography/turing/turing.cryptographer.ts
@@ -0,0 +1,4 @@
+import { Turing } from '../../../../../../src/modules/security/cryptography/turing/turing.cryptographer';
+import { mockDeep } from 'jest-mock-extended';
+
+export const mockTuring = () => mockDeep<Turing>();
