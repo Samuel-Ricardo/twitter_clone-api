79d840f96ac5e3e7a130b04755c4f0cd98595218
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-11-11 12:33:55 -0300

    [:clown_face:] | create: factory - cryptography (mock:module/security)

---
+++ test/mock/module/security/cryptography/cryptography.factory.ts
@@ -0,0 +1,13 @@
+import { DeepMockProxy } from 'jest-mock-extended';
+import { ALGORITHM_FACTORY_MOCK as ALGORITHM } from './algorithm/algorithm.factory';
+import { CRYPTOGRAPHY_MODULE_MOCK } from './cryptography.module';
+import { Turing } from '@modules/security/cryptography/turing/turing.cryptographer';
+import { CRYPTOGRAPHY_REGISTRY_MOCK } from './cryptography.registry';
+
+export const CRYPTOGRAPHY_FACTORY_MOCK = {
+  ALGORITHM,
+  TURING: () =>
+    CRYPTOGRAPHY_MODULE_MOCK.get<DeepMockProxy<Turing>>(
+      CRYPTOGRAPHY_REGISTRY_MOCK.TURING,
+    ),
+};
