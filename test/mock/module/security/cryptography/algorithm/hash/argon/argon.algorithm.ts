e2969c50e4ec7b3e0d49e2e9839b671db7949b3f
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-11-11 14:08:48 -0300

    [:clown_face:] | simulate: algorithm - argon (mock:module/security)

--- test/mock/module/security/cryptography/algorithm/hash/argon/argon.algorithm.ts
+++ test/mock/module/security/cryptography/algorithm/hash/argon/argon.algorithm.ts
@@ -1,4 +1,16 @@
+import { interfaces } from 'inversify';
 import { Argon2 } from '../../../../../../../../src/modules/security/cryptography/algorithm/hash/argon/argon.algorithm';
 import { mockDeep } from 'jest-mock-extended';
+import { ISimulateArgonAlgorithm } from '@test/@types/simulate/security/cryptography/algorithm/hash/argon';
+import { ARGON_REGISTRY_MOCK } from '@test/mock/module/argon/argon.registry';
 
 export const mockArgonAlgorithm = () => mockDeep<Argon2>();
+
+export const simulateArgonAlgorithm = ({
+  container,
+}: interfaces.Context): ISimulateArgonAlgorithm => {
+  const engine = container.get<any>(ARGON_REGISTRY_MOCK[2]);
+  const argon = new Argon2(engine);
+
+  return { argon, engine };
+};
