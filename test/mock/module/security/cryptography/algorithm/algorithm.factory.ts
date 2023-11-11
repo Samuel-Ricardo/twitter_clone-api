4743388cd603fc94be5421eb0dbd85e3908ffee4
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-11-11 12:22:42 -0300

    [:clown_face:] | create: factory - algorithm (mock:module/security)

---
+++ test/mock/module/security/cryptography/algorithm/algorithm.factory.ts
@@ -0,0 +1,22 @@
+import { DeepMockProxy } from 'jest-mock-extended';
+import { ALGORITHM_MODULE_MOCK } from './algorithm.module';
+import { Argon2 } from '../../../../../../src/modules/security/cryptography/algorithm/hash/argon/argon.algorithm';
+import { Crypto } from '../../../../../../src/modules/security/cryptography/algorithm/crypto/crypto.algorithm';
+import { ALGORITHM_REGISTRY_MOCK } from './algorithm.registry';
+
+export const ALGORITHM_FACTORY_MOCK = {
+  HASH: {
+    ARGON: {
+      2: () =>
+        ALGORITHM_MODULE_MOCK.get<DeepMockProxy<Argon2>>(
+          ALGORITHM_REGISTRY_MOCK.HASH.ARGON[2],
+        ),
+    },
+  },
+  IV: {
+    CRYPTO: () =>
+      ALGORITHM_MODULE_MOCK.get<DeepMockProxy<Crypto>>(
+        ALGORITHM_REGISTRY_MOCK.IV.CRYPTO,
+      ),
+  },
+};
