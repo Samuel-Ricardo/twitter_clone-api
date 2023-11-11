cf08ad9e20a63783e2f8a6e1290a94d2eb095327
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-11-11 11:38:57 -0300

    [:passport_control:] | create: middleware - cypher > arg (module/reactive)

--- src/modules/middleware/reactive/cypher/cypher.middleware.ts
+++ src/modules/middleware/reactive/cypher/cypher.middleware.ts
@@ -1,5 +1,13 @@
 import { MODULES } from '../../../app.factory';
 
+export const decryptArg = (arg: { encrypted?: string }) => {
+  return arg.encrypted
+    ? JSON.parse(
+        MODULES.SECURITY.CRYPTOGRAPHY.TURING().decryptIV(arg.encrypted),
+      )
+    : arg;
+};
+
 export const encryptResponse = (data: any) => {
   const cypher = MODULES.SECURITY.CRYPTOGRAPHY.TURING();
   return { encrypted: cypher.encryptIV(JSON.stringify(data)) };
