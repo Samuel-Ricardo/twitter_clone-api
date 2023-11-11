486dba5a05fc64530db046a5cc1ea4899067d5be
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-11-11 12:31:43 -0300

    [:clown_face:] | create: module - cryptography (mock:module/security)

---
+++ test/mock/module/security/cryptography/cryptography.module.ts
@@ -0,0 +1,15 @@
+import { Container } from 'inversify';
+import { ALGORITHM_MODULE_MOCK } from './algorithm/algorithm.module';
+import { CRYPTOGRAPHY_REGISTRY_MOCK } from './cryptography.registry';
+import { mockTuring } from './turing/turing.cryptographer';
+
+const MODULE = new Container({ autoBindInjectable: true });
+
+export const CRYPTOGRAPHY_MODULE_MOCK = Container.merge(
+  MODULE,
+  ALGORITHM_MODULE_MOCK,
+);
+
+CRYPTOGRAPHY_MODULE_MOCK.bind(CRYPTOGRAPHY_REGISTRY_MOCK.TURING).toDynamicValue(
+  mockTuring,
+);
