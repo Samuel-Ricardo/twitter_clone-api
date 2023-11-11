62b906eee10f97f1f7cf6828805ec19b38ee0a0f
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-11-11 12:34:21 -0300

    [:recycle:] | refact: imports

--- test/mock/module/security/cryptography/cryptography.factory.ts
+++ test/mock/module/security/cryptography/cryptography.factory.ts
@@ -1,7 +1,7 @@
 import { DeepMockProxy } from 'jest-mock-extended';
 import { ALGORITHM_FACTORY_MOCK as ALGORITHM } from './algorithm/algorithm.factory';
 import { CRYPTOGRAPHY_MODULE_MOCK } from './cryptography.module';
-import { Turing } from '@modules/security/cryptography/turing/turing.cryptographer';
+import { Turing } from '../../../../../src/modules/security/cryptography/turing/turing.cryptographer';
 import { CRYPTOGRAPHY_REGISTRY_MOCK } from './cryptography.registry';
 
 export const CRYPTOGRAPHY_FACTORY_MOCK = {
