cadab8d40e10f06cf650190d453a3f75a78bcde1
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-11-11 14:09:10 -0300

    [:clown_face:] | update: module - algorithm (mock:module/security)

--- test/mock/module/security/cryptography/algorithm/algorithm.module.ts
+++ test/mock/module/security/cryptography/algorithm/algorithm.module.ts
@@ -2,10 +2,18 @@ import { Container } from 'inversify';
 import { ALGORITHM_REGISTRY_MOCK } from './algorithm.registry';
 import { mockArgonAlgorithm } from './hash/argon/argon.algorithm';
 import { mockCryptoAlgorithm } from './crypto/crypto.algorithm';
+import { CRYPTO_MODULE_MOCK } from '@test/mock/module/crypto/crypto.module';
+import { ARGON_MODULE_MOCK } from '@test/mock/module/argon/argon.module';
 
-export const ALGORITHM_MODULE_MOCK = new Container({
+const MODULE = new Container({
   autoBindInjectable: true,
 });
+
+export const ALGORITHM_MODULE_MOCK = Container.merge(
+  MODULE,
+  CRYPTO_MODULE_MOCK,
+  ARGON_MODULE_MOCK,
+);
 
 ALGORITHM_MODULE_MOCK.bind(ALGORITHM_REGISTRY_MOCK.IV.CRYPTO).toDynamicValue(
   mockCryptoAlgorithm,
