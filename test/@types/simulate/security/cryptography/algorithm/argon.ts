99c4b18b492d104e7198e1ad08f172064d94b76d
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-11-13 12:16:13 -0300

    [:fire:] | delete: file

--- test/@types/simulate/security/cryptography/algorithm/argon.ts
+++
@@ -1,8 +0,0 @@
-import { DeepMockProxy } from 'jest-mock-extended';
-import { Argon2 } from '../../../../../../src/modules/security/cryptography/algorithm/hash/argon/argon.algorithm';
-import argonLib from 'argon2';
-
-export interface ISimulateArgonAlgorithm {
-  argon: Argon2;
-  engine: DeepMockProxy<typeof argonLib>;
-}
