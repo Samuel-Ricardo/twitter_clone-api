867ce589536b071aa763eace8064c832dc68f213
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-11-13 12:05:13 -0300

    [:clown_face:] | update: factory - cryptographer (mock:module)

--- test/mock/module/security/cryptography/cryptography.factory.ts
+++ test/mock/module/security/cryptography/cryptography.factory.ts
@@ -3,6 +3,7 @@ import { ALGORITHM_FACTORY_MOCK as ALGORITHM } from './algorithm/algorithm.facto
 import { CRYPTOGRAPHY_MODULE_MOCK } from './cryptography.module';
 import { Turing } from '../../../../../src/modules/security/cryptography/turing/turing.cryptographer';
 import { CRYPTOGRAPHY_REGISTRY_MOCK } from './cryptography.registry';
+import { ISimulateTuring } from '@test/@types/simulate/security/cryptography/turing';
 
 export const CRYPTOGRAPHY_FACTORY_MOCK = {
   ALGORITHM,
@@ -10,4 +11,10 @@ export const CRYPTOGRAPHY_FACTORY_MOCK = {
     CRYPTOGRAPHY_MODULE_MOCK.get<DeepMockProxy<Turing>>(
       CRYPTOGRAPHY_REGISTRY_MOCK.TURING,
     ),
+  SIMULATE: {
+    TURING: () =>
+      CRYPTOGRAPHY_MODULE_MOCK.get<ISimulateTuring>(
+        CRYPTOGRAPHY_REGISTRY_MOCK.SIMULATE.TURING,
+      ),
+  },
 };
