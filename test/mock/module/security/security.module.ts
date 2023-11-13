677ea154fb3fb5b3f6f7bcb13c208240a9819934
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-11-13 10:38:05 -0300

    [:clown_face:] | update: module - security (mock:module)

--- test/mock/module/security/security.module.ts
+++ test/mock/module/security/security.module.ts
@@ -3,7 +3,7 @@ import { CRYPTOGRAPHY_MODULE_MOCK } from './cryptography/cryptography.module';
 
 const MODULE = new Container({ autoBindInjectable: true });
 
-export const SECURITY_MODULE = Container.merge(
+export const SECURITY_MODULE_MOCK = Container.merge(
   MODULE,
   CRYPTOGRAPHY_MODULE_MOCK,
 );
