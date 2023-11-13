595fc4dd7401ebd569525240f51b68d22efff0dd
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-11-13 10:39:08 -0300

    [:clown_face:] | update: data - cypher (mock)

--- test/mock/data/cypher/crypto.ts
+++ test/mock/data/cypher/crypto.ts
@@ -7,7 +7,7 @@ export const AUTH_BREAKPOINT = ENV.SECURITY.CRYPTOGRAPHY.AUTH.BREAKPOINT;
 export const IV_BREAKPOINT = ENV.SECURITY.CRYPTOGRAPHY.IV.BREAKPOINT;
 
 export const CIPHERIV = mockDeep<CipherGCM>();
-export const DECIPHER = mockDeep<DecipherGCM>();
+export const DECIPHERIV = mockDeep<DecipherGCM>();
 export const AUTH_TAG = Buffer.from('auth_tag', 'utf8');
 export const INITIAL_VECTOR = Buffer.from('initial_vector', 'utf8');
 
